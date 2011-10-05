19ca93dc0dc0356961c05ca6db64141142de9809
brianwilkerson@google.com <brianwilkerson@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-10-05 19:11:46 +0000

    Added files missing in the previous CL to add support for resource directives Review URL: https://chromereviews.googleplex.com/3522020
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@63 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/internal/model/DartResourceImpl.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2011, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.tools.core.internal.model;
+
+import com.google.dart.tools.core.internal.model.info.DartElementInfo;
+import com.google.dart.tools.core.internal.model.info.DartResourceInfo;
+import com.google.dart.tools.core.internal.model.info.OpenableElementInfo;
+import com.google.dart.tools.core.internal.util.MementoTokenizer;
+import com.google.dart.tools.core.model.DartElement;
+import com.google.dart.tools.core.model.DartModelException;
+import com.google.dart.tools.core.model.DartResource;
+import com.google.dart.tools.core.workingcopy.WorkingCopyOwner;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+
+import java.util.Map;
+
+/**
+ * Instances of the class <code>DartResourceImpl</code> implement a file contained within a project
+ * that is included because of a #resource directive.
+ */
+public class DartResourceImpl extends OpenableElementImpl implements DartResource {
+  /**
+   * The file being represented by this element.
+   */
+  private IFile file;
+
+  /**
+   * Initialize a newly created resource element to be a child of the given library.
+   * 
+   * @param library the library containing the element
+   * @param file the file being represented by the element
+   */
+  protected DartResourceImpl(DartLibraryImpl library, IFile file) {
+    super(library);
+    this.file = file;
+  }
+
+  @Override
+  public String getElementName() {
+    return file.getName();
+  }
+
+  @Override
+  public int getElementType() {
+    return DART_RESOURCE;
+  }
+
+  @Override
+  public IResource getUnderlyingResource() throws DartModelException {
+    return file;
+  }
+
+  @Override
+  public IResource resource() {
+    return file;
+  }
+
+  @Override
+  protected boolean buildStructure(OpenableElementInfo info, IProgressMonitor pm,
+      Map<DartElement, DartElementInfo> newElements, IResource underlyingResource)
+      throws DartModelException {
+    // There is no structure to build at this point.
+    return true;
+  }
+
+  @Override
+  protected DartElementInfo createElementInfo() {
+    return new DartResourceInfo();
+  }
+
+  @Override
+  protected DartElement getHandleFromMemento(String token, MementoTokenizer tokenizer,
+      WorkingCopyOwner owner) {
+    // Resources do not have any children.
+    return this;
+  }
+
+  @Override
+  protected char getHandleMementoDelimiter() {
+    return MEMENTO_DELIMITER_VARIABLE;
+  }
+
+  @Override
+  protected String getHandleMementoName() {
+    return file.getProjectRelativePath().toPortableString();
+  }
+
+  @Override
+  protected IStatus validateExistence(IResource underlyingResource) {
+    if (!underlyingResource.exists()) {
+      return newDoesNotExistStatus();
+    }
+    return DartModelStatusImpl.VERIFIED_OK;
+  }
+}
