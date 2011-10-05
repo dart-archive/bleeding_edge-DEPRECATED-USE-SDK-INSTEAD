0cb420401715e7cd775dabac58b7404271de5fc0
zundel@google.com <zundel@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-10-05 14:01:12 +0000

    Create a directory for python package, move 'testing' in there
    Review URL: https://chromereviews.googleplex.com/3514028
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@40 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/tests/dartc/testcfg.py
+++ dart/samples/tests/dartc/testcfg.py
@@ -2,13 +2,13 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
-import testing
+from testing import test_configuration
 
 def GetConfiguration(context, root):
   return SampleCompilationTestConfiguration(context, root)
 
 
-class  SampleCompilationTestConfiguration(testing.CompilationTestConfiguration):
+class  SampleCompilationTestConfiguration(test_configuration.CompilationTestConfiguration):
  def __init__(self, context, root):
     super(SampleCompilationTestConfiguration, self).__init__(context, root)
 
--- dart/samples/tests/samples/testcfg.py
+++ dart/samples/tests/samples/testcfg.py
@@ -3,17 +3,16 @@
 # BSD-style license that can be found in the LICENSE file.
 
 import os
-import test
-from os.path import join, exists
-import testing
 
-class SamplesTestConfiguration(testing.StandardTestConfiguration):
+from testing import test_configuration, test_case
+
+class SamplesTestConfiguration(test_configuration.StandardTestConfiguration):
   def __init__(self, context, root):
     super(SamplesTestConfiguration, self).__init__(context, root)
 
   def ListTests(self, current_path, path, mode, arch):
     tests = []
-    src_dir = join(self.root[:-7], "src")
+    src_dir = os.path.join(self.root[:-7], "src")
     for root, dirs, files in os.walk(src_dir):
       ignore_dirs = [d for d in dirs if d.startswith('.')]
       for d in ignore_dirs:
@@ -22,9 +21,9 @@ class SamplesTestConfiguration(testing.StandardTestConfiguration):
         test_path = current_path + [ f[:-5] ]  # Remove .dart suffix.
         if not self.Contains(path, test_path):
           continue
-        tests.append(testing.StandardTestCase(self.context,
+        tests.append(test_case.StandardTestCase(self.context,
                                               test_path,
-                                              join(root, f),
+                                              os.path.join(root, f),
                                               mode,
                                               arch))
     return tests
