a8756dddf8727aab1adf6ce3a4ebf709347ec42c
ngeoffray@google.com <ngeoffray@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-10-07 14:28:18 +0000

    Add #library directives to make browser testing happy. Review URL: https://chromereviews.googleplex.com/3574016
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@222 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/chat/chat_stress_client.dart
+++ dart/samples/chat/chat_stress_client.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#library("chat_stress_client.dart");
 #import("http.dart");
 #import("../../client/json/dart_json.dart");
 
--- dart/samples/tests/src/chat/chat_server_test.dart
+++ dart/samples/tests/src/chat/chat_server_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+#library("chat_server_test.dart");
 #import("../../../chat/http.dart");
 #import("../../../../client/json/dart_json.dart");
 #import("../../../chat/chat_server_lib.dart");
