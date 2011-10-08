c1c21b48d04d16a95a38665c35fca871a03a7964
danrubel@google.com <danrubel@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-10-08 11:19:18 +0000

    Track out of date in CachingArtifactProvider Review URL: https://chromereviews.googleplex.com/3574021
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@274 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/internal/builder/CachingArtifactProvider.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/internal/builder/CachingArtifactProvider.java
@@ -30,6 +30,10 @@ import java.util.Map;
  * memory, never writing anything to disk.
  */
 public abstract class CachingArtifactProvider extends DartArtifactProvider {
+  private class CacheElement {
+    String content;
+    long lastModified;
+  }
 
   /**
    * Answer the encoded URI scheme specific part so that we don't have to decode then encode when we
@@ -52,7 +56,7 @@ public abstract class CachingArtifactProvider extends DartArtifactProvider {
    * A mapping of the value returned by {@link #getLocalUriPart(Source, String, String)} to artifact
    * content
    */
-  private final Map<String, String> cache = new HashMap<String, String>();
+  private final Map<String, CacheElement> cache = new HashMap<String, CacheElement>();
 
   /**
    * Remove all artifacts from the cache
@@ -69,12 +73,12 @@ public abstract class CachingArtifactProvider extends DartArtifactProvider {
    */
   @Override
   public Reader getArtifactReader(Source source, String part, String extension) throws IOException {
-    String content;
+    CacheElement elem;
     synchronized (cache) {
-      content = cache.get(getLocalUriPart(source, part, extension));
+      elem = cache.get(getLocalUriPart(source, part, extension));
     }
-    if (content != null) {
-      return new StringReader(content);
+    if (elem != null) {
+      return new StringReader(elem.content);
     }
     return null;
   }
@@ -95,8 +99,11 @@ public abstract class CachingArtifactProvider extends DartArtifactProvider {
       @Override
       public void close() throws IOException {
         super.close();
+        CacheElement elem = new CacheElement();
+        elem.content = toString();
+        elem.lastModified = System.currentTimeMillis();
         synchronized (cache) {
-          cache.put(uriPart, toString());
+          cache.put(uriPart, elem);
         }
       }
     };
@@ -108,7 +115,8 @@ public abstract class CachingArtifactProvider extends DartArtifactProvider {
   @Override
   public boolean isOutOfDate(Source source, Source base, String extension) {
     synchronized (cache) {
-      return cache.get(getLocalUriPart(source, "", extension)) == null;
+      CacheElement elem = cache.get(getLocalUriPart(source, "", extension));
+      return elem == null || elem.lastModified < source.getLastModified();
     }
   }
 }
