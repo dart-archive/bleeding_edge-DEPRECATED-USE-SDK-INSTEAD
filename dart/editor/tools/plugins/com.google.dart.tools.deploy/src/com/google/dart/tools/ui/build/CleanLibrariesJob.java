dfdbef25e3d10cbf410f2b0402eecc83643e68ee
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-10-13 18:40:58 +0000

    Give the user a way to refresh their workspace in the event that things get out of date. In addition, refresh the workspace when the app starts up. This is a fairly lightweight call in the event that the workspace is not out of sync with the disk. Review URL: http://codereview.chromium.org//8277010
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@409 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.deploy/src/com/google/dart/tools/ui/build/CleanLibrariesJob.java
+++ dart/editor/tools/plugins/com.google.dart.tools.deploy/src/com/google/dart/tools/ui/build/CleanLibrariesJob.java
@@ -1,26 +1,26 @@
 /*
  * Copyright (c) 2011, the Dart project authors.
- *
- * Licensed under the Eclipse Public License v1.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.google.dart.tools.ui.build;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
 
 /**
@@ -37,12 +37,22 @@ public class CleanLibrariesJob extends Job {
   @Override
   protected IStatus run(IProgressMonitor monitor) {
     try {
-      ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.CLEAN_BUILD, monitor);
+      SubMonitor subMonitor = SubMonitor.convert(monitor,
+          BuildMessages.CleanLibrariesJob_cleanLibrariesProgress, 100);
+
+      // Refresh the workspace.
+      ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE,
+          subMonitor.newChild(50));
+
+      // Clean the projects, forcing a rebuild.
+      ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.CLEAN_BUILD,
+          subMonitor.newChild(50));
+
+      subMonitor.done();
     } catch (CoreException ex) {
       return ex.getStatus();
     }
 
     return Status.OK_STATUS;
   }
-
 }
