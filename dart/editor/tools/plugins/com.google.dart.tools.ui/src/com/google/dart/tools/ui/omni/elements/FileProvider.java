b902a538ab51f8d240e138f0166587e264da0e8c
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-10-21 04:13:32 +0000

    Fix to filter .children and .project files from Search Box search results. Review URL: http://codereview.chromium.org//8363023
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@595 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/omni/elements/FileProvider.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/omni/elements/FileProvider.java
@@ -1,16 +1,14 @@
 /*
  * Copyright (c) 2011, the Dart project authors.
- *
- * Licensed under the Eclipse Public License v1.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.google.dart.tools.ui.omni.elements;
@@ -87,6 +85,17 @@ public class FileProvider extends OmniProposalProvider {
       }
 
       String name = resource.getName();
+
+      //exclude .project && .children files
+      //TODO (pquitslund): consider centralizing this filter when core search is integrated
+      if (name.equals(".project") || name.equals(".children")) { //$NON-NLS-1$ //$NON-NLS-2$
+        IPath path = resource.getFullPath();
+        // e.g., /MyProject/.project
+        if (path.segmentCount() == 2) {
+          return false;
+        }
+      }
+
       if (nameMatches(name)) {
         if (containerPattern != null) {
           // match full container path:
