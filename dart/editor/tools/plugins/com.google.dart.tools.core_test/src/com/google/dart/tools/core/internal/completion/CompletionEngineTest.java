754bdb4c111796a9aa7071cb754fa035ea8e6e18
messick@google.com <messick@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-10-26 20:10:48 +0000

    Simplify test definition, add tests. Review URL: http://codereview.chromium.org//8390060
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@775 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/internal/completion/CompletionEngineTest.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/internal/completion/CompletionEngineTest.java
@@ -14,8 +14,6 @@
 package com.google.dart.tools.core.internal.completion;
 
 import com.google.dart.tools.core.model.DartModelException;
-import com.google.dart.tools.core.model.DartProject;
-import com.google.dart.tools.core.workingcopy.WorkingCopyOwner;
 
 import junit.framework.TestCase;
 
@@ -23,7 +21,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 
 import java.net.URISyntaxException;
-import java.util.Hashtable;
+import java.util.Collection;
 
 /**
  * Short specific code completion tests
@@ -38,148 +36,121 @@ public class CompletionEngineTest extends TestCase {
 //  }
 
   public void testCompletion_constructor_field() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class X { X(this.field); int f!ield;}");
-    requestor.assertSuggested("field");
+    test("class X { X(this.field); int f!1ield;}", "1+field");
   }
 
-  public void testCompletion_forStmt_incVar() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo { mth() { for (int i = 0; i < 5; i!++); }}");
-    requestor.assertSuggested("i");
-  }
-
-  public void testCompletion_forStmt_testVar() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo { mth() { for (int i = 0; i! < 5; i++); }}");
-    requestor.assertSuggested("i");
-  }
-
-  public void testCompletion_forStmt_typeInit() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo { mth() { for (in!t i = 0; i < 5; i++); }}");
-    requestor.assertSuggested("int");
+  public void testCompletion_forStmt_vars() throws Exception {
+    test("class Foo { mth() { for (in!1t i = 0; i!2 < 5; i!3++); }}", "1+int", "2+i", "3+i");
+//        "2-int", "3-int"); // not clear these negative results should be eliminated
   }
 
   public void testCompletion_ifStmt_field1() throws Exception {
-    MockCompletionRequestor requestor = testNonCompletion("class Foo { int myField = 7; mth() { if (!) {}}}");
-    requestor.assertNotSuggested("myField");
+    test("class Foo { int myField = 7; mth() { if (!1) {}}}", "1-myField");
   }
 
   public void testCompletion_ifStmt_field1a() throws Exception {
-    MockCompletionRequestor requestor = testNonCompletion("class Foo { int myField = 7; mth() { if (!) }}");
-    requestor.assertNotSuggested("myField");
+    test("class Foo { int myField = 7; mth() { if (!1) }}", "1-myField");
   }
 
   public void testCompletion_ifStmt_field2() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo { int myField = 7; mth() { if (m!) {}}}");
-    requestor.assertSuggested("myField");
+    test("class Foo { int myField = 7; mth() { if (m!1) {}}}", "1+myField");
   }
 
   public void testCompletion_ifStmt_field2a() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo { int myField = 7; mth() { if (m!) }}");
-    requestor.assertSuggested("myField");
+    test("class Foo { int myField = 7; mth() { if (m!1) }}", "1+myField");
   }
 
   public void testCompletion_ifStmt_field2b() throws Exception {
-    MockCompletionRequestor requestor = testNonCompletion("class Foo { myField = 7; mth() { if (m!) {}}}");
-    requestor.assertNotSuggested("myField");
+    test("class Foo { myField = 7; mth() { if (m!1) {}}}", "1-myField");
   }
 
   public void testCompletion_ifStmt_localVar() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo { mth() { int value = 7; if (v!) {}}}");
-    requestor.assertSuggested("value");
+    test("class Foo { mth() { int value = 7; if (v!1) {}}}", "1+value");
   }
 
   public void testCompletion_ifStmt_localVara() throws Exception {
-    MockCompletionRequestor requestor = testNonCompletion("class Foo { mth() { value = 7; if (v!) {}}}");
-    requestor.assertNotSuggested("value");
+    test("class Foo { mth() { value = 7; if (v!1) {}}}", "1-value");
   }
 
   public void testCompletion_ifStmt_topLevelVar() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("int topValue = 7; class Foo { mth() { if (t!) {}}}");
-    requestor.assertSuggested("topValue");
+    test("int topValue = 7; class Foo { mth() { if (t!1) {}}}", "1+topValue");
   }
 
   public void testCompletion_ifStmt_topLevelVara() throws Exception {
-    MockCompletionRequestor requestor = testNonCompletion("topValue = 7; class Foo { mth() { if (t!) {}}}");
-    requestor.assertNotSuggested("topValue");
+    test("topValue = 7; class Foo { mth() { if (t!1) {}}}", "1-topValue");
   }
 
   public void testCompletion_newMemberType1() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo { ! }");
-    requestor.assertSuggested("List");
-    requestor.assertSuggested("Collection");
+    test("class Foo { !1 }", "1+Collection", "1+List");
   }
 
   public void testCompletion_newMemberType2() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo {!}");
-    requestor.assertSuggested("List");
-    requestor.assertSuggested("Collection");
+    test("class Foo {!1}", "1+Collection", "1+List");
   }
 
   public void testCompletion_newMemberType3() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo {L!}");
-    requestor.assertSuggested("List");
-    requestor.assertNotSuggested("Collection");
+    test("class Foo {L!1}", "1-Collection", "1+List");
   }
 
   public void testCompletion_newMemberType4() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Foo {C!}");
-    requestor.assertNotSuggested("List");
-    requestor.assertSuggested("Collection");
+    test("class Foo {C!1}", "1+Collection", "1-List");
+  }
+
+  public void testCompletion_return() throws Exception {
+    test("class X { m() { return Ma!1th.P!2I; }}", "1+Math", "2+PI");
   }
 
   public void testCompletion_staticField1() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("class Sunflower {static final num MAX_D = 300;num xc, yc;Sunflower() {xc = yc = MA! }}");
-    requestor.assertSuggested("MAX_D");
+    test(
+        // cannot complete Sunflower due to bug in test framework
+        "class Sunflower {static final n!2um MAX_D = 300;nu!3m xc, yc;Sunflower() {x!Xc = y!Yc = MA!1 }}",
+        "1+MAX_D", "X+xc", "Y+yc", "2+num", "3+num");
   }
 
   public void testCompletion_topLevelField_init1() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("final num PI2 = Mat!");
-    requestor.assertSuggested("Math");
+    test("final num PI2 = Mat!1", "1+Math");
   }
 
   public void testCompletion_topLevelField_init2() throws Exception {
-    MockCompletionRequestor requestor = testCompletion("final num PI2 = Mat!h.PI;");
-    requestor.assertSuggested("Math");
-  }
-
-  private MockCompletionRequestor runTest(String originalSource) throws URISyntaxException,
-      DartModelException {
-    int index = originalSource.indexOf('!');
-    assertTrue("Expected exclamation point ('!') within the source"
-        + " denoting the position at which code completion should occur", index != -1);
-    String modifiedSource = originalSource.substring(0, index)
-        + originalSource.substring(index + 1);
-
-    CompletionEngine engine;
-    CompletionEnvironment environment = null;
-    MockCompletionRequestor requestor = new MockCompletionRequestor();
-    Hashtable<String, String> options = null;
-    DartProject project = null;
-    WorkingCopyOwner owner = null;
-    IProgressMonitor monitor = new NullProgressMonitor();
-    engine = new CompletionEngine(environment, requestor, options, project, owner, monitor);
-
-    MockLibrarySource library = new MockLibrarySource("FooLib");
-    MockDartSource sourceFile = new MockDartSource(library, "Foo.dart", "");
-    engine.complete(library, sourceFile, modifiedSource, index, 0);
-    return requestor;
+    test("final num PI2 = Mat!1h.PI;", "1+Math", "1+Match", "1-void");
   }
 
   /**
-   * Generate a series of code completion suggestions for the specified source at the location
-   * within the source denoted by an exclaimation point ("!"). An exception is thrown if the source
-   * does not contain "!".
+   * Run a set of completion tests on the given <code>originalSource</code>. The source string has
+   * completion points embedded in it, which are identified by '!X' where X is a single character.
+   * Each X is matched to positive or negative results in the array of
+   * <code>validationStrings</code>. Validation strings contain the name of a prediction with a two
+   * character prefix. The first character of the prefix corresponds to an X in the
+   * <code>originalSource</code>. The second character is either a '+' or a '-' indicating whether
+   * the string is a positive or negative result.
+   * 
+   * @param originalSource The source for a completion test that contains completion points
+   * @param validationStrings The positive and negative predictions
    */
-  private MockCompletionRequestor testCompletion(String originalSource) throws URISyntaxException,
+  private void test(String originalSource, String... results) throws URISyntaxException,
       DartModelException {
-    MockCompletionRequestor requestor = runTest(originalSource);
-    assertTrue("Expected code completion suggestions", requestor.validate());
-    return requestor;
-  }
-
-  private MockCompletionRequestor testNonCompletion(String originalSource)
-      throws URISyntaxException, DartModelException {
-    MockCompletionRequestor requestor = runTest(originalSource);
-    assertFalse("Expected code completion suggestions", requestor.validate());
-    return requestor;
+    Collection<CompletionSpec> completionTests = CompletionSpec.from(originalSource, results);
+    assertTrue("Expected exclamation point ('!') within the source"
+        + " denoting the position at which code completion should occur",
+        !completionTests.isEmpty());
+    IProgressMonitor monitor = new NullProgressMonitor();
+    MockLibrarySource library = new MockLibrarySource("FooLib");
+    MockDartSource sourceFile = new MockDartSource(library, "Foo.dart", "");
+    for (CompletionSpec test : completionTests) {
+      MockCompletionRequestor requestor = new MockCompletionRequestor();
+      CompletionEngine engine = new CompletionEngine(null, requestor, null, null, null, monitor);
+      engine.complete(library, sourceFile, test.source, test.completionPoint, 0);
+      if (test.positiveResults.size() > 0) {
+        assertTrue("Expected code completion suggestions", requestor.validate());
+      } else {
+        assertFalse("Expected code completion suggestions", requestor.validate());
+      }
+      for (String result : test.positiveResults) {
+        requestor.assertSuggested(result);
+      }
+      for (String result : test.negativeResults) {
+        requestor.assertNotSuggested(result);
+      }
+    }
   }
 }
---
+++ dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/internal/completion/CompletionSpec.java
@@ -0,0 +1,108 @@
+package com.google.dart.tools.core.internal.completion;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class CompletionSpec {
+
+  /**
+   * Parse a set of completion tests from the given <code>originalSource</code>. The source string
+   * has completion points embedded in it, which are identified by '!X' where X is a single
+   * character. Each X is matched to positive or negative results in the array of
+   * <code>validationStrings</code>. Validation strings contain the name of a prediction with a two
+   * character prefix. The first character of the prefix corresponds to an X in the
+   * <code>originalSource</code>. The second character is either a '+' or a '-' indicating whether
+   * the string is a positive or negative result.
+   * 
+   * @param originalSource The source for a completion test that contains completion points
+   * @param validationStrings The positive and negative predictions
+   * @return a collection of CompletionSpec instances
+   */
+  static Collection<CompletionSpec> from(String originalSource, String[] validationStrings) {
+    Map<Character, CompletionSpec> tests = new HashMap<Character, CompletionSpec>();
+    String modifiedSource = originalSource;
+    while (true) {
+      int index = modifiedSource.indexOf('!');
+      if (index < 0) {
+        break;
+      }
+      int n = 1;
+      char id = modifiedSource.charAt(index + 1);
+      if (id != '!') {
+        n = 2;
+        CompletionSpec test = new CompletionSpec(id);
+        tests.put(id, test);
+        test.completionPoint = index;
+      }
+      modifiedSource = modifiedSource.substring(0, index) + modifiedSource.substring(index + n);
+    }
+    if (modifiedSource.equals(originalSource)) {
+      throw new IllegalStateException("No tests in source: " + originalSource);
+    }
+    for (String result : validationStrings) {
+      if (result.length() < 3) {
+        throw new IllegalStateException("Invalid completion result: " + result);
+      }
+      char id = result.charAt(0);
+      char sign = result.charAt(1);
+      String value = result.substring(2);
+      CompletionSpec test = tests.get(id);
+      if (test == null) {
+        throw new IllegalStateException("Invliad completion result id: " + id + " for: " + result);
+      }
+      test.source = modifiedSource;
+      if (sign == '+') {
+        test.positiveResults.add(value);
+      } else if (sign == '-') {
+        test.negativeResults.add(value);
+      } else {
+        String err = "Invalid completion result sign: " + sign + " for: " + result;
+        throw new IllegalStateException(err);
+      }
+    }
+    List<Character> badPoints = new ArrayList<Character>();
+    List<Character> badResults = new ArrayList<Character>();
+    for (CompletionSpec test : tests.values()) {
+      if (test.completionPoint == -1) {
+        badPoints.add(test.id);
+      }
+      if (test.positiveResults.isEmpty() && test.negativeResults.isEmpty()) {
+        badResults.add(test.id);
+      }
+    }
+    if (!(badPoints.isEmpty() && badResults.isEmpty())) {
+      StringBuffer err = new StringBuffer();
+      if (!badPoints.isEmpty()) {
+        err.append("No completion point for tests:");
+        for (Character ch : badPoints) {
+          err.append(' ').append(ch);
+        }
+        err.append(' ');
+      }
+      if (!badResults.isEmpty()) {
+        err.append("No results for tests:");
+        for (Character ch : badPoints) {
+          err.append(' ').append(ch);
+        }
+      }
+      throw new IllegalStateException(err.toString());
+    }
+    return tests.values();
+  }
+
+  char id;
+  int completionPoint;
+  List<String> positiveResults;
+  List<String> negativeResults;
+  String source;
+
+  CompletionSpec(char id) {
+    this.id = id;
+    this.completionPoint = -1;
+    this.positiveResults = new ArrayList<String>();
+    this.negativeResults = new ArrayList<String>();
+  }
+}
