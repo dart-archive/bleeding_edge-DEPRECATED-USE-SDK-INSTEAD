11c6f5865218bdcc9e9b9ed5ff89ff3ad5136311
kasperl@google.com <kasperl@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-11-08 12:21:53 +0000

    First steps towards making the SSA value set hashed.
    R=ngeoffray@google.com
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org//8499015
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@1302 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/leg/ssa/value_set.dart
+++ dart/frog/leg/ssa/value_set.dart
@@ -5,17 +5,19 @@
 // TODO(kasperl): This is a very naive non-hashed implementation of a
 // value set for global value numbering. Should be optimized.
 class ValueSet {
-  List<HInstruction> instructions;
-  ValueSet() : instructions = new List<HInstruction>();
+  List<ValueSetNode> table;
+  ValueSet() : table = new List<ValueSetNode>(1);
 
   void add(HInstruction instruction) {
     assert(lookup(instruction) === null);
-    instructions.add(instruction);
+    int index = tableIndexForInstruction(instruction);
+    table[index] = new ValueSetNode(instruction, table[index]);
   }
 
   HInstruction lookup(HInstruction instruction) {
-    for (int i = 0, length = instructions.length; i < length; i++) {
-      HInstruction cached = instructions[i];
+    int index = tableIndexForInstruction(instruction);
+    for (ValueSetNode node = table[index]; node !== null; node = node.next) {
+      HInstruction cached = node.value;
       if (cached.equals(instruction)) return cached;
     }
     return null;
@@ -23,6 +25,32 @@ class ValueSet {
 
   void kill(int flags) {
     int depends = HInstruction.computeDependsOnFlags(flags);
-    instructions = instructions.filter((e) => (e.flags & depends) == 0);
+    for (int i = 0, length = table.length; i < length; i++) {
+      ValueSetNode previous = null;
+      ValueSetNode current = table[i];
+      while (current !== null) {
+        ValueSetNode next = current.next;
+        HInstruction cached = current.value;
+        if ((cached.flags & depends) != 0) {
+          if (previous === null) {
+            table[i] = next;
+          } else {
+            previous.next = next;
+          }
+        } else {
+          previous = current;
+        }
+        current = next;
+      }
+    }
   }
+
+  // TODO(kasperl): Replace this with a proper hash based version.
+  int tableIndexForInstruction(HInstruction instruction) => 0;
+}
+
+class ValueSetNode {
+  final HInstruction value;
+  ValueSetNode next;
+  ValueSetNode(this.value, this.next);
 }
