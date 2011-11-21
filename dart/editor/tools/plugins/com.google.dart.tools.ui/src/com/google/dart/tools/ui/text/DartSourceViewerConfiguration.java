b6c29ddd376bf36d1cfe2ef7473f716c546fced4
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-11-21 19:11:14 +0000

    Change our editor tab spacing defaults to 2 spaces. Review URL: http://codereview.chromium.org//8603011
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@1713 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/text/DartSourceViewerConfiguration.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/text/DartSourceViewerConfiguration.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2011, the Dart project authors.
- *
+ * 
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -103,6 +103,8 @@ import java.util.Map;
  */
 @SuppressWarnings("restriction")
 public class DartSourceViewerConfiguration extends TextSourceViewerConfiguration {
+
+  private static final int TAB_WIDTH_IN_SPACES = 2;
 
   /**
    * Creates and returns a preference store which combines the preference stores from the text tools
@@ -632,7 +634,7 @@ public class DartSourceViewerConfiguration extends TextSourceViewerConfiguration
    */
   @Override
   public int getTabWidth(ISourceViewer sourceViewer) {
-    return super.getTabWidth(sourceViewer);
+    return TAB_WIDTH_IN_SPACES;
     // return CodeFormatterUtil.getTabWidth(getProject());
   }
 
