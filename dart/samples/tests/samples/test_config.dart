94b63e3c9a28a8f782c89c66bf67d160f58ccd30
ager@google.com <ager@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-11-21 15:12:01 +0000

    Various test script changes:
    - Supply options to cctests and filter based on name.
    - Add samples test suite.
    - Minor cleanup in the scripts.
    
    R=whesse@google.com
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org//8547012
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@1703 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/samples/tests/samples/test_config.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#library("samples_test_config");
+
+#import("../../../tools/testing/dart/test_suite.dart");
+
+class SamplesTestSuite extends StandardTestSuite {
+  SamplesTestSuite(Map configuration)
+      : super(configuration,
+              "samples/tests/samples/src",
+              ["tests/standalone/samples.status"]);
+
+  void listRecursively() => true;
+}
