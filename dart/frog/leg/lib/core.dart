4f572ccf15ad1676cb874bbe5ae0bb591a73039a
floitsch@google.com <floitsch@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-12-01 15:30:38 +0000

    Add checks to core.dart.
    Review URL: http://codereview.chromium.org//8769007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@1985 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/leg/lib/core.dart
+++ dart/frog/leg/lib/core.dart
@@ -24,89 +24,169 @@ guard$string(x) {
   throw "Type guard failed.";
 }
 
+/**
+  * Returns true if both arguments are numbers.
+  * If only the first argument is a number, throws the given message as
+  * exception.
+  */
+bool checkNumbers(var a, var b, var message) {
+  if (JS(@"typeof $0 === 'number'", a)) {
+    if (JS(@"typeof $0 === 'number'", b)) {
+      return true;
+    } else {
+      throw message;
+    }
+  }
+  return false;
+}
+
 add(var a, var b) {
-  return JS(@"$0 + $1", a, b);
+  if (checkNumbers(a, b, "num+ expects a number as second operand.")) {
+    return JS(@"$0 + $1", a, b);
+  } else if (JS(@"typeof $0 === 'string'", a)) {
+    if (JS(@"typeof $0 === 'string'", b) ||
+        JS(@"typeof $0 === 'number'", b)) {
+      return JS(@"$0 + $1", a, b);
+    }
+    throw "Unimplemented String+.";
+  }
+  throw "Unimplemented user-defined +.";
 }
 
 div(var a, var b) {
-  return JS(@"$0 / $1", a, b);
+  if (checkNumbers(a, b, "num/ expects a number as second operand.")) {
+    return JS(@"$0 / $1", a, b);
+  }
+  throw "Unimplemented user-defined /.";
 }
 
 mul(var a, var b) {
-  return JS(@"$0 * $1", a, b);
+  if (checkNumbers(a, b, "num* expects a number as second operand.")) {
+    return JS(@"$0 * $1", a, b);
+  }
+  throw "Unimplemented user-defined *.";
 }
 
 sub(var a, var b) {
-  return JS(@"$0 - $1", a, b);
+  if (checkNumbers(a, b, "num- expects a number as second operand.")) {
+    return JS(@"$0 - $1", a, b);
+  }
+  throw "Unimplemented user-defined binary -.";
 }
 
 mod(var a, var b) {
-  // Euclidean Modulo.
-  int result = JS(@"$0 % $1", a, b);
-  if (result == 0) return 0;  // Make sure we don't return -0.0.
-  if (result > 0) return result;
-  if (b < 0) {
-    return result - b;
-  } else {
-    return result + b;
+  if (checkNumbers(a, b, "int% expects an int as second operand.")) {
+    // Euclidean Modulo.
+    int result = JS(@"$0 % $1", a, b);
+    if (result == 0) return 0;  // Make sure we don't return -0.0.
+    if (result > 0) return result;
+    if (b < 0) {
+      return result - b;
+    } else {
+      return result + b;
+    }
   }
+  throw "Unimplemented user-defined %.";
 }
 
 tdiv(var a, var b) {
-  var tmp = a / b;
-  // TODO(ngeoffray): Use tmp.floor and tmp.ceil when
-  // we can handle them.
-  if (tmp < 0) {
-    return JS(@"Math.ceil($0)", tmp);
-  } else {
-    return JS(@"Math.floor($0)", tmp);
+  if (checkNumbers(a, b, "num~/ expects a number as second operand.")) {
+    var tmp = a / b;
+    // TODO(ngeoffray): Use tmp.floor and tmp.ceil when
+    // we can handle them.
+    if (tmp < 0) {
+      return JS(@"Math.ceil($0)", tmp);
+    } else {
+      return JS(@"Math.floor($0)", tmp);
+    }
   }
+  throw "Unimplemented user-defined ~/.";
 }
 
 eq(var a, var b) {
-  return JS(@"$0 === $1", a, b);
+  if (JS(@"typeof $0 === 'undefined'", a) ||
+      JS(@"typeof $0 === 'number'", a) ||
+      JS(@"typeof $0 === 'boolean'", a) ||
+      JS(@"typeof $0 === 'string'", a)) {
+    return JS(@"$0 === $1", a, b);
+  }
+  throw "Unimplemented user-defined ==.";
 }
 
 gt(var a, var b) {
-  return JS(@"$0 > $1", a, b);
+  if (checkNumbers(a, b, "num> expects a number as second operand.")) {
+    return JS(@"$0 > $1", a, b);
+  }
+  throw "Unimplemented user-defined binary >.";
 }
 
 ge(var a, var b) {
-  return JS(@"$0 >= $1", a, b);
+  if (checkNumbers(a, b, "num>= expects a number as second operand.")) {
+    return JS(@"$0 >= $1", a, b);
+  }
+  throw "Unimplemented user-defined binary >=.";
 }
 
 lt(var a, var b) {
-  return JS(@"$0 < $1", a, b);
+  if (checkNumbers(a, b, "num< expects a number as second operand.")) {
+    return JS(@"$0 < $1", a, b);
+  }
+  throw "Unimplemented user-defined binary <.";
 }
 
 le(var a, var b) {
-  return JS(@"$0 <= $1", a, b);
+  if (checkNumbers(a, b, "num<= expects a number as second operand.")) {
+    return JS(@"$0 <= $1", a, b);
+  }
+  throw "Unimplemented user-defined binary <=.";
 }
 
 shl(var a, var b) {
-  return JS(@"$0 << $1", a, b);
+  // TODO(floitsch): inputs must be integers.
+  if (checkNumbers(a, b, "int<< expects an int as second operand.")) {
+    return JS(@"$0 << $1", a, b);
+  }
+  throw "Unimplemented user-defined binary <<.";
 }
 
 shr(var a, var b) {
-  return JS(@"$0 >> $1", a, b);
+  // TODO(floitsch): inputs must be integers.
+  if (checkNumbers(a, b, "int>> expects an int as second operand.")) {
+    return JS(@"$0 >> $1", a, b);
+  }
+  throw "Unimplemented user-defined binary >>.";
 }
 
 and(var a, var b) {
-  return JS(@"$0 & $1", a, b);
+  // TODO(floitsch): inputs must be integers.
+  if (checkNumbers(a, b, "int& expects an int as second operand.")) {
+    return JS(@"$0 & $1", a, b);
+  }
+  throw "Unimplemented user-defined binary &.";
 }
 
 or(var a, var b) {
-  return JS(@"$0 | $1", a, b);
+  // TODO(floitsch): inputs must be integers.
+  if (checkNumbers(a, b, "int| expects an int as second operand.")) {
+    return JS(@"$0 | $1", a, b);
+  }
+  throw "Unimplemented user-defined binary |.";
 }
 
 xor(var a, var b) {
-  return JS(@"$0 ^ $1", a, b);
+  // TODO(floitsch): inputs must be integers.
+  if (checkNumbers(a, b, "int^ expects an int as second operand.")) {
+    return JS(@"$0 ^ $1", a, b);
+  }
+  throw "Unimplemented user-defined binary ^.";
 }
 
 not(var a) {
-  return JS(@"~$0", a);
+  if (JS(@"typeof $0 === 'number'", a)) return JS(@"~$0", a);
+  throw "Unimplemented user-defined ~.";
 }
 
 neg(var a) {
-  return JS(@"-$0", a);
+  if (JS(@"typeof $0 === 'number'", a)) return JS(@"-$0", a);
+  throw "Unimplemented user-defined unary-.";
 }
