f9f33f93ef0f2ae754a65b2342801c411f2b92b6
ngeoffray@google.com <ngeoffray@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-12-02 15:54:25 +0000

    Forgot that file from last commit. Review URL: http://codereview.chromium.org//8773042
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@2027 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/frog/tests/leg/src/mock_compiler.dart
@@ -0,0 +1,79 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#library('mock_compiler');
+
+#import("../../../leg/leg.dart");
+#import("../../../leg/elements/elements.dart");
+#import("../../../leg/tree/tree.dart");
+#import("../../../leg/util/util.dart");
+#import("parser_helper.dart");
+
+class WarningMessage {
+  Node node;
+  Message message;
+  WarningMessage(this.node, this.message);
+}
+
+class MockCompiler extends Compiler {
+  List warnings;
+  List errors;
+  Node parsedTree;
+
+  MockCompiler() : super(null), warnings = [], errors = [] {
+    parseScript('lt() {} add() {}');
+  }
+
+  void reportWarning(Node node, var message) {
+    warnings.add(new WarningMessage(node, message.message));
+  }
+
+  void reportError(Node node, var message) {
+    errors.add(new WarningMessage(node, message.message));
+  }
+
+  void clearWarnings() {
+    warnings = [];
+  }
+
+  void clearErrors() {
+    errors = [];
+  }
+
+  TreeElements resolveStatement(String text) {
+    parsedTree = parseStatement(text);
+    return resolveNodeStatement(parsedTree);
+  }
+
+  TreeElements resolveNodeStatement(Node tree) {
+    ResolverVisitor visitor = resolverVisitor();
+    visitor.visit(tree);
+    // Resolve the type annotations encountered in the code.
+    while (!resolver.toResolve.isEmpty()) {
+      resolver.toResolve.removeFirst().resolve(this);
+    }
+    return visitor.mapping;
+  }
+
+  resolverVisitor() {
+    Element mockElement =
+        new Element(buildSourceString(''), ElementKind.FUNCTION, null);
+    ResolverVisitor visitor = new FullResolverVisitor(this, mockElement);
+    visitor.context = new BlockScope(visitor.context);
+    return visitor;
+  }
+
+  parseScript(String text) {
+    for (Link<Element> link = parseUnit(text, this);
+         !link.isEmpty();
+         link = link.tail) {
+      universe.define(link.head);
+    }
+  }
+
+  resolve(ClassElement element) {
+    return resolver.resolveType(element);
+  }
+}
+
