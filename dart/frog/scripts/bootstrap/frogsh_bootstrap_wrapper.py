c05297ed038dab77303468b37a4d2a5839f0c3aa
sigmund@google.com <sigmund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-12-03 01:43:20 +0000

    Make ./tools/build.py not fail on a clean checkout.
    BUG= http://code.google.com/p/dart/issues/detail?id=669
    
    Review URL: http://codereview.chromium.org//8789007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@2047 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/scripts/bootstrap/frogsh_bootstrap_wrapper.py
+++ dart/frog/scripts/bootstrap/frogsh_bootstrap_wrapper.py
@@ -8,6 +8,16 @@ import sys
 HOME = os.path.dirname(os.path.realpath(__file__))
 HOME = os.path.join(HOME, os.pardir, os.pardir)
 
+FROGSH_FALLBACK = """#!/bin/sh
+echo -en "\033[31mERROR\033[0m: Building frogsh in 'debug' mode requires "
+echo "building the dart"
+echo "runtime in 'release' mode. Retry building frogsh as follows:"
+echo " > rm $0"
+echo " > ./tools/build.py -m release runtime"
+echo " > ./tools/build.py -m debug frogsh"
+exit 1
+"""
+
 sys.path.append(HOME)
 import frog
 
@@ -21,11 +31,15 @@ def main(args):
   # if js_out.find('Release') != -1:
   exit_code = frog.main(frog_args)
   if exit_code:
-    return exit_code
+    if js_out.find('Release') != -1:
+      return exit_code
+    else:
+      with open(js_out, 'w') as f:
+        f.write(FROGSH_FALLBACK)
 
   os.chmod(js_out, stat.S_IXUSR | stat.S_IXGRP | stat.S_IRUSR |
            stat.S_IRGRP | stat.S_IWUSR)
-  return exit_code
+  return 0
 
 
 if __name__ == '__main__':
