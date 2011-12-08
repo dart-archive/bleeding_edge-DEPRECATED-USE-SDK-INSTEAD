73585e74e36b836ad4e29e1fd1c0ff83fca4508d
jimhug@google.com <jimhug@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-12-08 02:22:05 +0000

    toss now works as minimal dart server
    Running toss will let you auto-compile dart code from localhost - so that you can change code in any editor and hit reload to see your changes.  Very early, but checking in so others can use and make better.
    
    Review URL: http://codereview.chromium.org//8873006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@2218 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/tip/toss.dart
+++ dart/frog/tip/toss.dart
@@ -3,34 +3,69 @@
 // BSD-style license that can be found in the LICENSE file.
 
 #import('../lib/node/node.dart');
+#import('../file_system_node.dart');
+#import('../lang.dart');
+
+String compileDart(String basename, String filename) {
+  // Get the home directory from our executable.
+  var homedir = path.dirname(fs.realpathSync(process.argv[1]));
+
+  var fs = new NodeFileSystem();
+
+  var basedir = path.dirname(basename);
+  filename = '$basedir/$filename';
+
+  if (compile(homedir, [null, null, filename], fs)) {
+    var code = world.getGeneratedCode();
+    return code;
+  } else {
+    return 'alert("compilation of $filename failed, see console for errors");';
+  }
+}
+
+
+String frogify(String filename) {
+  final s = @'<script\s+type="application/dart"(?:\s+src="(\w+.dart)")?>([\s\S]*)</script>';
+
+  final text = fs.readFileSync(filename, 'utf8');
+  final re = new RegExp(s, true, false);
+  var m = re.firstMatch(text);
+  if (m === null) return text;
+
+  var dname = m.group(1);
+  var contents = m.group(2);
+
+  print('compiling $dname');
+
+  var compiled = compileDart(filename, dname);
+
+  return text.substring(0, m.start()) +
+    '<script type="application/javascript">${compiled}</script>' +
+    text.substring(m.start() + m.group(0).length);
+}
 
 void main() {
   http.createServer((ServerRequest req, ServerResponse res) {
-    //print("Request for ${req.url} received.");
-
     var filename;
-    if (req.url == '/') {
+    if (req.url.endsWith('.html')) {
       res.setHeader('Content-Type', 'text/html');
-      filename = './tip/tip.html';
-    } else if (req.url == '/favicon.ico') {
-      res.setHeader('Content-Type', 'image/gif');
-      filename = './tip/dart_16_16.gif';
+    } else if (req.url.endsWith('.css')) {
+        res.setHeader('Content-Type', 'text/css');
     } else {
-      if (req.url.endsWith('.html')) {
-        res.setHeader('Content-Type', 'text/html');
-      } else if (req.url.endsWith('.css')) {
-          res.setHeader('Content-Type', 'text/css');
-      } else {
-        res.setHeader('Content-Type', 'text/plain');
-      }
-      filename = '../' + req.url;
+      res.setHeader('Content-Type', 'text/plain');
     }
+    filename = '../' + req.url;
 
     if (path.existsSync(filename)) {
       var stat = fs.statSync(filename);
       if (stat.isFile()) {
         res.statusCode = 200;
-        res.end(fs.readFileSync(filename, 'utf8'));
+        String data;
+        if (filename.endsWith('.html')) {
+          res.end(frogify(filename));
+        } else {
+          res.end(fs.readFileSync(filename, 'utf8'));
+        }
         return;
       }
       // TODO(jimhug): Directory listings?
