6ec3900e11e60690d64a01c40a183e7252fa9a6a
sra@google.com <sra@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-12-17 05:19:38 +0000

    Move frog tests using 'native' from tests/language to frog/tests
    These tests are critical to dart:dom, but use non-standard features so
    should not be under tests/language
    
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org//8962013
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@2559 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/tests/frog/src/NativeNamedConstructors1FrogTest.js
+++ dart/frog/tests/frog/src/NativeNamedConstructors1FrogTest.js
@@ -2,7 +2,4 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// This function models a pre-existing native class e.g. Float32Array.
-function A(value) {
-  this._x = value;
-}
+function A(x) { this._x = x; }
