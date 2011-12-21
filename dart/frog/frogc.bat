62b3eed64034fa9420f73a8408422a74e4166464
efortuna@google.com <efortuna@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2011-12-21 01:58:16 +0000

    Paths on Windows fix.
    TBR=dgrove@google.com
    Review URL: http://codereview.chromium.org//9020001
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@2681 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/frogc.bat
+++ dart/frog/frogc.bat
@@ -7,4 +7,8 @@ REM Does string have a trailing slash? If so, remove it.
 IF %SCRIPTPATH:~-1%==\ set SCRIPTPATH=%SCRIPTPATH:~0,-1%
 set LIBPATH=%SCRIPTPATH%\..\lib
 
-%SCRIPTPATH%\dart.exe --new_gen_heap_size=128 %SCRIPTPATH%\frogc.dart --no_colors --libdir=%LIBPATH% %*
+REM Frog gets confused by Window's backward slashes.
+set script=%*
+set script=%script:\=/%
+
+%SCRIPTPATH%\dart.exe --new_gen_heap_size=128 %SCRIPTPATH%\frogc.dart --no_colors --libdir=%LIBPATH% %script%
