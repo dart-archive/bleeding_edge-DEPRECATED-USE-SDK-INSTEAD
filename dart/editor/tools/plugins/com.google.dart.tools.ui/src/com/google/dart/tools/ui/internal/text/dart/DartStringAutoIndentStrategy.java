14655321497a6d6a09d805514ab7590a696a4dfe
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-01-06 17:22:07 +0000

    fix for auto indent in strings for new line Review URL: http://codereview.chromium.org//9122004
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@3046 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/dart/DartStringAutoIndentStrategy.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/dart/DartStringAutoIndentStrategy.java
@@ -1,16 +1,14 @@
 /*
  * Copyright (c) 2011, the Dart project authors.
- *
- * Licensed under the Eclipse Public License v1.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.google.dart.tools.ui.internal.text.dart;
@@ -214,9 +212,11 @@ public class DartStringAutoIndentStrategy extends DefaultIndentLineAutoEditStrat
         command.text = "\"\"" + command.text + command.text + "\"\"\""; //$NON-NLS-1$//$NON-NLS-2$
         command.caretOffset = command.offset + 3;
         command.shiftsCaret = false;
-      } else {
-        command.text = "\" +" + command.text + indentation + "\""; //$NON-NLS-1$//$NON-NLS-2$
       }
+      // do not add " + \n " in java style when user hits enter in a single line string
+      // else {
+      //  command.text = "\" +" + command.text + indentation + "\""; //$NON-NLS-1$//$NON-NLS-2$
+      // }
     } else if (command.text.length() > 1
         && preferenceStore.getBoolean(PreferenceConstants.EDITOR_ESCAPE_STRINGS)) {
       command.text = getModifiedText(command.text, indentation, delimiter);
