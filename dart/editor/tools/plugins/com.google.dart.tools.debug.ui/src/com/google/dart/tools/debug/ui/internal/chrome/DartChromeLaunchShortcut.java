2ab9567db54589be61051bbeee3cc04cc07120f7
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-01-11 19:24:07 +0000

    TBR fix editor build - missing file for cl 9168018 Review URL: http://codereview.chromium.org//9187014
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@3201 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.debug.ui/src/com/google/dart/tools/debug/ui/internal/chrome/DartChromeLaunchShortcut.java
+++ dart/editor/tools/plugins/com.google.dart.tools.debug.ui/src/com/google/dart/tools/debug/ui/internal/chrome/DartChromeLaunchShortcut.java
@@ -1,16 +1,14 @@
 /*
  * Copyright (c) 2011, the Dart project authors.
- *
- * Licensed under the Eclipse Public License v1.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.google.dart.tools.debug.ui.internal.chrome;
@@ -53,7 +51,7 @@ public class DartChromeLaunchShortcut extends AbstractLaunchShortcut {
   @Override
   protected ILaunchConfigurationType getConfigurationType() {
     ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
-    ILaunchConfigurationType type = manager.getLaunchConfigurationType(DartDebugCorePlugin.CHROME_LAUNCH_CONFIG_ID);
+    ILaunchConfigurationType type = manager.getLaunchConfigurationType(DartDebugCorePlugin.DARTIUM_LAUNCH_CONFIG_ID);
 
     return type;
   }
@@ -73,7 +71,7 @@ public class DartChromeLaunchShortcut extends AbstractLaunchShortcut {
 
     // Create and launch a new configuration
     ILaunchManager manager = DebugPlugin.getDefault().getLaunchManager();
-    ILaunchConfigurationType type = manager.getLaunchConfigurationType(DartDebugCorePlugin.CHROME_LAUNCH_CONFIG_ID);
+    ILaunchConfigurationType type = manager.getLaunchConfigurationType(DartDebugCorePlugin.DARTIUM_LAUNCH_CONFIG_ID);
     ILaunchConfigurationWorkingCopy launchConfig = null;
     try {
       launchConfig = type.newInstance(null, resource.getName());
