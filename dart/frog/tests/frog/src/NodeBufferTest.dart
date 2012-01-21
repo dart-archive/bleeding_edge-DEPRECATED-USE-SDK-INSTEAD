b8a8788680dcd317546252711c84501f17c49237
jimhug@google.com <jimhug@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-01-21 14:48:27 +0000

    Revert r3471 and r3472
    Review URL: https://chromiumcodereview.appspot.com//9226043
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@3475 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/tests/frog/src/NodeBufferTest.dart
+++ dart/frog/tests/frog/src/NodeBufferTest.dart
@@ -7,13 +7,6 @@
 #import('unittest_node.dart');
 #import('../../../lib/node/node.dart');
 
-void compareBuffers(Buffer a, Buffer b) {
-  Expect.equals(a.length, b.length);
-  for (int i = 0; i < a.length; i++) {
-    Expect.equals(a[i], b[i]);
-  }
-}
-
 main() {
   group('Node Buffer', () {
       test('constructor', () {
@@ -44,7 +37,7 @@ main() {
       test('charsWritten', () {
           Buffer b = new Buffer(4);
           int bytesWritten = b.write('é', 0, 4, 'utf8');
-          int charsWritten = Buffers.charsWritten;
+          int charsWritten = b.charsWritten;
           Expect.equals(2, bytesWritten);
           Expect.equals(1, charsWritten);
         });
@@ -70,12 +63,12 @@ main() {
       test('isBuffer', () {
           Buffer b = new Buffer(3);
           var o = [3];
-          Expect.equals(true, Buffers.isBuffer(b));
-          Expect.equals(false, Buffers.isBuffer(o));
+          Expect.equals(true, Buffer.isBuffer(b));
+          Expect.equals(false, Buffer.isBuffer(o));
       });
       test('byteLength', () {
-          Expect.equals(1, Buffers.byteLength('e'));
-          Expect.equals(2, Buffers.byteLength('é'));
+          Expect.equals(1, Buffer.byteLength('e'));
+          Expect.equals(2, Buffer.byteLength('é'));
       });
       test('length', () {
           Expect.equals(1, new Buffer.fromString('e').length);
@@ -135,97 +128,10 @@ main() {
           Expect.equals('aAc', a.toString('utf8', 0, 3));
       });
       test('INSPECT_MAX_BYTES', () {
-          int oldVal = Buffers.INSPECT_MAX_BYTES;
-          Buffers.INSPECT_MAX_BYTES = 77;
-          Expect.equals(77, Buffers.INSPECT_MAX_BYTES);
-          Buffers.INSPECT_MAX_BYTES = oldVal;
+          int oldVal = Buffer.INSPECT_MAX_BYTES;
+          Buffer.INSPECT_MAX_BYTES = 77;
+          Expect.equals(77, Buffer.INSPECT_MAX_BYTES);
+          Buffer.INSPECT_MAX_BYTES = oldVal;
       });
-      group('List<num>', () {
-        test('indexOf', () {
-            Buffer a = new Buffer.fromString('abcb');
-            Expect.equals(-1, a.indexOf(17));
-            Expect.equals(1, a.indexOf(98));
-            Expect.equals(3, a.indexOf(98, 2));
-        });
-        test('lastIndexOf', () {
-            Buffer a = new Buffer.fromString('abcb');
-            Expect.equals(-1, a.lastIndexOf(17));
-            Expect.equals(3, a.lastIndexOf(98));
-            Expect.equals(1, a.lastIndexOf(98, 2));
-        });
-        test('last', () {
-            Buffer a = new Buffer.fromString('abc');
-            Expect.equals(99, a.last());
-        });
-        test('getRange', () {
-            Buffer a = new Buffer.fromString('abcb');
-            Buffer b = a.getRange(1,2);
-            compareBuffers(b, new Buffer.fromString('bc'));
-        });
-        test('sort', () {
-            Buffer a = new Buffer.fromString('the quick brown fox jumped over the lazy dog');
-            Buffer b = new Buffer.fromString('        abcddeeeefghhijklmnoooopqrrttuuvwxyz');
-            a.sort((a,b) => a < b ? -1 : ((a == b) ? 0 : 1));
-            compareBuffers(b, a);
-        });
-        test('forEach', () {
-            Buffer a = new Buffer.fromList([0, 1, 2, 3]);
-            int expected = 0;
-            a.forEach((e) {
-              Expect.equals(expected, e);
-              expected++;
-            });
-            Expect.equals(a.length, expected);
-        });
-        test('filter', () {
-            Buffer a = new Buffer.fromList([0, 1, 2, 3]);
-            int expected = 0;
-            Buffer b = a.filter((e) {
-              Expect.equals(expected, e);
-              expected++;
-              return (e & 1) == 0;
-            });
-            Expect.equals(a.length, expected);
-            compareBuffers(new Buffer.fromList([0, 2]), b);
-        });
-        test('map', () {
-            Buffer a = new Buffer.fromList([10, 11, 12, 13]);
-            int expected = 10;
-            int count = 0;
-            Buffer b = a.map((e) {
-              Expect.equals(expected, e);
-              expected++;
-              count++;
-              return e * 2;
-            });
-            Expect.equals(a.length, count);
-            compareBuffers(new Buffer.fromList([20, 22, 24, 26]), b);
-        });
-        test('every', () {
-            Buffer a = new Buffer.fromList([0, 1, 2, 3]);
-            Expect.equals(true, a.every((e) => e >= 0));
-            Expect.equals(false, a.every((e) => e < 3));
-        });
-        test('some', () {
-            Buffer a = new Buffer.fromList([0, 1, 2, 3]);
-            Expect.equals(true, a.some((e) => e == 3));
-            Expect.equals(false, a.some((e) => e == 4));
-        });
-        test('isEmpty', () {
-            Expect.equals(true, (new Buffer(0)).isEmpty());
-            Expect.equals(false, (new Buffer(1)).isEmpty());
-        });
-        test('iterator', () {
-          Buffer a = new Buffer.fromList([0, 1, 2, 3]);
-          int expected = 0;
-          Iterator i = a.iterator();
-          while (i.hasNext()) {
-            var e = i.next();
-            Expect.equals(expected, e);
-            expected++;
-          }
-          Expect.equals(a.length, expected);
-        });
-    });
   });
 }
