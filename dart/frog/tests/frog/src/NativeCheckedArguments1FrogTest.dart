2b8dd6fe191ac33f577627fedc020d56dc168ec5
kasperl@google.com <kasperl@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-01-31 09:16:47 +0000

    Update the status file to make sure we only fail the test in checked mode.
    R=ngeoffray@google.com
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//9233051
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@3713 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/tests/frog/src/NativeCheckedArguments1FrogTest.dart
+++ dart/frog/tests/frog/src/NativeCheckedArguments1FrogTest.dart
@@ -24,7 +24,7 @@ A.prototype.cmp = function (x) { return 0; };
 
 function B() {}
 B.prototype.foo = function (x) { return x + 'ha!'; };
-B.prototype.cmp = function (x) { return 0; };
+B.prototype.cmp = function (x) { return 1; };
 
 makeA = function(){return new A;};
 makeB = function(){return new B;};
@@ -55,13 +55,65 @@ checkedModeTest() {
   expectThrows(() => a.cmp(b));
   expectThrows(() => a.cmp(5));
 
-  Expect.equals(0, b.cmp(b));
+  Expect.equals(1, b.cmp(b));
   expectThrows(() => b.cmp(a));
   expectThrows(() => b.cmp(5));
+
+  // Check that we throw the same errors when the locals are typed.
+  A aa = things[0];
+  B bb = things[1];
+
+  Expect.equals(124, aa.foo(123));
+  expectThrows(() => aa.foo('xxx'));
+
+  Expect.equals('helloha!', bb.foo('hello'));
+  expectThrows(() => bb.foo(123));
+
+  Expect.equals(0, aa.cmp(aa));
+  expectThrows(() => aa.cmp(bb));
+  expectThrows(() => aa.cmp(5));
+
+  Expect.equals(1, bb.cmp(bb));
+  expectThrows(() => bb.cmp(aa));
+  expectThrows(() => bb.cmp(5));
 }
 
 uncheckedModeTest() {
-  // TODO. Verify unchecked behaviour.
+  var things = [makeA(), makeB()];
+  var a = things[0];
+  var b = things[1];
+
+  Expect.equals(124, a.foo(123));
+  Expect.equals('xxx1', a.foo('xxx'));
+
+  Expect.equals('helloha!', b.foo('hello'));
+  Expect.equals('123ha!', b.foo(123));
+
+  Expect.equals(0, a.cmp(a));
+  Expect.equals(0, a.cmp(b));
+  Expect.equals(0, a.cmp(5));
+
+  Expect.equals(1, b.cmp(b));
+  Expect.equals(1, b.cmp(a));
+  Expect.equals(1, b.cmp(5));
+
+  // Check that we do not throw errors when the locals are typed.
+  A aa = things[0];
+  B bb = things[1];
+
+  Expect.equals(124, aa.foo(123));
+  Expect.equals('xxx1', aa.foo('xxx'));
+
+  Expect.equals('helloha!', bb.foo('hello'));
+  Expect.equals('123ha!', bb.foo(123));
+
+  Expect.equals(0, aa.cmp(aa));
+  Expect.equals(0, aa.cmp(bb));
+  Expect.equals(0, aa.cmp(5));
+
+  Expect.equals(1, bb.cmp(bb));
+  Expect.equals(1, bb.cmp(aa));
+  Expect.equals(1, bb.cmp(5));
 }
 
 bool isCheckedMode() {
