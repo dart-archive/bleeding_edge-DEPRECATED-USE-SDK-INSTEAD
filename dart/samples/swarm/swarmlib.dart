77f296fc5bb5d18be0daf8787a5ad2631c491ea0
dgrove@google.com <dgrove@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-01-31 22:50:10 +0000

    Finish moving client/samples/ to samples/ . Review URL: http://codereview.chromium.org//9316008
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@3758 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/samples/swarm/swarmlib.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#library("swarmlib");
+
+#import('dart:coreimpl');
+#import('dart:json');
+#import('dart:html');
+#import('../../client/base/base.dart');
+#import('../../client/view/view.dart');
+#import('../../client/observable/observable.dart');
+#import('../../client/touch/touch.dart');
+#import('../../client/util/utilslib.dart');
+
+#source('App.dart');
+#source('BiIterator.dart');
+#source('ConfigHintDialog.dart');
+#source('HelpDialog.dart');
+#source('SwarmState.dart');
+#source('SwarmViews.dart');
+#source('SwarmApp.dart');
+#source('DataSource.dart');
+#source('Decoder.dart');
+#source('UIState.dart');
+#source('Views.dart');
+#source('CSS.dart');
+
+// TODO(jimhug): Remove this when deploying.
+#source('CannedData.dart');
