fd8365b13ac1195d51e3ec2475c3249eef7f42ab
efortuna@google.com <efortuna@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-02-17 00:42:19 +0000

    Run VM without type checks on Builbots for browser tests. Review URL: https://chromiumcodereview.appspot.com//9414026
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@4344 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/scripts/bootstrap/frog_bootstrap_wrapper.py
+++ dart/frog/scripts/bootstrap/frog_bootstrap_wrapper.py
@@ -8,6 +8,7 @@ import sys
 
 HOME = os.path.dirname(os.path.realpath(__file__))
 HOME = os.path.join(HOME, os.pardir, os.pardir)
+BUILDER_NAME = os.environ.get('BUILDBOT_BUILDERNAME')
 
 END_SCRIPT = '''
 VM = '%s'
@@ -29,13 +30,16 @@ def main(args):
   shutil.copy(os.path.join(HOME, 'scripts', 'bootstrap', 'frog.bat'),
               frog + '.bat')
 
-  if 'Release' in product_dir:
+  if 'Release' in product_dir and \
+    (BUILDER_NAME == None or 'web' not in BUILDER_NAME):
     # We want to run our tests on the VM in production mode and in
     # developer mode. It turns out that developer mode is really slow
     # on the debug VM, so it makes sense to make the pairings:
     # (release VM, developer mode) and (debug VM, production mode).
     vm_flags = '--vm_flags=--enable_asserts --enable_type_checks'
   else:
+    # For test turnaround time, we run fully in release mode on 
+    # our web browser buildbots (and with debug VM).
     vm_flags = ''
 
   with open(frog, 'a+') as f:
