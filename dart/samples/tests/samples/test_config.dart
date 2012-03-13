f31c6f71006dcf05f996b5579f56a98e2d827823
ahe@google.com <ahe@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-03-13 08:51:00 +0000

    Don't skip tests.
    Review URL: https://chromiumcodereview.appspot.com//9691035
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@5385 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/tests/samples/test_config.dart
+++ dart/samples/tests/samples/test_config.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -11,7 +11,8 @@ class SamplesTestSuite extends StandardTestSuite {
       : super(configuration,
               "samples",
               "samples/tests/samples/src",
-              ["samples/tests/samples/samples.status"]);
+              ["samples/tests/samples/samples.status",
+               "samples/tests/samples/samples-leg.status"]);
 
   bool listRecursively() => true;
 }
