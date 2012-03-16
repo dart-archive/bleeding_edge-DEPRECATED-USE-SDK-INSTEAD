30696731192abf27b3c2b81b99413c7417b725f4
messick@google.com <messick@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-03-16 16:28:56 +0000

    Clean up mark occurrences a bit for the demo. Review URL: https://chromiumcodereview.appspot.com//9706104
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@5574 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/utilities/ast/NameOccurrencesFinder.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/utilities/ast/NameOccurrencesFinder.java
@@ -1,10 +1,13 @@
 package com.google.dart.tools.core.utilities.ast;
 
+import com.google.dart.compiler.ast.ASTVisitor;
+import com.google.dart.compiler.ast.DartDeclaration;
+import com.google.dart.compiler.ast.DartExpression;
 import com.google.dart.compiler.ast.DartFunction;
 import com.google.dart.compiler.ast.DartFunctionTypeAlias;
+import com.google.dart.compiler.ast.DartInvocation;
 import com.google.dart.compiler.ast.DartMethodDefinition;
 import com.google.dart.compiler.ast.DartNode;
-import com.google.dart.compiler.ast.ASTVisitor;
 import com.google.dart.compiler.ast.DartParameter;
 import com.google.dart.compiler.resolver.Element;
 import com.google.dart.compiler.resolver.VariableElement;
@@ -31,6 +34,30 @@ public class NameOccurrencesFinder extends ASTVisitor<Void> {
     this.ast = ast;
     DartNode container = findTargetContainer();
     container.accept(this);
+  }
+
+  @Override
+  public Void visitDeclaration(DartDeclaration<?> node) {
+    if (node.getElement() == target) {
+      DartNode name = node.getName();
+      if (name != null) {
+        matches.add(name);
+        return null;
+      }
+    }
+    return super.visitDeclaration(node);
+  }
+
+  @Override
+  public Void visitInvocation(DartInvocation node) {
+    if (node.getElement() == target) {
+      DartExpression target = node.getTarget();
+      if (target != null) {
+        matches.add(target);
+        return null;
+      }
+    }
+    return super.visitInvocation(node);
   }
 
   @Override
