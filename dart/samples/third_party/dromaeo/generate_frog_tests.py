f812deaeba33ad4680bc874c5b4c711791567076
vsm@google.com <vsm@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-03-28 23:50:55 +0000

    Invoke frog directly for Dromaeo build
    Bypass htmlconvertor and invoke frog directly.  At some point, I'd
    like to get dart.js to do the heavy lifting, but this is an expedient
    first cut.
    
    Jacob: this will make the comparisons to js more meaningful.  htmlconvertor was reformating the html and that seemed to affect perf in subtle ways.  Here, everything should be the same besides the script tag.
    
    Review URL: https://chromiumcodereview.appspot.com//9905001
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@5963 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/third_party/dromaeo/generate_frog_tests.py
+++ dart/samples/third_party/dromaeo/generate_frog_tests.py
@@ -3,14 +3,34 @@
 import glob
 import os
 import os.path
+import re
 import subprocess
+import sys
+
+SAMPLES_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DART_PATH = os.path.dirname(SAMPLES_PATH)
+TOOLS_PATH = os.path.join(DART_PATH, 'tools')
+
+sys.path.append(TOOLS_PATH)
+import utils
+
+def Compile(source, target):
+  binary = os.path.abspath(os.path.join(DART_PATH,
+                                        utils.GetBuildRoot(utils.GuessOS(),
+                                                           'release', 'ia32'),
+                                        'frog', 'bin', 'frogsh'))
+
+  cmd = [binary, '--compile-only',
+         '--libdir=' + os.path.join(DART_PATH, 'frog', 'lib'),
+         '--out=' + target]
+  cmd.append(source)
+  print 'Executing: ' + ' '.join(cmd)
+  subprocess.call(cmd)
 
 def HtmlConvert(infile):
   (head, tail) = os.path.split(infile)
-  htmlconverter = os.path.join('..', '..', '..', 'client',
-                               'tools', 'htmlconverter.py')
+
   if head == 'tests':
-    htmlconverter = os.path.join('..', htmlconverter)
     outdir = 'frog'
     os.chdir('tests')
   elif head == '':
@@ -18,9 +38,23 @@ def HtmlConvert(infile):
   else:
     raise 'Illegal input: ' + infile
 
-  cmd = ['python', htmlconverter, tail, '-o', outdir]
-  print 'Executing: ' + ' '.join(cmd)
-  subprocess.call(cmd)
+  pattern = r'<script type="application/dart" src="([\w-]+).dart">'
+  infile = open(tail, 'r')
+  outfilename = os.path.join(outdir, tail.replace('.html', '-js.html'))
+  outfile = open(outfilename, 'w')
+
+  print 'Converting %s to %s' % (tail, outfilename)
+  for line in infile:
+    result = re.search(pattern, line)
+    if result:
+      dartname = result.group(1) + '.dart'
+      jsname = os.path.join(outdir, dartname + '.js')
+      Compile(dartname, jsname)
+      script = '<script type="text/javascript" src="%s">' % (dartname + '.js')
+      outfile.write(re.sub(pattern, script, line))
+    else:
+      outfile.write(line)
+
   if head == 'tests':
     os.chdir('..')
 
