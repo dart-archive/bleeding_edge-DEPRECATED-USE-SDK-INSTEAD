3bb97ab85d79e28301dd0af99461416394aaf97b
zundel@google.com <zundel@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-04-03 23:06:42 +0000

    Metrics package copied over from dartc
    Review URL: https://chromiumcodereview.appspot.com//9959123
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@6151 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/metrics/DartEventType.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2012, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.engine.metrics;
+
+import com.google.dart.engine.metrics.Tracer.EventType;
+
+/**
+ * Dart events for SpeedTracer.
+ */
+public enum DartEventType implements EventType {
+  ADD_OUTOFDATE("MistyRose"), BUILD_LIB_SCOPES("violet"), COMPILE("green"), //
+  COMPILE_APP("gray"), COMPILE_LIBRARIES("brown"), EXEC_PHASE("blue"), //
+  IMPORT_EMBEDDED_LIBRARIES("purple"), IS_SOURCE_OUTOFDATE("Chartreuse"), //
+  SCANNER("GoldenRod"), PARSE("red"), PARSE_OUTOFDATE("LightCoral"), //
+  RESOLVE_LIBRARIES("black"), TIMESTAMP_OUTOFDATE("LightSteelBlue"), //
+  UPDATE_LIBRARIES("yellow"), UPDATE_RESOLVE("orange"), WRITE_METRICS("LightChiffon");
+
+  final String cssColor;
+  final String name;
+
+  DartEventType(String cssColor) {
+    this(null, cssColor);
+  }
+
+  DartEventType(String name, String cssColor) {
+    this.name = name;
+    this.cssColor = cssColor;
+  }
+
+  @Override
+  public String getColor() {
+    return cssColor;
+  }
+
+  @Override
+  public String getName() {
+    return name == null ? toString() : name;
+  }
+}
