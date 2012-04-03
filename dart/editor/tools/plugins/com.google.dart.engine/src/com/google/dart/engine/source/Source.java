c1940efd5d8489ed6216848213f73a36c6867cdc
brianwilkerson@google.com <brianwilkerson@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-04-03 20:14:19 +0000

    First cut at a revised interface for Source
    This is primarily intended to be a starting point for discussion. I believe the current implementation of source objects is too complex and too error prone, so this is an attempt to simplify things.
    
    (I am addressing this now because I need it to add support for compilation errors.)
    Review URL: https://chromiumcodereview.appspot.com//9959102
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@6144 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/source/Source.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2012, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.engine.source;
+
+import java.io.File;
+
+/**
+ * The interface <code>Source</code> defines the behavior of objects representing source code that
+ * can be compiled.
+ */
+public interface Source {
+  /**
+   * Return the name that should be displayed to users when discussing this source.
+   * 
+   * @return the name that should be displayed to users when discussing this source
+   */
+  public String getDisplayName();
+
+  /**
+   * Return the file represented by this source, or <code>null</code> if this source does not exist.
+   * 
+   * @return the file represented by this source
+   */
+  public File getFile();
+
+  /**
+   * Return <code>true</code> if this source is in one of the system libraries.
+   * 
+   * @return <code>true</code> if this is in a system library
+   */
+  public boolean isInSystemLibrary();
+}
