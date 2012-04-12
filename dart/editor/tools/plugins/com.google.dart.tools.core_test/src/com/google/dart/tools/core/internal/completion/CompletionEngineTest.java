5766ca19436dcfc0478be4ca07cc10c989661b0b
zundel@google.com <zundel@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-04-12 13:44:20 +0000

    Fix flaxy test issue for analysis parser found in r6414
    http://code.google.com/p/dart/source/detail?r=6414
    
    Review URL: https://chromiumcodereview.appspot.com//10069006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@6462 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/internal/completion/CompletionEngineTest.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/internal/completion/CompletionEngineTest.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2012, the Dart project authors.
- * 
+ *
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -199,10 +199,9 @@ public class CompletionEngineTest extends TestCase {
     test("t3() {var x=new List!1}", "1+List");
   }
 
-  // TODO(zundel): investigate intermittent parse error
-//  public void testCommentSnippets037() throws Exception {
-//    test("t3() {var x=new List.!1}", "1+from");
-//  }
+  public void testCommentSnippets037() throws Exception {
+    test("t3() {var x=new List.!1}", "1+from");
+  }
 
   public void testCommentSnippets038() throws Exception {
     test("int xa; String s = '$x!1'", "1+xa");
@@ -337,7 +336,7 @@ public class CompletionEngineTest extends TestCase {
    * character prefix. The first character of the prefix corresponds to an X in the
    * <code>originalSource</code>. The second character is either a '+' or a '-' indicating whether
    * the string is a positive or negative result.
-   * 
+   *
    * @param originalSource The source for a completion test that contains completion points
    * @param validationStrings The positive and negative predictions
    */
