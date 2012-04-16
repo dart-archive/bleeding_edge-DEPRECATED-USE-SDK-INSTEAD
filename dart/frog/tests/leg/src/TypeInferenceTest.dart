c97b8f61b274f484ac6dc37e180d39b7e3b833cc
floitsch@google.com <floitsch@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-04-16 20:06:00 +0000

    Revert "Refactor type propagation."
    This reverts commit r6599.
    
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//10106004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@6600 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/tests/leg/src/TypeInferenceTest.dart
+++ dart/frog/tests/leg/src/TypeInferenceTest.dart
@@ -49,8 +49,14 @@ main(a) {
 
 main() {
   String generated = compile(TEST_ONE, 'sum');
+  RegExp regexp = new RegExp("i = \\(?$anyIdentifier \\+ \\(1\\)\\)?");
+  // TODO(ngeoffray): Do live range analysis to make this test pass.
+  Expect.isFalse(regexp.hasMatch(generated));
 
-  Regexp regexp = new RegExp("sum = \\(?$anyIdentifier \\+ $anyIdentifier\\)?");
+  regexp = new RegExp("sum = \\(?$anyIdentifier \\+ $anyIdentifier\\)?");
+  Expect.isTrue(regexp.hasMatch(generated));
+
+  regexp = const RegExp("typeof param0 !== 'number'");
   Expect.isTrue(regexp.hasMatch(generated));
 
   regexp = const RegExp("typeof param1 !== 'number'");
