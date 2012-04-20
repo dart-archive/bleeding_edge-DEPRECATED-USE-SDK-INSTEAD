082f50ed1cc6e8fcc23b40526c0849bae9cab125
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-04-20 18:34:58 +0000

    Copyright cleanup phase 1 (the Java edition).
    This query identified the suspects:
    
    find . -type f  -exec grep -L "Dart project authors."  {} \; | grep -v \.svn | grep -v \.gif | grep -v \.png | grep -v \.exsd | grep -v README | grep -v \.properties | grep -v \.xml | grep -v \.jar | grep -v \.classpath | grep -v \.options | grep -v \.prefs | grep -v \.zip | grep -v \.project | grep -v \.txt | grep -v MANIFEST.MF | grep -v third_party |  grep -v \.ic[on] | grep -v \.tmpl | grep -v \.css | grep -v \.psd | grep -v \.jpg | grep -v \.svg | grep -v \.gitignore | grep -v \.class
    
    (Many of these could be fixed (the ones missing a copyright altogether) using a search/replace in eclipse matching files starting with package (\Apackage).  Straight-forward search/replace was used for the cases where the copyright details were wrong (e.g., refering to Google vs. Dart).)
    
    NOTE: a bunch of config files are missing copyrights as well.  Fixes for those to follow.
    Review URL: https://chromiumcodereview.appspot.com//10164008
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@6806 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/filesview/ResourceContentProvider.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/filesview/ResourceContentProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Google Inc.
+ * Copyright (c) 2012, the Dart project authors.
  * 
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -11,7 +11,6 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package com.google.dart.tools.ui.internal.filesview;
 
 import org.eclipse.core.filesystem.EFS;
