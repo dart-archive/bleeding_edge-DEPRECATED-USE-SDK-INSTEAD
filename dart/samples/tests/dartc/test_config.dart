1568eb931093a92dda957d2a23ea8556b95e4b90
sigmund@google.com <sigmund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-05-02 22:55:51 +0000

    reenable some dartc tests that have been disabled accidentally. Move client/dartc/ to samples/dartc/.
    Review URL: https://chromiumcodereview.appspot.com//10343008
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@7262 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/samples/tests/dartc/test_config.dart
@@ -0,0 +1,51 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#library("client_dartc_test_config");
+
+#import("../../../tools/testing/dart/test_suite.dart");
+
+class SamplesDartcTestSuite extends DartcCompilationTestSuite {
+  SamplesDartcTestSuite(Map configuration)
+      : super(configuration,
+              'dartc',
+              'samples',
+              [ 'actors',
+                'belay',
+                'calculator',
+                'chat',
+                'clock',
+                'dartcombat',
+                'hi',
+                'isolate',
+                'isolate_html',
+                'leap',
+                'logo',
+                'markdown',
+                'matrix',
+                'pond',
+                'proxy',
+                'sample_extension',
+                'slider',
+                'spirodraw',
+                'sunflower',
+                'swarm',
+                'tests',
+                'third_party',
+                'time',
+                'total',
+                'ui_lib',
+              ],
+              ['samples/tests/dartc/dartc.status']);
+
+  bool isTestFile(String filename) {
+    if (!filename.endsWith(".dart")) return false;
+    // Using readOptionsFromFile here causes the file to be read twice,
+    // because readOptionsFromFile is called again in the superclass.
+    // Avoid this in new code.
+    return readOptionsFromFile(filename)["containsLeadingHash"];
+  }
+
+  bool listRecursively() => true;
+}
