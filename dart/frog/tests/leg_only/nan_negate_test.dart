a4c4ead92ddb7a1e3a9e0eb70995f689a37aeda8
lrn@google.com <lrn@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-05-10 13:10:15 +0000

    Fix bug in NaN-related test.
    Not-equality isn't one of the operations that always yield false on NaN.
    
    Review URL: https://chromiumcodereview.appspot.com//10383105
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@7487 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/tests/leg_only/nan_negate_test.dart
+++ dart/frog/tests/leg_only/nan_negate_test.dart
@@ -2,8 +2,60 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Don't convert !(a op b) til (a neg-op b) when a or b might be NaN.
-main() {
+// Don't convert !(a op b) to (a neg-op b) when a or b might be NaN.
+test(double n) {
+  // Force known type to double, preserves NaN.
+  n = 0.0 + n;
+  Expect.isFalse(n >= 0);
+  Expect.isTrue(!(n < 0));
+
+  Expect.isFalse(n <= 0);
+  Expect.isTrue(!(n > 0));
+
+  Expect.isFalse(n < 0);
+  Expect.isTrue(!(n >= 0));
+
+  Expect.isFalse(n > 0);
+  Expect.isTrue(!(n <= 0));
+
+  Expect.isFalse(n == 0);
+  Expect.isFalse(!(n != 0));
+
+  Expect.isTrue(n != 0);
+  Expect.isTrue(!(n == 0));
+
+  Expect.isFalse(n === 0);
+  Expect.isFalse(!(n !== 0));
+
+  Expect.isTrue(n !== 0);
+  Expect.isTrue(!(n === 0));
+
+  Expect.isFalse(0 >= n);
+  Expect.isTrue(!(0 < n));
+
+  Expect.isFalse(0 <= n);
+  Expect.isTrue(!(0 > n));
+
+  Expect.isFalse(0 < n);
+  Expect.isTrue(!(0 >= n));
+
+  Expect.isFalse(0 > n);
+  Expect.isTrue(!(0 <= n));
+
+  Expect.isFalse(0 == n);
+  Expect.isFalse(!(0 != n));
+
+  Expect.isTrue(0 != n);
+  Expect.isTrue(!(0 == n));
+
+  Expect.isFalse(0 === n);
+  Expect.isFalse(!(0 !== n));
+
+  Expect.isTrue(0 !== n);
+  Expect.isTrue(!(0 === n));
+}
+
+testConstant() {
   Expect.isFalse(double.NAN >= 0);
   Expect.isTrue(!(double.NAN < 0));
 
@@ -17,16 +69,16 @@ main() {
   Expect.isTrue(!(double.NAN <= 0));
 
   Expect.isFalse(double.NAN == 0);
-  Expect.isTrue(!(double.NAN != 0));
+  Expect.isFalse(!(double.NAN != 0));
 
-  Expect.isFalse(double.NAN != 0);
+  Expect.isTrue(double.NAN != 0);
   Expect.isTrue(!(double.NAN == 0));
 
   Expect.isFalse(double.NAN === 0);
   Expect.isFalse(!(double.NAN !== 0));
 
-  Expect.isFalse(double.NAN !== 0);
-  Expect.isFalse(!(double.NAN === 0));
+  Expect.isTrue(double.NAN !== 0);
+  Expect.isTrue(!(double.NAN === 0));
 
   Expect.isFalse(0 >= double.NAN);
   Expect.isTrue(!(0 < double.NAN));
@@ -41,14 +93,25 @@ main() {
   Expect.isTrue(!(0 <= double.NAN));
 
   Expect.isFalse(0 == double.NAN);
-  Expect.isTrue(!(0 != double.NAN));
+  Expect.isFalse(!(0 != double.NAN));
 
-  Expect.isFalse(0 != double.NAN);
+  Expect.isTrue(0 != double.NAN);
   Expect.isTrue(!(0 == double.NAN));
 
   Expect.isFalse(0 === double.NAN);
   Expect.isFalse(!(0 !== double.NAN));
 
-  Expect.isFalse(0 !== double.NAN);
-  Expect.isFalse(!(0 === double.NAN));
+  Expect.isTrue(0 !== double.NAN);
+  Expect.isTrue(!(0 === double.NAN));
+}
+
+main() {
+  test(hideConstant(27, double.NAN));
+  testConstant();
+}
+
+double hideConstant(int n, double result) {
+  if (n == 1) return result;
+  if ((n & 1) == 0) return hideConstant(n >> 1, result);
+  return hideConstant(3 * n + 1, result);
 }
