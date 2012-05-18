ed483bbdfd97a574469d9f0e97d31cbdc9aa754f
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-05-18 21:55:56 +0000

    TBR disabling an intermittent test Review URL: https://chromiumcodereview.appspot.com//10408033
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@7770 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/utilities/ast/TestAll.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/utilities/ast/TestAll.java
@@ -19,7 +19,8 @@ import junit.framework.TestSuite;
 public class TestAll {
   public static Test suite() {
     TestSuite suite = new TestSuite("Tests in " + TestAll.class.getPackage().getName());
-    suite.addTestSuite(DartElementLocatorTest.class);
+    // TODO(devoncarew): this test fails intermittently - re-enable when it is fixed.
+    //suite.addTestSuite(DartElementLocatorTest.class);
     return suite;
   }
 }
