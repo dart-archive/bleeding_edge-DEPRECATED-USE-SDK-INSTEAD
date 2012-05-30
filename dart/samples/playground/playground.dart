d09bd9dede074423cf0bb81a640dda07f18208c9
vsm@google.com <vsm@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-05-30 21:54:05 +0000

    Add Dartium Playground as sample
    Review URL: https://chromiumcodereview.appspot.com//10447095
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8129 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/samples/playground/playground.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#import('dart:html');
+
+void run() {
+  // Get user script.
+  final textarea = document.query('#code');
+  final text = textarea.value;
+
+  // Clear previous output.
+  final output = document.query('#output');
+  output.innerHTML = '';
+
+  // Run user script in new iframe.
+  final iframe = new IFrameElement();
+  iframe.height = '200';
+  iframe.width = '100%';
+  iframe.src = '''data:text/html,
+<html>
+  <body>
+    <script type="application/dart">
+${text.replaceAll('\n', '%0A')}
+    </script>
+    <script>window.navigator.webkitStartDart();</script>
+  </body>
+</html>
+''';
+  output.nodes.add(iframe);
+}
+
+void main() {
+  final button = document.query('#button');
+  button.on.click.add((e) => run(), false);
+}
