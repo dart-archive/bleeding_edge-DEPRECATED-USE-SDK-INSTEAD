b45ea9c24a69379fb4c4439ca6b1364d153edb34
ager@google.com <ager@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-06-04 15:46:56 +0000

    Change test to account for new treatment of updating definitions.
    R=ngeoffray@google.com
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//10514004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8252 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/frog/tests/leg/ssa_phi_codegen_test.dart
+++ dart/frog/tests/leg/ssa_phi_codegen_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 // Test that parameters keep their names in the output.
@@ -66,10 +66,10 @@ main() {
   Expect.isTrue(regexp.hasMatch(generated));
 
   generated = compile(TEST_TWO, 'main');
-  regexp = new RegExp("t = \\(?$anyIdentifier +");
+  regexp = new RegExp("t \\+= 10");
   Expect.isTrue(regexp.hasMatch(generated));
 
-  regexp = new RegExp("i = \\(?$anyIdentifier +");
+  regexp = new RegExp("\\+\\+i");
   Expect.isTrue(regexp.hasMatch(generated));
 
   generated = compile(TEST_THREE, 'foo');
