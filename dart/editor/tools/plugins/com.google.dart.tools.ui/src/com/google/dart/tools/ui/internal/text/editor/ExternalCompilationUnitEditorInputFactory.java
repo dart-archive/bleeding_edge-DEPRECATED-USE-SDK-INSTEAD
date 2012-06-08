7709b1a557183be543b48bb03062193c2c7a7014
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-06-08 21:45:36 +0000

    Fix to ensure that editors open on external (non-bundled) files can be reconstituted on restart. Review URL: https://chromiumcodereview.appspot.com//10535090
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8469 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/editor/ExternalCompilationUnitEditorInputFactory.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/editor/ExternalCompilationUnitEditorInputFactory.java
@@ -1,16 +1,14 @@
 /*
- * Copyright (c) 2011, the Dart project authors.
- *
- * Licensed under the Eclipse Public License v1.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * Copyright (c) 2012, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.google.dart.tools.ui.internal.text.editor;
@@ -77,6 +75,10 @@ public class ExternalCompilationUnitEditorInputFactory implements IElementFactor
       try {
         ExternalCompilationUnitImpl unit = DartModelManager.getInstance().getDartModel().getBundledCompilationUnit(
             unitUri);
+        // if we can't find a bundled compilation unit, fall back to a filestore
+        if (unit == null) {
+          return new FileStoreEditorInput(EFS.getStore(fileUri));
+        }
         return new ExternalCompilationUnitEditorInput(EFS.getStore(fileUri), unit);
       } catch (DartModelException exception) {
         return new FileStoreEditorInput(EFS.getStore(fileUri));
