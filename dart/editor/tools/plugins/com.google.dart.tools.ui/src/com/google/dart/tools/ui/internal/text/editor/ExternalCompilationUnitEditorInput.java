95e4e113e9f598662a02347b57c8d60863fda71e
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-06-08 23:10:44 +0000

    Fix to ensure that external resources are treated as read-only.
    This is an old chesnut.  See: dartbug.com/3429 for some recent context, motivation and user woes.
    Review URL: https://chromiumcodereview.appspot.com//10536079
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8474 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/editor/ExternalCompilationUnitEditorInput.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/editor/ExternalCompilationUnitEditorInput.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, the Dart project authors.
+ * Copyright (c) 2012, the Dart project authors.
  * 
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -69,8 +69,8 @@ public class ExternalCompilationUnitEditorInput extends FileStoreEditorInput {
    * @return whether this editor input if modifiable
    */
   public boolean isModifiable() {
-    // TODO (danrubel) Investigate implementing or removing this method in future CL
-    return true;
+    //external files should never be treated as modifiable
+    return false;
   }
 
   @Override
