f5377ae135b4098aac4ff5fd189d1cecab327423
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-06-12 19:47:46 +0000

    Enable semantic hightlighting. Review URL: https://chromiumcodereview.appspot.com//10540125
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8570 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/DartCoreDebug.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/DartCoreDebug.java
@@ -49,7 +49,7 @@ public class DartCoreDebug {
   public static final boolean ANALYSIS_SERVER = true; //isOptionTrue("experimental/analysis/server");
   public static final boolean ENABLE_UPDATE = isOptionTrue("experimental/update");
   public static final boolean ENABLE_ALT_KEY_BINDINGS = isOptionTrue("experimental/altKeyBindings");
-  public static final boolean ENABLE_SEMANTIC_HIGHLIGHTING = isOptionTrue("experimental/semanticHighlighting");
+  public static final boolean ENABLE_SEMANTIC_HIGHLIGHTING = true; //isOptionTrue("experimental/semanticHighlighting");
 
   private static boolean isOptionTrue(String optionSuffix) {
     return "true".equalsIgnoreCase(Platform.getDebugOption(DartCore.PLUGIN_ID + "/" + optionSuffix));
