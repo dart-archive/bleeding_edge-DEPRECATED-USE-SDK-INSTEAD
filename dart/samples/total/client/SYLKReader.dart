538849403c2b428a80183298c12d1092486939d0
scheglov@google.com <scheglov@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-06-12 20:21:34 +0000

    Issue 3528. Additional checks for 'case expressions' http://code.google.com/p/dart/issues/detail?id=3528
    R=brianwilkerson@google.com
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//10540114
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8573 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/total/client/SYLKReader.dart
+++ dart/samples/total/client/SYLKReader.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -103,14 +103,10 @@ class SYLKReader extends Reader {
         }
 
         String text;
-        switch (true) {
-          case req.status == 200 && req.responseText != null :
-            text = req.responseText;
-            break;
-
-          default:
-            text = 'ID;P';
-            break;
+        if (req.status == 200 && req.responseText != null) {
+          text = req.responseText;
+        } else {
+          text = 'ID;P';
         }
 
         try {
