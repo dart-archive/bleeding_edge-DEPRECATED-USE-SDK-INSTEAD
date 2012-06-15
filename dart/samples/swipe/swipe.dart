d078674d6f77780eec0ed69c9fecb2604e231a75
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-06-15 21:45:32 +0000

    Add a css 3d transforms + touch gestures demo. Review URL: https://chromiumcodereview.appspot.com//10542145
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8750 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/samples/swipe/swipe.dart
@@ -0,0 +1,123 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#library("swipe");
+
+#import('dart:html');
+
+Element target;
+
+var figureWidth;
+
+double anglePos = 0.0;
+
+var timeoutHandle;
+
+void main() {
+  target = query('#target');
+  
+  initialize3D();
+  
+  // Handle touch events.
+  int touchStartX;
+  
+  target.on.touchStart.add((TouchEvent event) {
+    event.preventDefault();
+    
+    if (event.touches.length > 0) {
+      touchStartX = event.touches[0].pageX;
+    }
+  });
+  
+  target.on.touchMove.add((TouchEvent event) {
+    event.preventDefault();
+    
+    if (touchStartX != null && event.touches.length > 0) {
+      int newTouchX = event.touches[0].pageX;
+      
+      if (newTouchX > touchStartX) {
+        spinFigure(target, ((newTouchX - touchStartX) / 20).toInt() + 1);
+        touchStartX = null;
+      } else if (newTouchX < touchStartX) {
+        spinFigure(target, ((newTouchX - touchStartX) / 20).toInt() - 1);
+        touchStartX = null;
+      }
+    }
+  });
+  
+  target.on.touchEnd.add((TouchEvent event) {
+    event.preventDefault();
+    
+    touchStartX = null;
+  });
+  
+  // Handle key events.
+  document.on.keyDown.add((KeyboardEvent event) {
+    switch (event.keyIdentifier) {
+      case KeyName.LEFT:
+        startSpin(target, -1);
+        break;
+      case KeyName.RIGHT:
+        startSpin(target, 1);
+        break;
+    }
+  });
+  
+  document.on.keyUp.add((event) => stopSpin());
+}
+
+void initialize3D() {
+  num childCount = target.elements.length;
+  
+  query("#target").rect.then((ElementRect r) {
+    num width = r.client.width;
+    figureWidth = ((width / 2) / Math.tan(Math.PI / childCount)).toInt();
+   
+    target.style.transform = "translateZ(-${figureWidth}px)";
+   
+    num radius = (figureWidth * 1.2).round();
+    query('#container2').style.width = "${radius}px";
+   
+    for (int i = 0; i < childCount; i++) {
+      var panel = target.elements[i];
+      
+      panel.style.transform =
+          "rotateY(${i * (360 / childCount)}deg) translateZ(${radius}px)";
+    }
+    
+    spinFigure(target, -1);
+  });
+}
+
+void spinFigure(Element figure, int direction) {
+  num childCount = target.elements.length;
+  
+  anglePos += (360.0 / childCount) * direction;
+  
+  figure.style.transform =
+      'translateZ(-${figureWidth}px) rotateY(${anglePos}deg)';
+}
+
+/**
+ * Start an indefinite spin in the given direction.
+ */
+void startSpin(Element figure, int direction) {
+  // If we're not already spinning -
+  if (timeoutHandle == null) {
+    spinFigure(figure, direction);
+    
+    timeoutHandle = window.setInterval(
+        () => spinFigure(figure, direction), 100);
+  }
+}
+
+/**
+ * Stop any spin that may be in progress.
+ */
+void stopSpin() {
+  if (timeoutHandle != null) {
+    window.clearInterval(timeoutHandle);
+    timeoutHandle = null;
+  }
+}
