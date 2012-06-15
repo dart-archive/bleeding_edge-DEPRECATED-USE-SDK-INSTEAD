a0ef9482998849675f87dec15098b0a8db8fffe4
samhop@google.com <samhop@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-06-15 20:30:51 +0000

    minor doc change
    TBR=vsm
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com//10565005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8739 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/solar/solar.dart
+++ dart/samples/solar/solar.dart
@@ -2,6 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/** 
+ * A solar system visualization. 
+ */
+
 #library("solar");
 
 #import("dart:html");
