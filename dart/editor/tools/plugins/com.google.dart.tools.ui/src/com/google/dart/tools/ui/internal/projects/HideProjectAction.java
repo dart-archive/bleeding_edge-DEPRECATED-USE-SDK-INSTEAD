3f63ed161747d493d69e6ea94dbc297711bb12e3
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-06-19 22:44:21 +0000

    Fix to ensure associated editors are reliably closed with their parent folders.
    dartbug.com/3620
    Review URL: https://chromiumcodereview.appspot.com//10592008
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@8879 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/projects/HideProjectAction.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/projects/HideProjectAction.java
@@ -13,12 +13,25 @@
  */
 package com.google.dart.tools.ui.internal.projects;
 
+import com.google.dart.tools.ui.DartToolsPlugin;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.window.IShellProvider;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.CloseResourceAction;
+import org.eclipse.ui.part.FileEditorInput;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Standard action for hiding the currently selected project(s).
@@ -31,13 +44,6 @@ public class HideProjectAction extends CloseResourceAction {
   public HideProjectAction(IShellProvider shellProvider) {
     super(shellProvider, ProjectMessages.HideProjectAction_text);
     setToolTipText(ProjectMessages.HideProjectAction_tooltip);
-  }
-
-  @Override
-  public void run() {
-
-    super.run();
-
   }
 
   @Override
@@ -56,9 +62,62 @@ public class HideProjectAction extends CloseResourceAction {
   }
 
   @Override
-  protected void invokeOperation(IResource resource, IProgressMonitor monitor)
-      throws CoreException {
-    ((IProject) resource).delete(false, true, monitor);
+  protected void invokeOperation(IResource resource, IProgressMonitor monitor) throws CoreException {
+
+    try {
+      //preemptively close associated editors where we can, deferring only to super for dirty ones
+      closeCleanEditors(resource);
+
+    } catch (Throwable e) {
+      //if we hit any exceptions, log them and trust super to do the right thing
+      DartToolsPlugin.log(e);
+    } finally {
+
+      ((IProject) resource).delete(false, true, monitor);
+    }
+  }
+
+  private void closeCleanEditors(final IResource project) throws Throwable {
+    final Throwable[] ex = new Throwable[1];
+    Display.getDefault().syncExec(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+          IEditorReference[] editorRefs = collectEditorReferences(activePage, project);
+          activePage.closeEditors(editorRefs, false);
+        } catch (Throwable th) {
+          ex[0] = th;
+        }
+      }
+    });
+    if (ex[0] != null) {
+      throw ex[0];
+    }
+  }
+
+  private IEditorReference[] collectEditorReferences(IWorkbenchPage activePage, IResource project)
+      throws PartInitException {
+
+    List<IEditorReference> references = new ArrayList<IEditorReference>();
+
+    //grab editor references for non-dirty resources associated w/projects slated to close
+    for (IEditorReference editorRef : activePage.getEditorReferences()) {
+
+      IEditorInput input = editorRef.getEditorInput();
+      if (input instanceof FileEditorInput) {
+        FileEditorInput fileEditorInput = (FileEditorInput) input;
+        //defer dirty file handling to the super implementation
+        if (!editorRef.isDirty()) {
+          IFile file = fileEditorInput.getFile();
+          if (file.getProject().equals(project)) {
+            references.add(editorRef);
+          }
+        }
+      }
+    }
+
+    return references.toArray(new IEditorReference[] {});
   }
 
 }
