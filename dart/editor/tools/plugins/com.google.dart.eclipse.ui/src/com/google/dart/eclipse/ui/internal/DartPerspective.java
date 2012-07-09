e680f5a60de8390d212b630a93fe7d01fc3da06f
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-07-09 18:25:18 +0000

    Plugin distro dart perspective. Review URL: https://chromiumcodereview.appspot.com//10762002
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@9475 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.eclipse.ui/src/com/google/dart/eclipse/ui/internal/DartPerspective.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2012, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.eclipse.ui.internal;
+
+/*
+ * Copyright (c) 2012, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import com.google.dart.tools.ui.DartUI;
+
+import org.eclipse.search.ui.NewSearchUI;
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IPlaceholderFolderLayout;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.progress.IProgressConstants;
+
+/**
+ * The Dart Tooling for Eclipse perspective.
+ */
+public class DartPerspective implements IPerspectiveFactory {
+
+  private static final String DEBUGGER_VIEW_ID = "com.google.dart.tools.debug.debuggerView"; //$NON-NLS-1$
+
+  private static final String BR = "bottomRight"; //$NON-NLS-1$
+  private static final String TL = "topLeft"; //$NON-NLS-1$
+  private static final String TR = "topRight"; //$NON-NLS-1$
+  private static final String OUTLINE_FOLDER = "outlineFolder"; //$NON-NLS-1$
+
+  private static final String WIZARD_NEW_PROJECT = "com.google.dart.project.wizard"; //$NON-NLS-1$
+  private static final String WIZARD_NEW_FILE = "org.eclipse.ui.wizards.new.file"; //$NON-NLS-1$
+  private static final String WIZARD_NEW_FOLDER = "org.eclipse.ui.wizards.new.folder"; //$NON-NLS-1$
+  private static final String WIZARD_NEW_TEXT = "org.eclipse.ui.editors.wizards.UntitledTextFileWizard"; //$NON-NLS-1$
+
+  public DartPerspective() {
+  }
+
+  @Override
+  public void createInitialLayout(IPageLayout layout) {
+    String editorArea = layout.getEditorArea();
+
+    // Top left: Project Explorer view
+    IFolderLayout topLeft = layout.createFolder(TL, IPageLayout.LEFT, 0.25f, editorArea);
+
+    topLeft.addView(IPageLayout.ID_PROJECT_EXPLORER);
+    topLeft.addPlaceholder(DartUI.ID_APPS_VIEW);
+
+    // Bottom left: Outline view and Property Sheet view
+    IPlaceholderFolderLayout outlinefolder = layout.createPlaceholderFolder(
+        OUTLINE_FOLDER,
+        IPageLayout.BOTTOM,
+        0.50f,
+        TL);
+    outlinefolder.addPlaceholder(IPageLayout.ID_PROP_SHEET);
+    outlinefolder.addPlaceholder(DartUI.ID_DARTUNIT_VIEW);
+
+    // Bottom right: info views
+    IFolderLayout outputfolder = layout.createFolder(BR, IPageLayout.BOTTOM, 0.75f, editorArea);
+    outputfolder.addView(IPageLayout.ID_PROBLEM_VIEW);
+    outputfolder.addPlaceholder(NewSearchUI.SEARCH_VIEW_ID);
+    outputfolder.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
+    //TODO (pquitslund): move console to shared UI
+    //outputfolder.addPlaceholder(DartUI.ID_CONSOLE_VIEW);
+    outputfolder.addPlaceholder(IPageLayout.ID_TASK_LIST);
+    outputfolder.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
+
+    // Top right: Debugger View
+    IPlaceholderFolderLayout debuggerFolder = layout.createPlaceholderFolder(
+        TR,
+        IPageLayout.RIGHT,
+        0.70f,
+        editorArea);
+    debuggerFolder.addPlaceholder(DEBUGGER_VIEW_ID);
+
+    outlinefolder.addPlaceholder(IPageLayout.ID_OUTLINE);
+
+    layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
+
+    layout.addShowViewShortcut(NewSearchUI.SEARCH_VIEW_ID);
+    layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
+    layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+    layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+    layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+    layout.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
+
+    // new actions - wizards
+    layout.addNewWizardShortcut(WIZARD_NEW_PROJECT);
+    layout.addNewWizardShortcut(WIZARD_NEW_FOLDER);
+    layout.addNewWizardShortcut(WIZARD_NEW_FILE);
+    layout.addNewWizardShortcut(WIZARD_NEW_TEXT);
+  }
+}
