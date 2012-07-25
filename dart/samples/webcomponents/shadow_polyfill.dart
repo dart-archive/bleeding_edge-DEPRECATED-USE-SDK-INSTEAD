adfd93094aebbdd4a3d574e18c5069e7c80f042b
sigmund@google.com <sigmund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-07-25 22:31:48 +0000

    expand also declarations found within the shadow root.
    Review URL: https://chromiumcodereview.appspot.com//10823026
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@9916 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/webcomponents/shadow_polyfill.dart
+++ dart/samples/webcomponents/shadow_polyfill.dart
@@ -2,10 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/** 
+/**
  * Polyfill script for custom elements. To use this script, your app must
  * create a CustomElementsManager with the appropriate lookup function before
- * doing any DOM queries or modifications. 
+ * doing any DOM queries or modifications.
  * Currently, all custom elements must be registered with the polyfill.  To
  * register custom elements, provide the appropriate lookup function to your
  * CustomElementsManager.
@@ -48,7 +48,7 @@ abstract class WebComponent {
 /** Loads and manages the custom elements on a page. */
 class CustomElementsManager {
 
-  /** 
+  /**
    * Maps tag names to our internal dart representation of the custom element.
    */
   Map<String, _CustomDeclaration> _customDeclarations;
@@ -69,7 +69,7 @@ class CustomElementsManager {
     // TODO(samhop): DOM objects (and everything else) should be hashable
     _customElements = new ListMap<Element, WebComponent>();
 
-    // Listen for all insertions and deletions on the DOM so that we can 
+    // Listen for all insertions and deletions on the DOM so that we can
     // catch custom elements being inserted and call the appropriate callbacks.
     _insertionObserver = new MutationObserver((mutations, observer) {
       for (var mutation in mutations) {
@@ -103,14 +103,14 @@ class CustomElementsManager {
   }
 
   /**
-   * Load the document at the given url and parse it to extract 
+   * Load the document at the given url and parse it to extract
    * custom element declarations.
    */
   void _load(String url) {
     var request = new XMLHttpRequest();
     // We use a blocking request here because no Dart is allowed to run
     // until DOM content is loaded.
-    // TODO(samhop): give a decent timeout message if custom elements fail to 
+    // TODO(samhop): give a decent timeout message if custom elements fail to
     // load
     request.open('GET', url, async: false);
     request.on.readyStateChange.add((Event e) {
@@ -118,8 +118,8 @@ class CustomElementsManager {
         if (request.status >= 200 && request.status < 300
             || request.status == 304 || request.status == 0) {
           var declarations = _parse(request.response);
-          declarations.forEach((declaration) { 
-            _customDeclarations[declaration.name] = declaration; 
+          declarations.forEach((declaration) {
+            _customDeclarations[declaration.name] = declaration;
           });
         } else {
           window.console.error(
@@ -142,11 +142,12 @@ class CustomElementsManager {
   }
 
   /** Look for all custom elements uses and expand them appropriately. */
-  List _expandDeclarations() {
+  List _expandDeclarations([root=null]) {
     var newCustomElements = [];
     _customDeclarations.getValues().forEach((declaration) {
       var query = '${declaration.extendz}[is=${declaration.name}]';
-      queryAll(query).forEach((Element e) { 
+      var target = root == null ? document : root;
+      target.queryAll(query).forEach((Element e) {
         var newElement = declaration.morph(e);
         // must call the inserted callback here for elements in the initial
         // markup, since our mutation observers won't be able to see them
@@ -162,7 +163,7 @@ class CustomElementsManager {
 
   /**
    * Expands the given html string into a custom element.
-   * Assumes only one element use in htmlString (or only returns the 
+   * Assumes only one element use in htmlString (or only returns the
    * first one) and assumes corresponding custom element is already
    * registered.
    */
@@ -206,7 +207,7 @@ class _CustomDeclaration {
     if (other is! _CustomDeclaration) {
       return false;
     } else {
-      return other.name == name && 
+      return other.name == name &&
              other.extendz == extendz &&
              other.template == template;
     }
@@ -229,10 +230,11 @@ class _CustomDeclaration {
     var newCustomElement = manager._lookup(this.name)(shadowRoot, e);
     manager._customElements[e] = newCustomElement;
     newCustomElement.created();
+    manager._expandDeclarations(shadowRoot);
 
     // TODO(samhop): investigate refactoring/redesigning the API so that
     // components which don't need their attributes observed don't have an
-    // observer created, for perf reasons. 
+    // observer created, for perf reasons.
     var attributeObserver = new MutationObserver((mutations, observer) {
       for (var mutation in mutations) {
         if (mutation.type == 'attributes') {
