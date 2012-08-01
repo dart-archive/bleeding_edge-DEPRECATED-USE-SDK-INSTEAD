9e4cf0de99585edcc49186991a63719f353dbc33
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-08-01 21:59:35 +0000

    comment out action to avaoid spurious exceptions Review URL: https://chromiumcodereview.appspot.com//10825138
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@10142 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/editor/CompilationUnitEditor.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/editor/CompilationUnitEditor.java
@@ -32,7 +32,6 @@ import com.google.dart.tools.ui.IWorkingCopyManager;
 import com.google.dart.tools.ui.PreferenceConstants;
 import com.google.dart.tools.ui.actions.DartEditorActionDefinitionIds;
 import com.google.dart.tools.ui.actions.GenerateActionGroup;
-import com.google.dart.tools.ui.actions.OrganizeImportsAction;
 import com.google.dart.tools.ui.actions.RefactorActionGroup;
 import com.google.dart.tools.ui.internal.text.DartHelpContextIds;
 import com.google.dart.tools.ui.internal.text.DartStatusConstants;
@@ -1397,9 +1396,9 @@ public class CompilationUnitEditor extends DartEditor implements IDartReconcilin
     markAsSelectionDependentAction("Format", true); //$NON-NLS-1$
     PlatformUI.getWorkbench().getHelpSystem().setHelp(action, DartHelpContextIds.FORMAT_ACTION);
 
-    action = new OrganizeImportsAction(this);
-    action.setActionDefinitionId(DartEditorActionDefinitionIds.ORGANIZE_IMPORTS);
-    setAction("OrganizeImports", action);
+//    action = new OrganizeImportsAction(this);
+//    action.setActionDefinitionId(DartEditorActionDefinitionIds.ORGANIZE_IMPORTS);
+//    setAction("OrganizeImports", action);
 
     //     action = new AddBlockCommentAction(
     //     DartEditorMessages.getBundleForConstructedKeys(),
