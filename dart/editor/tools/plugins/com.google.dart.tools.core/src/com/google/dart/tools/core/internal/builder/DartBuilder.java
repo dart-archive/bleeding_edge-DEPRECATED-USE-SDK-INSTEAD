6faa9959ebaa84fc7cc750538af0a984ed97ebc1
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-08-01 20:43:54 +0000

    Dart build participant support. Review URL: https://chromiumcodereview.appspot.com//10837030
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@10139 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/internal/builder/DartBuilder.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/internal/builder/DartBuilder.java
@@ -13,7 +13,9 @@
  */
 package com.google.dart.tools.core.internal.builder;
 
+import com.google.dart.tools.core.DartCore;
 import com.google.dart.tools.core.analysis.AnalysisServer;
+import com.google.dart.tools.core.builder.DartBuildParticipant;
 import com.google.dart.tools.core.internal.model.SystemLibraryManagerProvider;
 import com.google.dart.tools.core.internal.util.Extensions;
 
@@ -23,10 +25,18 @@ import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.ISafeRunnable;
+import org.eclipse.core.runtime.RegistryFactory;
+import org.eclipse.core.runtime.SafeRunner;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -35,13 +45,68 @@ import java.util.Map;
  */
 public class DartBuilder extends IncrementalProjectBuilder {
 
+  private static final String PARTICIPANT_EXTENSION_POINT = "buildParticipant"; //$NON-NLS-1$
+  private static final String PARTICIPANT_CONTRIBUTION = "buildParticipant"; //$NON-NLS-1$
+  private static final String PARTICIPANT_CLASS_ATTR = "class"; //$NON-NLS-1$
+
   private final AnalysisServer server = SystemLibraryManagerProvider.getDefaultAnalysisServer();
 
-  @SuppressWarnings("rawtypes")
-  @Override
-  protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
+  private static DartBuildParticipant[] PARTICIPANTS;
 
-    IResourceDelta delta = getDelta(getProject());
+  private static DartBuildParticipant[] getBuildParticipants() {
+    if (PARTICIPANTS == null) {
+      loadParticipantExtensions();
+    }
+    return PARTICIPANTS;
+  }
+
+  private static void loadParticipantExtensions() {
+    IExtensionRegistry registry = RegistryFactory.getRegistry();
+    ArrayList<DartBuildParticipant> participants = new ArrayList<DartBuildParticipant>();
+
+    IExtensionPoint point = registry.getExtensionPoint(
+        DartCore.PLUGIN_ID,
+        PARTICIPANT_EXTENSION_POINT);
+
+    for (IExtension extension : point.getExtensions()) {
+      for (IConfigurationElement element : extension.getConfigurationElements()) {
+        try {
+          if (element.getName().equals(PARTICIPANT_CONTRIBUTION)) {
+            DartBuildParticipant participant = (DartBuildParticipant) element.createExecutableExtension(PARTICIPANT_CLASS_ATTR);
+            participants.add(participant);
+          }
+        } catch (CoreException e) {
+          DartCore.logError(e);
+        }
+      }
+    }
+
+    PARTICIPANTS = participants.toArray(new DartBuildParticipant[participants.size()]);
+  }
+
+  @Override
+  protected IProject[] build(final int kind, final Map<String, String> args,
+      final IProgressMonitor monitor) throws CoreException {
+
+    final IResourceDelta delta = getDelta(getProject());
+
+    //notify participants
+    for (final DartBuildParticipant participant : getBuildParticipants()) {
+
+      SafeRunner.run(new ISafeRunnable() {
+
+        @Override
+        public void handleException(Throwable exception) {
+          DartCore.logError("Error notifying build participant", exception);
+        }
+
+        @Override
+        public void run() throws Exception {
+          participant.build(kind, args, delta, monitor);
+        }
+      });
+
+    }
 
     // If delta is null, then building a new project
 
@@ -108,7 +173,26 @@ public class DartBuilder extends IncrementalProjectBuilder {
   }
 
   @Override
-  protected void clean(IProgressMonitor monitor) throws CoreException {
+  protected void clean(final IProgressMonitor monitor) throws CoreException {
+
+    //notify participants
+    for (final DartBuildParticipant participant : getBuildParticipants()) {
+
+      SafeRunner.run(new ISafeRunnable() {
+
+        @Override
+        public void handleException(Throwable exception) {
+          DartCore.logError("Error notifying build participant", exception);
+        }
+
+        @Override
+        public void run() throws Exception {
+          participant.clean(getProject(), monitor);
+        }
+      });
+
+    }
+
     AnalysisServer server = SystemLibraryManagerProvider.getDefaultAnalysisServer();
     server.reanalyze();
   }
