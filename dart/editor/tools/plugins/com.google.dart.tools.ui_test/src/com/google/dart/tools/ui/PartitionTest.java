cd9cdb13dbe02f6cba8fa4a75bb172183fa01b20
brianwilkerson@google.com <brianwilkerson@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-08-13 17:29:43 +0000

    Test partitioning when a comment follows a block Review URL: https://chromiumcodereview.appspot.com//10826275
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@10589 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui_test/src/com/google/dart/tools/ui/PartitionTest.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui_test/src/com/google/dart/tools/ui/PartitionTest.java
@@ -1,20 +1,19 @@
 /*
  * Copyright (c) 2011, the Dart project authors.
- *
- * Licensed under the Eclipse Public License v1.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.google.dart.tools.ui;
 
+import com.google.common.base.Joiner;
 import com.google.dart.tools.ui.text.DartPartitions;
 import com.google.dart.tools.ui.text.DartTextTools;
 
@@ -25,20 +24,32 @@ import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.ITypedRegion;
 
 public class PartitionTest extends TestCase {
+  public void testPartitioner_commentAfterBlock() {
+    assertPartitionCount(3, Joiner.on("\n").join(//
+        "class X {", //
+        "  void m(v) {", //
+        "    if (v < 0) {", //
+        "      return;", //
+        "    }", //
+        "    // Comment", //
+        "  }", //
+        "}"));
+  }
 
-  private static final String source = "" // comments improve formatting...
-      + "class X {\n" //
-      + "var s='''test''';\n" //
-      + "}\n"; //
+  public void testPartitioner_multilineString() {
+    assertPartitionCount(3, Joiner.on("\n").join(//
+        "class X {", //
+        "  var s='''test''';", //
+        "}"));
+  }
 
-  public void testPartitioner() {
+  private void assertPartitionCount(int expectedPartitionCount, String source) {
     Document doc = new Document(source);
     DartTextTools tools = DartToolsPlugin.getDefault().getJavaTextTools();
     IDocumentPartitioner part = tools.createDocumentPartitioner();
     doc.setDocumentPartitioner(DartPartitions.DART_PARTITIONING, part);
     part.connect(doc);
     ITypedRegion[] parts = part.computePartitioning(0, source.length());
-    assertTrue(parts.length == 3);
+    assertEquals(expectedPartitionCount, parts.length);
   }
-
 }
