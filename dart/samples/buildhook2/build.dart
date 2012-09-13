b1f6cad1a3b68ba25cd5ec73f2ec3e37ded50e24
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-09-13 23:09:46 +0000

    Update the buildhooks2 example. Review URL: https://chromiumcodereview.appspot.com//10908149
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@12359 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/buildhook2/build.dart
+++ dart/samples/buildhook2/build.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 #import("dart:io");
+#import("package:args/args.dart");
 
 bool cleanBuild;
 bool fullBuild;
@@ -12,20 +13,12 @@ List<String> removedFiles;
 /**
  * This build script is invoked automatically by the Editor whenever a file
  * in the project changes. It must be placed in the root of a project and named
- * 'build.dart'. The legal command-line parameters are:
- *
- * * <none>: a full build is requested
- * * --clean: remove any build artifacts
- * * --changed=one.foo: the file has changed since the last build
- * * --removed=one.foo: the file has been removed since the last build
- *
- * Any error code other then 0 returned by the script is considered an error.
- * The error, and any stdout or stderr text, will by printed to the console.
+ * 'build.dart'. See the source code of [processArgs] for information about the
+ * legal command line options.
  */
 void main() {
   print("running build.dart...");
 
-  // TODO: change this script to use the pkg/args library
   processArgs();
 
   if (cleanBuild) {
@@ -42,65 +35,52 @@ void main() {
 }
 
 /**
- * Handle the -clean and -changed command-line args.
+ * Handle the --changed, --removed, --clean and --help command-line args.
  */
 void processArgs() {
-  changedFiles = [];
-  removedFiles = [];
-
-  List<String> args = new Options().arguments;
-
-  for (String arg in args) {
-    if (arg == "--clean") {
-      cleanBuild = true;
-    } else if (arg.startsWith("--changed=")) {
-      changedFiles.add(arg.substring("--changed=".length));
-    } else if (arg.startsWith("--removed=")) {
-      removedFiles.add(arg.substring("--removed=".length));
-    }
+  var parser = new ArgParser();
+  parser.addOption("changed", help: "the file has changed since the last build",
+      allowMultiple: true);
+  parser.addOption("removed", help: "the file was removed since the last build",
+      allowMultiple: true);
+  parser.addFlag("clean", negatable: false, help: "remove any build artifacts");
+  parser.addFlag("help", negatable: false, help: "displays this help and exit");
+  var args = parser.parse(new Options().arguments);
+  if (args["help"]) {
+    print(parser.getUsage());
+    exit(0);
   }
 
+  changedFiles = args["changed"];
+  removedFiles = args["removed"];
+  cleanBuild = args["clean"];
   fullBuild = changedFiles.isEmpty() && removedFiles.isEmpty() && !cleanBuild;
 }
 
 /**
- * Delete all generated .foobar files.
+ * Delete all generated files.
  */
 void handleCleanCommand() {
   Directory current = new Directory.current();
-  DirectoryLister lister = current.list(true);
-
-  lister.onFile = (String file) {
-    if (file.endsWith(".foobar")) {
-      new File(file).delete();
-    }
-  };
+  current.list(true).onFile = _maybeClean;
 }
 
 /**
  * Recursively scan the current directory looking for .foo files to process.
  */
 void handleFullBuild() {
-  List<String> files = [];
+  var files = <String>[];
+  var lister = new Directory.current().list(true);
 
-  Directory current = new Directory.current();
-  DirectoryLister lister = current.list(true);
-
-  lister.onFile = (String file) {
-    if (file.endsWith(".foo")) {
-      files.add(file);
-    }
-  };
-  lister.onDone = (bool completed) => handleChangedFiles(files);
+  lister.onFile = (file) => files.add(file);
+  lister.onDone = (_) => handleChangedFiles(files);
 }
 
 /**
  * Process the given list of changed files.
  */
 void handleChangedFiles(List<String> files) {
-  for (String arg in files) {
-    _processFile(arg);
-  }
+  files.forEach(_processFile);
 }
 
 /**
@@ -131,5 +111,14 @@ void _processFile(String arg) {
     out.close();
 
     print("wrote     : ${outFile.name}");
+  }
+}
+
+/**
+ * If this file is a generated file (based on the extension), delete it.
+ */
+void _maybeClean(String file) {
+  if (file.endsWith(".foobar")) {
+    new File(file).delete();
   }
 }
