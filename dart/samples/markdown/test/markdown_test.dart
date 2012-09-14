9cbe830641891f1ff9d2c7068a6403828f863251
rnystrom@google.com <rnystrom@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-09-14 21:12:17 +0000

    Update unittest to new package layout.
    Review URL: https://codereview.chromium.org//10917275
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@12404 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/markdown/test/markdown_test.dart
+++ dart/samples/markdown/test/markdown_test.dart
@@ -8,7 +8,7 @@
 #import('../lib.dart');
 
 // TODO(rnystrom): Better path to unittest.
-#import('../../../pkg/unittest/unittest.dart');
+#import('../../../pkg/unittest/lib/unittest.dart');
 
 /// Most of these tests are based on observing how showdown behaves:
 /// http://softwaremaniacs.org/playground/showdown-highlight/
