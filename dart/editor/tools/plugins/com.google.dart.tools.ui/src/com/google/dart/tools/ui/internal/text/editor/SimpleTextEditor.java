4d8e9b45615b80772d47530e0c8b3a4813319e88
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-10-05 22:07:31 +0000

    Fix to "upgrade" an active simple text editor to a content-specific one if the file extension changes (dartbug.com/2267). Review URL: https://codereview.chromium.org//11073002
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@13325 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/editor/SimpleTextEditor.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/editor/SimpleTextEditor.java
@@ -1,9 +1,29 @@
-// Copyright (c) 2011, the Dart project authors. All Rights Reserved.
-
+/*
+ * Copyright (c) 2012, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.google.dart.tools.ui.internal.text.editor;
 
+import com.google.dart.tools.ui.DartToolsPlugin;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.texteditor.ITextEditorActionConstants;
 
 /**
@@ -16,6 +36,37 @@ public class SimpleTextEditor extends TextEditor {
 
   public SimpleTextEditor() {
     setRulerContextMenuId("#DartSimpleTextEditorRulerContext"); //$NON-NLS-1$
+  }
+
+  @Override
+  protected void doSetInput(IEditorInput input) throws CoreException {
+
+    final IFile file = (IFile) input.getAdapter(IFile.class);
+
+    IEditorDescriptor descriptor = IDE.getEditorDescriptor(file);
+
+    //Re-open input with a more appropriate editor if there's a better fit
+    if (!descriptor.getId().equals(ID)) {
+
+      close(true);
+
+      Display.getDefault().asyncExec(new Runnable() {
+
+        @Override
+        public void run() {
+          try {
+            IDE.openEditor(DartToolsPlugin.getActivePage(), file, false);
+          } catch (PartInitException e) {
+            DartToolsPlugin.log(e);
+          }
+        }
+      });
+
+      return;
+    }
+
+    //Else, carry on
+    super.doSetInput(input);
   }
 
   @Override
