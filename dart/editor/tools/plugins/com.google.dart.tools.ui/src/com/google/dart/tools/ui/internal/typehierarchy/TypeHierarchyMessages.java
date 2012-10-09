05ff611f7251448fa73acdb106806973be236fe8
scheglov@google.com <scheglov@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-10-09 22:34:10 +0000

    Forgotten files
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@13447 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/typehierarchy/TypeHierarchyMessages.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2010 IBM Corporation and others. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: IBM Corporation - initial API and
+ * implementation
+ *******************************************************************************/
+package com.google.dart.tools.ui.internal.typehierarchy;
+
+import org.eclipse.osgi.util.NLS;
+
+public final class TypeHierarchyMessages extends NLS {
+
+  private static final String BUNDLE_NAME = "com.google.dart.tools.ui.internal.typehierarchy.TypeHierarchyMessages";//$NON-NLS-1$
+
+  public static String EnableMemberFilterAction_label;
+
+  public static String EnableMemberFilterAction_tooltip;
+  public static String EnableMemberFilterAction_description;
+  public static String HistoryDropDownAction_clearhistory_label;
+  public static String ToggleOrientationAction_horizontal_label;
+  public static String ToggleOrientationAction_horizontal_tooltip;
+  public static String ToggleOrientationAction_horizontal_description;
+  public static String ToggleOrientationAction_vertical_label;
+  public static String ToggleOrientationAction_vertical_tooltip;
+  public static String ToggleOrientationAction_vertical_description;
+  public static String ToggleOrientationAction_automatic_label;
+  public static String ToggleOrientationAction_automatic_tooltip;
+  public static String ToggleOrientationAction_automatic_description;
+  public static String ToggleOrientationAction_single_label;
+  public static String ToggleOrientationAction_single_tooltip;
+  public static String ToggleOrientationAction_single_description;
+  public static String FocusOnSelectionAction_label;
+  public static String FocusOnSelectionAction_tooltip;
+  public static String FocusOnSelectionAction_description;
+  public static String FocusOnTypeAction_label;
+  public static String FocusOnTypeAction_tooltip;
+  public static String FocusOnTypeAction_description;
+  public static String FocusOnTypeAction_dialog_title;
+  public static String FocusOnTypeAction_dialog_message;
+  public static String HistoryDropDownAction_tooltip;
+  public static String HistoryAction_description;
+  public static String HistoryAction_tooltip;
+  public static String HistoryAction_inputElements_1;
+  public static String HistoryAction_inputElements_2;
+  public static String HistoryAction_inputElements_more;
+  public static String HistoryListDialog_title;
+  public static String HistoryListDialog_label;
+  public static String HistoryListDialog_remove_button;
+  public static String HistoryListAction_label;
+  public static String ShowInheritedMembersAction_label;
+  public static String ShowInheritedMembersAction_tooltip;
+  public static String ShowInheritedMembersAction_description;
+  public static String ShowQualifiedTypeNamesAction_label;
+  public static String ShowQualifiedTypeNamesAction_tooltip;
+  public static String ShowQualifiedTypeNamesAction_description;
+  public static String SortByDefiningTypeAction_label;
+  public static String SortByDefiningTypeAction_tooltip;
+  public static String SortByDefiningTypeAction_description;
+  public static String TypeHierarchyViewPart_createinput;
+  public static String TypeHierarchyViewPart_error_title;
+  public static String TypeHierarchyViewPart_error_message;
+  public static String TypeHierarchyViewPart_empty;
+  public static String TypeHierarchyViewPart_nodecl;
+  public static String TypeHierarchyViewPart_exception_title;
+  public static String TypeHierarchyViewPart_exception_message;
+  public static String TypeHierarchyViewPart_tooltip;
+  public static String TypeHierarchyViewPart_tooltip2;
+  public static String TypeHierarchyViewPart_tooltip_more;
+  public static String TypeHierarchyViewPart_ws_description;
+  public static String TypeHierarchyViewPart_ws_description2;
+  public static String TypeHierarchyViewPart_ws_description_more;
+  public static String TypeHierarchyViewPart_ws_tooltip;
+  public static String TypeHierarchyViewPart_ws_tooltip2;
+  public static String TypeHierarchyViewPart_ws_tooltip_more;
+  public static String TypeHierarchyViewPart_restoreinput;
+  public static String TypeHierarchyViewPart_layout_submenu;
+  public static String TypeHierarchyLifeCycle_computeInput;
+  public static String ToggleViewAction_subtypes_label;
+  public static String ToggleViewAction_subtypes_tooltip;
+  public static String ToggleViewAction_subtypes_description;
+  public static String ToggleViewAction_supertypes_label;
+  public static String ToggleViewAction_supertypes_tooltip;
+  public static String ToggleViewAction_supertypes_description;
+  public static String ToggleViewAction_vajhierarchy_label;
+  public static String ToggleViewAction_vajhierarchy_tooltip;
+  public static String ToggleViewAction_vajhierarchy_description;
+  public static String HierarchyInformationControl_methodhierarchy_label;
+  public static String HierarchyInformationControl_hierarchy_label;
+  public static String HierarchyInformationControl_toggle_traditionalhierarchy_label;
+  public static String HierarchyInformationControl_toggle_superhierarchy_label;
+  static {
+    NLS.initializeMessages(BUNDLE_NAME, TypeHierarchyMessages.class);
+  }
+
+  private TypeHierarchyMessages() {
+    // Do not instantiate
+  }
+}
