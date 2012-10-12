fe3b9373a5f1a0c43a1f81df1d39a5df08614508
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-10-12 09:27:27 +0000

    Use 'part of' in the samples. Review URL: https://codereview.chromium.org//11094075
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@13603 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/spirodraw/ColorPicker.dart
+++ dart/samples/spirodraw/ColorPicker.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// part of spirodraw;
+part of spirodraw;
 
 typedef void PickerListener(String selectedColor);
 
