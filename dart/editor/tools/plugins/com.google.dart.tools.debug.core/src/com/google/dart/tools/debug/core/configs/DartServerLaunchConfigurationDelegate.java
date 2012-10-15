94305771d6bde9eb50556448f2bcdb13ac146881
iposva@google.com <iposva@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-10-15 17:53:43 +0000

    Fix bug 5883: - Fix condition when removing isolates being debugged. - Reenable debugging for Linux and Windows. Review URL: https://codereview.chromium.org//11145023
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@13661 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.debug.core/src/com/google/dart/tools/debug/core/configs/DartServerLaunchConfigurationDelegate.java
+++ dart/editor/tools/plugins/com.google.dart.tools.debug.core/src/com/google/dart/tools/debug/core/configs/DartServerLaunchConfigurationDelegate.java
@@ -17,6 +17,7 @@ import com.google.dart.tools.core.DartCore;
 import com.google.dart.tools.core.model.DartSdkManager;
 import com.google.dart.tools.debug.core.DartDebugCorePlugin;
 import com.google.dart.tools.debug.core.DartLaunchConfigWrapper;
+import com.google.dart.tools.debug.core.server.ServerDebugTarget;
 import com.google.dart.tools.debug.core.util.NetUtils;
 
 import org.eclipse.core.resources.IResource;
@@ -114,11 +115,9 @@ public class DartServerLaunchConfigurationDelegate extends LaunchConfigurationDe
     commandsList.add(vmExecPath);
     commandsList.addAll(Arrays.asList(launchConfig.getVmArgumentsAsArray()));
 
-    // BUG(5883) Debugging of standalone programs is disabled
-    // because it sometimes leads to program crashes.
-//    if (enableDebugging && !DartCore.isWindows()) {
-//      commandsList.add("--debug:" + connectionPort);
-//    }
+    if (enableDebugging && !DartCore.isWindows()) {
+      commandsList.add("--debug:" + connectionPort);
+    }
 
     String packageRoot = DartCore.getPlugin().getPackageRootPref();
     if (packageRoot != null) {
@@ -177,22 +176,20 @@ public class DartServerLaunchConfigurationDelegate extends LaunchConfigurationDe
 
     eclipseProcess.setAttribute(IProcess.ATTR_CMDLINE, generateCommandLine(commands));
 
-    // BUG(5883) Debugging of standalone programs is disabled
-    // because it sometimes leads to program crashes.
-//    if (enableDebugging && !DartCore.isWindows()) {
-//      ServerDebugTarget debugTarget = new ServerDebugTarget(launch, eclipseProcess, connectionPort);
-//
-//      try {
-//        debugTarget.connect();
-//
-//        launch.addDebugTarget(debugTarget);
-//      } catch (DebugException ex) {
-//        // We don't throw an exception if the process died before we could connect.
-//        if (!isProcessDead(runtimeProcess)) {
-//          throw ex;
-//        }
-//      }
-//    }
+    if (enableDebugging && !DartCore.isWindows()) {
+      ServerDebugTarget debugTarget = new ServerDebugTarget(launch, eclipseProcess, connectionPort);
+
+      try {
+        debugTarget.connect();
+
+        launch.addDebugTarget(debugTarget);
+      } catch (DebugException ex) {
+        // We don't throw an exception if the process died before we could connect.
+        if (!isProcessDead(runtimeProcess)) {
+          throw ex;
+        }
+      }
+    }
 
     monitor.done();
   }
