deec0c25f16aa1c573f9030573cf1c633431e998
sethladd@google.com <sethladd@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-10-16 23:22:14 +0000

    also ignore ds_store remove unwanted file update the hello sample
    BUG=
    
    Review URL: https://codereview.chromium.org//11142015
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@13711 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/hi/hi.dart
+++ dart/samples/hi/hi.dart
@@ -1,11 +1,11 @@
-// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#library('hi');
+library hi;
 
-#import('dart:html');
+import 'dart:html';
 
 main() {
-  document.query('#status').innerHTML = 'Hi, Dart';
+  query('#status').text = 'Hi, Dart';
 }
