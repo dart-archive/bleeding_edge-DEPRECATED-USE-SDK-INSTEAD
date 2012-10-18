56340845c6b8afbf6e46fea97902421ddd3e901e
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-10-18 21:10:36 +0000

    UpdateUtils.{unzip(..), copy(..)} tests to verify perms preservation (dartbug.com/5908).
    [NOTE: archive.zip not include in review since upload chokes gcl.]
    Review URL: https://codereview.chromium.org//11194059
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@13799 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui_test/src/com/google/dart/tools/ui/internal/update/UpdateUtilsTest.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui_test/src/com/google/dart/tools/ui/internal/update/UpdateUtilsTest.java
@@ -13,11 +13,53 @@
  */
 package com.google.dart.tools.ui.internal.update;
 
+import com.google.dart.tools.core.test.util.FileUtilities;
+import com.google.dart.tools.core.test.util.PluginUtilities;
+import com.google.dart.tools.core.test.util.TestUtilities;
 import com.google.dart.tools.update.core.internal.UpdateUtils;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class UpdateUtilsTest extends TestCase {
+
+  private static final NullProgressMonitor NULL_MONITOR = new NullProgressMonitor();
+
+  private static final FileFilter NO_OVERWRITE = new FileFilter() {
+    @Override
+    public boolean accept(File file) {
+      return false;
+    }
+  };
+
+  private File tempDir;
+
+  private static final String TEST_PLUGIN_ID = "com.google.dart.tools.ui_test";
+
+  private static final String TEST_DATA_DIR = "test_data";
+
+  private static final String NULL_TASK = "";
+
+  public void testCopyPreservesPerms() throws Exception {
+
+    File fromDir = getDataDir("testCopyPreservesPerms");
+    File toDir = getToDir();
+
+    UpdateUtils.copyDirectory(fromDir, toDir, NO_OVERWRITE, NULL_MONITOR);
+
+    assertTrue(new File(toDir, "exec").canExecute());
+    assertFalse(new File(toDir, "no_exec").canExecute());
+  }
 
   public void testParseRevisionJSON() throws Exception {
 
@@ -33,4 +75,39 @@ public class UpdateUtilsTest extends TestCase {
     assertEquals(revision, "9826");
 
   }
+
+  public void testUnzipPreservesPerms() throws Exception {
+
+    File fromDir = getDataDir("testUnzipPreservesPerms");
+    File toDir = getToDir();
+
+    UpdateUtils.unzip(new File(fromDir, "archive.zip"), toDir, NULL_TASK, NULL_MONITOR);
+
+    assertTrue(new File(toDir, "exec").canExecute());
+    assertFalse(new File(toDir, "no_exec").canExecute());
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    if (tempDir != null) {
+      FileUtilities.delete(tempDir);
+    }
+  }
+
+  private File getDataDir(String testName) throws MalformedURLException, IOException {
+    URL pluginInstallUri = PluginUtilities.getInstallUrl(TEST_PLUGIN_ID);
+    URL sourceUrl = new URL(pluginInstallUri, TEST_DATA_DIR + "/" + getClass().getSimpleName()
+        + "/" + testName);
+    IPath sourcePath = new Path(FileLocator.toFileURL(sourceUrl).getPath());
+    File fromDir = sourcePath.toFile();
+    return fromDir;
+  }
+
+  private File getToDir() {
+    if (tempDir == null) {
+      tempDir = TestUtilities.createTempDirectory();
+    }
+    return tempDir.getAbsoluteFile();
+  }
+
 }
