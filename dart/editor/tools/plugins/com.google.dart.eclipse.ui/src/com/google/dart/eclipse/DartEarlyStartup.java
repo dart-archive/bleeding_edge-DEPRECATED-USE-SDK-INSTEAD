899e211be3e80aac2112d046eacf9a5152b8bd00
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-10-24 19:56:54 +0000

    fix for dartbug.com/5380 Review URL: https://codereview.chromium.org//11234063
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@14035 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.eclipse.ui/src/com/google/dart/eclipse/DartEarlyStartup.java
+++ dart/editor/tools/plugins/com.google.dart.eclipse.ui/src/com/google/dart/eclipse/DartEarlyStartup.java
@@ -17,11 +17,23 @@ package com.google.dart.eclipse;
 import com.google.dart.tools.core.model.DartSdkManager;
 import com.google.dart.tools.core.model.DartSdkUpgradeJob;
 
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PerspectiveAdapter;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.Perspective;
+import org.eclipse.ui.internal.WorkbenchPage;
+import org.eclipse.ui.internal.registry.IActionSetDescriptor;
+
+import java.util.ArrayList;
 
 /**
  * Check for and possibly install a Dart SDK.
  */
+@SuppressWarnings("restriction")
 public class DartEarlyStartup implements IStartup {
 
   public DartEarlyStartup() {
@@ -30,6 +42,42 @@ public class DartEarlyStartup implements IStartup {
 
   @Override
   public void earlyStartup() {
+
+    // remove the debug action set toolbar items from Dart perspective
+    Display.getDefault().asyncExec(new Runnable() {
+      @Override
+      public void run() {
+        final IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        if (workbenchWindow != null) {
+          workbenchWindow.addPerspectiveListener(new PerspectiveAdapter() {
+
+            @Override
+            public void perspectiveActivated(IWorkbenchPage page,
+                IPerspectiveDescriptor perspectiveDescriptor) {
+              super.perspectiveActivated(page, perspectiveDescriptor);
+              if (perspectiveDescriptor.getId().indexOf("com.google.dart.tools.ui.DartPerspective") > -1) {
+                if (workbenchWindow.getActivePage() instanceof WorkbenchPage) {
+                  WorkbenchPage worbenchPage = (WorkbenchPage) workbenchWindow.getActivePage();
+                  // Get the perspective
+                  Perspective perspective = worbenchPage.findPerspective(perspectiveDescriptor);
+                  ArrayList<IActionSetDescriptor> toRemove = new ArrayList<IActionSetDescriptor>();
+                  if (perspective != null) {
+                    for (IActionSetDescriptor actionSetDescriptor : perspective.getAlwaysOnActionSets()) {
+                      if (actionSetDescriptor.getId().indexOf("org.eclipse.debug.ui.debugActionSet") > -1) {
+                        // Add the action set descriptor to the list of the action sets to remove
+                        toRemove.add(actionSetDescriptor);
+                      }
+                    }
+                    perspective.turnOffActionSets(toRemove.toArray(new IActionSetDescriptor[toRemove.size()]));
+                  }
+                }
+              }
+            }
+          });
+        }
+      }
+    });
+
     DartSdkManager manager = DartSdkManager.getManager();
 
     if (!manager.hasSdk()) {
