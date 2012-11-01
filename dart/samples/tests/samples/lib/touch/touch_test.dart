a5dde12423dd67c7b334c8d173334c432a64d310
gram@google.com <gram@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-11-01 23:58:26 +0000

    Build break: Fix unittest path in touch_test
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@14447 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/tests/samples/lib/touch/touch_test.dart
+++ dart/samples/tests/samples/lib/touch/touch_test.dart
@@ -2,13 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#library('touch_tests');
+library touchTests;
 
-#import('dart:html'); // TODO(rnystrom): Only needed to tell architecture.py
+import 'dart:html'; // TODO(rnystrom): Only needed to tell architecture.py
                       // that this is a web test. Come up with cleaner solution.
-#import('../../../../../pkg/unittest/unittest.dart');
-#import('../../../../../pkg/unittest/html_config.dart');
-#import('../../../../ui_lib/touch/touch.dart');
+import '../../../../../pkg/unittest/lib/unittest.dart';
+import '../../../../../pkg/unittest/lib/html_config.dart';
+import '../../../../ui_lib/touch/touch.dart';
 
 main() {
   useHtmlConfiguration();
