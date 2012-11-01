dart-archive/bleeding_edge-DEPRECATED-USE-SDK-INSTEAD

commit 72d5af006e88a858b854f8d0568ae371f0fb4ab3

- Represent strings internally in UTF-16 format, this makes it   compatible with webkit and will allow for easy externalization of   strings. One byte strings are retained for pure ASCII strings.   (The language specification was changed recently to reflect this as    follows "A string is a sequence of UTF-16 code units"). - Remove four byte string class and all references to it. - Rename some of the string functions in Dart API to make them   consistent and better describe the underlying functionality   Dart_NewString => Dart_NewStringFromCString   Dart_NewString8 => Dart_NewStringFromUTF8   Dart_NewString16 => Dart_NewStringFromUTF16   Dart_NewString32 => Dart_NewStringFromUTF32   Dart_NewExternalString8 => Dart_NewExternalUTF8String   Dart_NewExternalString16 => Dart_NewExternalUTF16String   Dart_NewExternalString32 => Dart_NewExternalUTF32String   Dart_StringGet8 => Dart_StringToUTF8   Dart_StringGet16 => Dart_StringToUTF16   Dart_StringToCString => Dart_StringToCString   Dart_IsString8 => Removed   Dart_IsString16 -> Removed   Dart_StringToBytes -> Removed   Dart_StringGet32 -> Removed Review URL: https://codereview.chromium.org//11318018
git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@14357 260f80e4-7a28-3924-810f-c04153c831b5

dart/runtime/bin/builtin_natives.cc
@@ -103,9 +103,11 @@ Dart_NativeFunction Builtin::NativeLookup(Dart_Handle name,
 // test/debug functionality in standalone dart mode.
 
 void Builtin::PrintString(FILE* out, Dart_Handle str) {
-  const uint8_t* characters = NULL;
-  intptr_t length;
-  Dart_Handle result = Dart_StringToBytes(str, &characters, &length);
+  intptr_t length = 0;
+  Dart_Handle result = Dart_StringLength(str, &length);
+  DART_CHECK_VALID(result);
+  uint8_t* chars = reinterpret_cast<uint8_t*>(malloc(length * sizeof(uint8_t)));
+  result = Dart_StringToUTF8(str, chars, &length);
   if (Dart_IsError(result)) {
     // TODO(turnidge): Consider propagating some errors here.  What if
     // an isolate gets interrupted by the embedder in the middle of
@@ -113,10 +115,11 @@ void Builtin::PrintString(FILE* out, Dart_Handle str) {
     // interrupt.
     fputs(Dart_GetError(result), out);
   } else {
-    fwrite(characters, sizeof(*characters), length, out);
+    fwrite(chars, sizeof(*chars), length, out);
   }
   fputc('\n', out);
   fflush(out);
+  free(chars);
 }
 
 

dart/runtime/bin/builtin.cc
@@ -26,7 +26,7 @@ Dart_Handle Builtin::Source(BuiltinLibraryId id) {
   ASSERT((sizeof(builtin_libraries_) / sizeof(builtin_lib_props)) ==
          kInvalidLibrary);
   ASSERT(id >= kBuiltinLibrary && id < kInvalidLibrary);
-  return Dart_NewString(builtin_libraries_[id].source_);
+  return DartUtils::NewString(builtin_libraries_[id].source_);
 }
 
 
@@ -39,7 +39,7 @@ Dart_Handle Builtin::LoadAndCheckLibrary(BuiltinLibraryId id) {
   ASSERT((sizeof(builtin_libraries_) / sizeof(builtin_lib_props)) ==
          kInvalidLibrary);
   ASSERT(id >= kBuiltinLibrary && id < kInvalidLibrary);
-  Dart_Handle url = Dart_NewString(builtin_libraries_[id].url_);
+  Dart_Handle url = DartUtils::NewString(builtin_libraries_[id].url_);
   Dart_Handle library = Dart_LookupLibrary(url);
   if (Dart_IsError(library)) {
     library = Dart_LoadLibrary(url, Source(id));
@@ -49,9 +49,10 @@ Dart_Handle Builtin::LoadAndCheckLibrary(BuiltinLibraryId id) {
     }
     if (builtin_libraries_[id].patch_url_ != NULL) {
       ASSERT(builtin_libraries_[id].patch_source_ != NULL);
-      Dart_Handle patch_url = Dart_NewString(builtin_libraries_[id].patch_url_);
+      Dart_Handle patch_url =
+          DartUtils::NewString(builtin_libraries_[id].patch_url_);
       Dart_Handle patch_source =
-          Dart_NewString(builtin_libraries_[id].patch_source_);
+          DartUtils::NewString(builtin_libraries_[id].patch_source_);
       DART_CHECK_VALID(Dart_LoadPatch(library, patch_url, patch_source));
     }
   }

dart/runtime/bin/builtin_nolib.cc
@@ -33,7 +33,7 @@ void Builtin::SetNativeResolver(BuiltinLibraryId id) {
          kInvalidLibrary);
   ASSERT(id >= kBuiltinLibrary && id < kInvalidLibrary);
   if (builtin_libraries_[id].has_natives_) {
-    Dart_Handle url = Dart_NewString(builtin_libraries_[id].url_);
+    Dart_Handle url = DartUtils::NewString(builtin_libraries_[id].url_);
     Dart_Handle library = Dart_LookupLibrary(url);
     ASSERT(!Dart_IsError(library));
     // Setup the native resolver for built in library functions.
@@ -46,7 +46,7 @@ Dart_Handle Builtin::LoadAndCheckLibrary(BuiltinLibraryId id) {
   ASSERT((sizeof(builtin_libraries_) / sizeof(builtin_lib_props)) ==
          kInvalidLibrary);
   ASSERT(id >= kBuiltinLibrary && id < kInvalidLibrary);
-  Dart_Handle url = Dart_NewString(builtin_libraries_[id].url_);
+  Dart_Handle url = DartUtils::NewString(builtin_libraries_[id].url_);
   Dart_Handle library = Dart_LookupLibrary(url);
   if (Dart_IsError(library)) {
     ASSERT(id > kUtfLibrary);

dart/runtime/bin/common.cc
@@ -24,16 +24,16 @@ void FUNCTION_NAME(Common_IsBuiltinList)(Dart_NativeArguments args) {
   // look them up and cache them now.
   if (object_array_class == NULL) {
     Dart_Handle core_lib =
-        Dart_LookupLibrary(Dart_NewString("dart:core"));
+        Dart_LookupLibrary(Dart_NewStringFromCString("dart:core"));
     ASSERT(!Dart_IsError(core_lib));
     object_array_class =
-        Dart_GetClass(core_lib, Dart_NewString("_ObjectArray"));
+        Dart_GetClass(core_lib, Dart_NewStringFromCString("_ObjectArray"));
     ASSERT(!Dart_IsError(object_array_class));
     immutable_array_class =
-        Dart_GetClass(core_lib, Dart_NewString("_ImmutableArray"));
+        Dart_GetClass(core_lib, Dart_NewStringFromCString("_ImmutableArray"));
     ASSERT(!Dart_IsError(immutable_array_class));
-    growable_object_array_class =
-        Dart_GetClass(core_lib, Dart_NewString("_GrowableObjectArray"));
+    growable_object_array_class = Dart_GetClass(
+        core_lib, Dart_NewStringFromCString("_GrowableObjectArray"));
     ASSERT(!Dart_IsError(growable_object_array_class));
     // Update the cache.
     isolate_data->object_array_class =

dart/runtime/bin/crypto.cc
@@ -13,7 +13,8 @@ void FUNCTION_NAME(Crypto_GetRandomBytes)(Dart_NativeArguments args) {
   Dart_Handle count_obj = Dart_GetNativeArgument(args, 0);
   int64_t count = 0;
   if (!DartUtils::GetInt64Value(count_obj, &count)) {
-    Dart_Handle error = Dart_NewString("Invalid argument, must be an int.");
+    Dart_Handle error =
+        DartUtils::NewString("Invalid argument, must be an int.");
     Dart_ThrowException(error);
   }
   uint8_t* buffer = new uint8_t[count];
@@ -25,7 +26,7 @@ void FUNCTION_NAME(Crypto_GetRandomBytes)(Dart_NativeArguments args) {
   Dart_Handle result = Dart_NewByteArray(count);
   if (Dart_IsError(result)) {
     delete[] buffer;
-    Dart_Handle error = Dart_NewString("Failed to allocate storage.");
+    Dart_Handle error = DartUtils::NewString("Failed to allocate storage.");
     Dart_ThrowException(error);
   }
   Dart_ListSetAsBytes(result, 0, buffer, count);
@@ -33,4 +34,3 @@ void FUNCTION_NAME(Crypto_GetRandomBytes)(Dart_NativeArguments args) {
   delete[] buffer;
   Dart_ExitScope();
 }
-

dart/runtime/bin/dartutils.cc
@@ -99,15 +99,15 @@ void DartUtils::SetIntegerField(Dart_Handle handle,
                                 const char* name,
                                 intptr_t val) {
   Dart_Handle result = Dart_SetField(handle,
-                                     Dart_NewString(name),
+                                     NewString(name),
                                      Dart_NewInteger(val));
   ASSERT(!Dart_IsError(result));
 }
 
 
 intptr_t DartUtils::GetIntegerField(Dart_Handle handle,
-                                            const char* name) {
-  Dart_Handle result = Dart_GetField(handle, Dart_NewString(name));
+                                    const char* name) {
+  Dart_Handle result = Dart_GetField(handle, NewString(name));
   ASSERT(!Dart_IsError(result));
   intptr_t value = DartUtils::GetIntegerValue(result);
   return value;
@@ -117,9 +117,7 @@ intptr_t DartUtils::GetIntegerField(Dart_Handle handle,
 void DartUtils::SetStringField(Dart_Handle handle,
                                const char* name,
                                const char* val) {
-  Dart_Handle result = Dart_SetField(handle,
-                                     Dart_NewString(name),
-                                     Dart_NewString(val));
+  Dart_Handle result = Dart_SetField(handle, NewString(name), NewString(val));
   ASSERT(!Dart_IsError(result));
 }
 
@@ -174,7 +172,7 @@ Dart_Handle DartUtils::CanonicalizeURL(CommandLineOptions* url_mapping,
   if (Dart_IsError(library_url)) {
     return Dart_Error("accessing library url failed");
   }
-  if (!Dart_IsString8(library_url)) {
+  if (!Dart_IsString(library_url)) {
     return Dart_Error("library url is not a string");
   }
   const char* library_url_str = NULL;
@@ -191,7 +189,7 @@ Dart_Handle DartUtils::CanonicalizeURL(CommandLineOptions* url_mapping,
   }
   // Calculate the canonical path.
   const char* canon_url_str = GetCanonicalPath(library_url_str, url_str);
-  Dart_Handle canon_url = Dart_NewString(canon_url_str);
+  Dart_Handle canon_url = NewString(canon_url_str);
   free(const_cast<char*>(canon_url_str));
 
   return canon_url;
@@ -210,18 +208,18 @@ Dart_Handle DartUtils::ReadStringFromFile(const char* filename) {
     return Dart_Error(error_msg);
   }
   intptr_t len = file->Length();
-  char* text_buffer = reinterpret_cast<char*>(malloc(len + 1));
+  uint8_t* text_buffer = reinterpret_cast<uint8_t*>(malloc(len));
   if (text_buffer == NULL) {
     delete file;
     return Dart_Error("Unable to allocate buffer");
   }
   if (!file->ReadFully(text_buffer, len)) {
     delete file;
+    free(text_buffer);
     return Dart_Error("Unable to fully read contents");
   }
-  text_buffer[len] = '\0';
   delete file;
-  Dart_Handle str = Dart_NewString(text_buffer);
+  Dart_Handle str = Dart_NewStringFromUTF8(text_buffer, len);
   free(text_buffer);
   return str;
 }
@@ -231,11 +229,13 @@ static Dart_Handle ResolveScriptUri(Dart_Handle script_uri,
                                     Dart_Handle builtin_lib) {
   const int kNumArgs = 3;
   Dart_Handle dart_args[kNumArgs];
-  dart_args[0] = Dart_NewString(DartUtils::original_working_directory);
+  dart_args[0] = DartUtils::NewString(DartUtils::original_working_directory);
   dart_args[1] = script_uri;
   dart_args[2] = (IsWindowsHost() ? Dart_True() : Dart_False());
-  return Dart_Invoke(
-      builtin_lib, Dart_NewString("_resolveScriptUri"), kNumArgs, dart_args);
+  return Dart_Invoke(builtin_lib,
+                     DartUtils::NewString("_resolveScriptUri"),
+                     kNumArgs,
+                     dart_args);
 }
 
 
@@ -246,7 +246,10 @@ static Dart_Handle FilePathFromUri(Dart_Handle script_uri,
   dart_args[0] = script_uri;
   dart_args[1] = (IsWindowsHost() ? Dart_True() : Dart_False());
   Dart_Handle script_path = Dart_Invoke(
-      builtin_lib, Dart_NewString("_filePathFromUri"), kNumArgs, dart_args);
+      builtin_lib,
+      DartUtils::NewString("_filePathFromUri"),
+      kNumArgs,
+      dart_args);
   return script_path;
 }
 
@@ -257,7 +260,7 @@ Dart_Handle DartUtils::LibraryTagHandler(Dart_LibraryTag tag,
   if (!Dart_IsLibrary(library)) {
     return Dart_Error("not a library");
   }
-  if (!Dart_IsString8(url)) {
+  if (!Dart_IsString(url)) {
     return Dart_Error("url is not a string");
   }
   const char* url_string = NULL;
@@ -285,7 +288,7 @@ Dart_Handle DartUtils::LibraryTagHandler(Dart_LibraryTag tag,
     dart_args[0] = library_url;
     dart_args[1] = url;
     return Dart_Invoke(
-        builtin_lib, Dart_NewString("_resolveUri"), kNumArgs, dart_args);
+        builtin_lib, NewString("_resolveUri"), kNumArgs, dart_args);
   }
   if (is_dart_scheme_url) {
     ASSERT(tag == kImportTag);
@@ -346,8 +349,7 @@ static Dart_Handle ReadSource(Dart_Handle script_uri,
 Dart_Handle DartUtils::LoadScript(const char* script_uri,
                                   Dart_Handle builtin_lib) {
   Dart_Handle resolved_script_uri;
-  resolved_script_uri = ResolveScriptUri(Dart_NewString(script_uri),
-                                         builtin_lib);
+  resolved_script_uri = ResolveScriptUri(NewString(script_uri), builtin_lib);
   if (Dart_IsError(resolved_script_uri)) {
     return resolved_script_uri;
   }
@@ -392,35 +394,35 @@ Dart_Handle DartUtils::LoadSource(CommandLineOptions* url_mapping,
 Dart_Handle DartUtils::PrepareForScriptLoading(const char* package_root,
                                                Dart_Handle builtin_lib) {
   // Setup the corelib 'print' function.
-  Dart_Handle print =
-      Dart_Invoke(builtin_lib, Dart_NewString("_getPrintClosure"), 0, 0);
-  Dart_Handle corelib = Dart_LookupLibrary(Dart_NewString("dart:core"));
+  Dart_Handle print = Dart_Invoke(
+      builtin_lib, NewString("_getPrintClosure"), 0, 0);
+  Dart_Handle corelib = Dart_LookupLibrary(NewString("dart:core"));
   Dart_Handle result = Dart_SetField(corelib,
-                                     Dart_NewString("_printClosure"), print);
+                                     NewString("_printClosure"),
+                                     print);
 
   // Setup the 'timer' factory.
-  Dart_Handle url = Dart_NewString(kIsolateLibURL);
+  Dart_Handle url = NewString(kIsolateLibURL);
   DART_CHECK_VALID(url);
   Dart_Handle isolate_lib = Dart_LookupLibrary(url);
   DART_CHECK_VALID(isolate_lib);
   Dart_Handle io_lib = Builtin::LoadAndCheckLibrary(Builtin::kIOLibrary);
   Dart_Handle timer_closure =
-      Dart_Invoke(io_lib, Dart_NewString("_getTimerFactoryClosure"), 0, NULL);
+      Dart_Invoke(io_lib, NewString("_getTimerFactoryClosure"), 0, NULL);
   Dart_Handle args[1];
   args[0] = timer_closure;
-  DART_CHECK_VALID(Dart_Invoke(isolate_lib,
-                               Dart_NewString("_setTimerFactoryClosure"),
-                               1, args));
+  DART_CHECK_VALID(Dart_Invoke(
+      isolate_lib, NewString("_setTimerFactoryClosure"), 1, args));
 
   // Set up package root if specified.
   if (package_root != NULL) {
-    result = Dart_NewString(package_root);
+    result = NewString(package_root);
     if (!Dart_IsError(result)) {
       const int kNumArgs = 1;
       Dart_Handle dart_args[kNumArgs];
       dart_args[0] = result;
       return Dart_Invoke(builtin_lib,
-                         Dart_NewString("_setPackageRoot"),
+                         NewString("_setPackageRoot"),
                          kNumArgs,
                          dart_args);
     }
@@ -492,16 +494,16 @@ Dart_Handle DartUtils::NewDartOSError() {
 
 Dart_Handle DartUtils::NewDartOSError(OSError* os_error) {
   // Create a Dart OSError object with the information retrieved from the OS.
-  Dart_Handle url = Dart_NewString("dart:io");
+  Dart_Handle url = NewString("dart:io");
   if (Dart_IsError(url)) return url;
   Dart_Handle lib = Dart_LookupLibrary(url);
   if (Dart_IsError(lib)) return lib;
-  Dart_Handle class_name = Dart_NewString("OSError");
+  Dart_Handle class_name = NewString("OSError");
   if (Dart_IsError(class_name)) return class_name;
   Dart_Handle clazz = Dart_GetClass(lib, class_name);
   if (Dart_IsError(clazz)) return clazz;
   Dart_Handle args[2];
-  args[0] = Dart_NewString(os_error->message());
+  args[0] = NewString(os_error->message());
   if (Dart_IsError(args[0])) return args[0];
   args[1] = Dart_NewInteger(os_error->code());
   if (Dart_IsError(args[1])) return args[1];

dart/runtime/bin/dartutils.h
@@ -110,6 +110,13 @@ class DartUtils {
   // Create a new Dart OSError object with the provided OS error.
   static Dart_Handle NewDartOSError(OSError* os_error);
 
+  // Create a new Dart String object from a C String.
+  static Dart_Handle NewString(const char* str) {
+    ASSERT((str != NULL) && (strlen(str) != 0));
+    return Dart_NewStringFromUTF8(reinterpret_cast<const uint8_t*>(str),
+                                  strlen(str));
+  }
+
   static void SetOriginalWorkingDirectory();
 
   // Global state that stores the original working directory..

dart/runtime/bin/dbg_message.cc
@@ -109,11 +109,11 @@ static void FormatEncodedString(dart::TextBuffer* buf, Dart_Handle str) {
   intptr_t str_len = 0;
   Dart_Handle res = Dart_StringLength(str, &str_len);
   ASSERT_NOT_ERROR(res);
-  uint32_t* codepoints =
-      reinterpret_cast<uint32_t*>(malloc(str_len * sizeof(uint32_t)));
+  uint16_t* codepoints =
+      reinterpret_cast<uint16_t*>(malloc(str_len * sizeof(uint16_t)));
   ASSERT(codepoints != NULL);
   intptr_t actual_len = str_len;
-  res = Dart_StringGet32(str, codepoints, &actual_len);
+  res = Dart_StringToUTF16(str, codepoints, &actual_len);
   ASSERT_NOT_ERROR(res);
   ASSERT(str_len == actual_len);
   buf->AddChar('\"');
@@ -692,7 +692,7 @@ bool DbgMessage::HandleGetSourceCmd(DbgMessage* in_msg) {
   intptr_t lib_id = msg_parser.GetIntParam("libraryId");
   char* url_chars = msg_parser.GetStringParam("url");
   ASSERT(url_chars != NULL);
-  Dart_Handle url = Dart_NewString(url_chars);
+  Dart_Handle url = DartUtils::NewString(url_chars);
   ASSERT_NOT_ERROR(url);
   free(url_chars);
   url_chars = NULL;
@@ -732,7 +732,7 @@ bool DbgMessage::HandleSetBpCmd(DbgMessage* in_msg) {
   int msg_id = msg_parser.MessageId();
   char* url_chars = msg_parser.GetStringParam("url");
   ASSERT(url_chars != NULL);
-  Dart_Handle url = Dart_NewString(url_chars);
+  Dart_Handle url = DartUtils::NewString(url_chars);
   ASSERT_NOT_ERROR(url);
   free(url_chars);
   url_chars = NULL;

commit 785acfc25a492bbeed016e0442bbf2fdc3807f16

Fix error in debug printing from the Dart executable, introduced in r14357.
BUG=

Review URL: https://codereview.chromium.org//11361032

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@14399 260f80e4-7a28-3924-810f-c04153c831b5

dart/runtime/bin/builtin_natives.cc
@@ -103,11 +103,9 @@ Dart_NativeFunction Builtin::NativeLookup(Dart_Handle name,
 // test/debug functionality in standalone dart mode.
 
 void Builtin::PrintString(FILE* out, Dart_Handle str) {
-  intptr_t length = 0;
-  Dart_Handle result = Dart_StringLength(str, &length);
-  DART_CHECK_VALID(result);
-  uint8_t* chars = reinterpret_cast<uint8_t*>(malloc(length * sizeof(uint8_t)));
-  result = Dart_StringToUTF8(str, chars, &length);
+  const char* chars = NULL;
+
+  Dart_Handle result = Dart_StringToCString(str, &chars);
   if (Dart_IsError(result)) {
     // TODO(turnidge): Consider propagating some errors here.  What if
     // an isolate gets interrupted by the embedder in the middle of
@@ -115,11 +113,11 @@ void Builtin::PrintString(FILE* out, Dart_Handle str) {
     // interrupt.
     fputs(Dart_GetError(result), out);
   } else {
+    intptr_t length = strlen(chars);
     fwrite(chars, sizeof(*chars), length, out);
   }
   fputc('\n', out);
   fflush(out);
-  free(chars);
 }
 
 