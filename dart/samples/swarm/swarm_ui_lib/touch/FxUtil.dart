9e4152fe9b76628a8246665b7289cf0761f42dcf
ricow@google.com <ricow@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-12-18 08:12:44 +0000

    Revert revision 16240
    This is causing compilation errors on dartium.
    
    Anton/Peter: could you coordinate relanding this with the code removed from multivm bindings?
    Review URL: https://codereview.chromium.org//11610006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@16246 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/swarm/swarm_ui_lib/touch/FxUtil.dart
+++ dart/samples/swarm/swarm_ui_lib/touch/FxUtil.dart
@@ -67,22 +67,12 @@ class FxUtil {
    * changed.
    */
   static Coordinate computeRelativePosition(Element element, Element target) {
-    final elementPoint = computePagePosition(element);
-    final targetPoint = computePagePosition(target);
-    return new Coordinate(elementPoint.x - targetPoint.x,
-        elementPoint.y - targetPoint.y);
-  }
-
-  /** Computes the position of an element on the page. */
-  static Coordinate computePagePosition(Element element) {
-    num left = 0;
-    num top = 0;
-    do {
-      left += element.offsetLeft;
-      top += element.offsetTop;
-      element = element.offsetParent;
-    } while (element != null);
-    return new Coordinate(left, top);
+    final testPoint = new Point(0, 0);
+    final pagePoint =
+        window.webkitConvertPointFromNodeToPage(element, testPoint);
+    final pointRelativeToTarget =
+        window.webkitConvertPointFromPageToNode(target, pagePoint);
+    return new Coordinate(pointRelativeToTarget.x, pointRelativeToTarget.y);
   }
 
   /** Clear a -webkit-transform from an element. */
