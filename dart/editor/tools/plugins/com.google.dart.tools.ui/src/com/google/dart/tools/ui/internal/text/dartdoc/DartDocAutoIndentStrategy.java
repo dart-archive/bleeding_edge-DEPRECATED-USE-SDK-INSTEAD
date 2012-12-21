a43c865070af614d6051c5b0a4e1caa81906da01
brianwilkerson@google.com <brianwilkerson@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2012-12-21 23:23:09 +0000

    Fix for issue 4343 Review URL: https://codereview.chromium.org//11570075
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@16457 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/dartdoc/DartDocAutoIndentStrategy.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/dartdoc/DartDocAutoIndentStrategy.java
@@ -19,13 +19,14 @@ import org.eclipse.jface.text.DefaultIndentLineAutoEditStrategy;
 import org.eclipse.jface.text.DocumentCommand;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITypedRegion;
+import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.TextUtilities;
 
 /**
  * A simple auto indent strategy for Dartdoc comments (as well as regular multi-line comments).
  */
 public class DartDocAutoIndentStrategy extends DefaultIndentLineAutoEditStrategy {
-  @SuppressWarnings("unused")
   private String partitioning;
 
   public DartDocAutoIndentStrategy(String partitioning) {
@@ -70,20 +71,21 @@ public class DartDocAutoIndentStrategy extends DefaultIndentLineAutoEditStrategy
    * @param c the command to deal with
    */
   private void autoIndentAfterNewLine(IDocument d, DocumentCommand c) {
-    if (c.offset == -1 || d.getLength() == 0) {
+    int offset = c.offset;
+    if (offset == -1 || d.getLength() == 0) {
       return;
     }
 
     try {
       // find start of line
-      int p = (c.offset == d.getLength() ? c.offset - 1 : c.offset);
+      int p = (offset == d.getLength() ? offset - 1 : offset);
       IRegion info = d.getLineInformationOfOffset(p);
       int start = info.getOffset();
 
       // find white spaces
-      int end = findEndOfWhiteSpace(d, start, c.offset);
+      int end = findEndOfWhiteSpace(d, start, offset);
 
-      String startStr = getLineStart(d, start, c.offset, true);
+      String startStr = getLineStart(d, start, offset, true);
 
       StringBuffer buf = new StringBuffer(c.text);
 
@@ -94,7 +96,7 @@ public class DartDocAutoIndentStrategy extends DefaultIndentLineAutoEditStrategy
 
       if (startStr.endsWith("*")) {
         // look for post '*' whitespace and preserve it (for 4-space code indents)
-        String postWs = getLineStart(d, end + 1, c.offset, false);
+        String postWs = getLineStart(d, end + 1, offset, false);
 
         if (postWs.length() > 0) {
           buf.append("*");
@@ -106,6 +108,22 @@ public class DartDocAutoIndentStrategy extends DefaultIndentLineAutoEditStrategy
       } else if (startStr.endsWith("/")) {
         // indent one more then the /** indent
         buf.append(" * ");
+        if (isNewComment(d, offset)) {
+          c.shiftsCaret = false;
+          c.caretOffset = offset + buf.length();
+          String lineDelimiter = TextUtilities.getDefaultLineDelimiter(d);
+
+          int eolOffset = start + info.getLength();
+          int replacementLength = eolOffset - p;
+          String restOfLine = d.get(p, replacementLength);
+          IRegion prefix = findPrefixRange(d, info);
+          String indentation = d.get(prefix.getOffset(), prefix.getLength());
+          String endTag = lineDelimiter + indentation + " */"; //$NON-NLS-1$
+
+          c.length = replacementLength;
+          buf.append(restOfLine);
+          buf.append(endTag);
+        }
       }
 
       c.text = buf.toString();
@@ -115,4 +133,70 @@ public class DartDocAutoIndentStrategy extends DefaultIndentLineAutoEditStrategy
     }
   }
 
+  /**
+   * Returns the range of the Javadoc prefix on the given line in <code>document</code>. The prefix
+   * greedily matches the following regex pattern: <code>\w*\*\w*</code>, that is, any number of
+   * whitespace characters, followed by an asterisk ('*'), followed by any number of whitespace
+   * characters.
+   * 
+   * @param document the document to which <code>line</code> refers
+   * @param line the line from which to extract the prefix range
+   * @return an <code>IRegion</code> describing the range of the prefix on the given line
+   * @throws BadLocationException if accessing the document fails
+   */
+  private IRegion findPrefixRange(IDocument document, IRegion line) throws BadLocationException {
+    int lineOffset = line.getOffset();
+    int lineEnd = lineOffset + line.getLength();
+    int indentEnd = findEndOfWhiteSpace(document, lineOffset, lineEnd);
+    if (indentEnd < lineEnd && document.getChar(indentEnd) == '*') {
+      indentEnd++;
+      while (indentEnd < lineEnd && document.getChar(indentEnd) == ' ') {
+        indentEnd++;
+      }
+    }
+    return new Region(lineOffset, indentEnd - lineOffset);
+  }
+
+  /**
+   * Guesses if the command operates within a newly created Javadoc comment or not. If in doubt, it
+   * will assume that the Javadoc is new.
+   * 
+   * @param document the document
+   * @param commandOffset the command offset
+   * @return <code>true</code> if the comment should be closed, <code>false</code> if not
+   */
+  private boolean isNewComment(IDocument document, int commandOffset) {
+
+    try {
+      int lineIndex = document.getLineOfOffset(commandOffset) + 1;
+      if (lineIndex >= document.getNumberOfLines()) {
+        return true;
+      }
+
+      IRegion line = document.getLineInformation(lineIndex);
+      ITypedRegion partition = TextUtilities.getPartition(
+          document,
+          partitioning,
+          commandOffset,
+          false);
+      int partitionEnd = partition.getOffset() + partition.getLength();
+      if (line.getOffset() >= partitionEnd) {
+        return false;
+      }
+
+      if (document.getLength() == partitionEnd) {
+        return true; // partition goes to end of document - probably a new comment
+      }
+
+      String comment = document.get(partition.getOffset(), partition.getLength());
+      if (comment.indexOf("/*", 2) != -1) {
+        return true; // enclosed another comment -> probably a new comment
+      }
+
+      return false;
+
+    } catch (BadLocationException e) {
+      return false;
+    }
+  }
 }
