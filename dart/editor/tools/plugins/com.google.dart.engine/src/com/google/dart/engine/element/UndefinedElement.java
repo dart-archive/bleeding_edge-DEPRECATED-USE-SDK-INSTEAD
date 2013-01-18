34fd77a2f8e6d9d4519953b6f6bc986c28370ca7
brianwilkerson@google.com <brianwilkerson@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-01-18 21:15:19 +0000

    Add marker interface for undefined names Review URL: https://codereview.chromium.org//12025024
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@17314 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/element/UndefinedElement.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2013, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.engine.element;
+
+/**
+ * The interface {@code UndefinedElement} defines the behavior of pseudo-elements that represent
+ * names that are undefined. This situation is not allowed by the language, so objects implementing
+ * this interface always represent an error. As a result, most of the normal operations on elements
+ * do not make sense and will return useless results.
+ */
+public interface UndefinedElement extends Element {
+}
