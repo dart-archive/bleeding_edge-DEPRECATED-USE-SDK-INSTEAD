3e26bfdc337a26fa58d32b42c6038a45b87d5c1a
brianwilkerson@google.com <brianwilkerson@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-01-23 15:43:45 +0000

    Add error listener for use within analysis context Review URL: https://codereview.chromium.org//12035036
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@17464 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/internal/context/RecordingErrorListener.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2013, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.engine.internal.context;
+
+import com.google.dart.engine.error.AnalysisError;
+import com.google.dart.engine.error.AnalysisErrorListener;
+
+/**
+ * Instances of the class {@code RecordingErrorListener} implement an error listener that will
+ * record the errors that are reported to it in a way that is appropriate for caching those errors
+ * within an analysis context.
+ */
+public class RecordingErrorListener implements AnalysisErrorListener {
+  @Override
+  public void onError(AnalysisError event) {
+    // TODO(brianwilkerson) Record the error
+  }
+}
