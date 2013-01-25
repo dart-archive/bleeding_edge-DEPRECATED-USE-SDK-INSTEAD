5a4a2ee5466d56eea1b835988816cb6b9d2d2008
sigmund@google.com <sigmund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-01-25 18:26:55 +0000

    Fix todomvc sample
    Review URL: https://codereview.chromium.org//12049093
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@17647 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/third_party/todomvc/build.dart
+++ dart/samples/third_party/todomvc/build.dart
@@ -6,4 +6,6 @@
 import 'dart:io';
 import 'package:web_ui/component_build.dart';
 
-void main() => build(new Options().arguments, ['web/main.html']);
+void main() {
+  build(new Options().arguments, ['web/main.html']);
+}
