bb2a3d6ed55e0d6551bd4a910da96cdbeabe4a18
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-01-30 20:41:29 +0000

    add pub supprt to new command line application Review URL: https://codereview.chromium.org//12090074
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@17873 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/generator/CommandLineSample.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/generator/CommandLineSample.java
@@ -28,10 +28,14 @@ public class CommandLineSample extends AbstractSample {
     List<String[]> templates = new ArrayList<String[]>();
 
     templates.add(new String[] {
-        "{name.lower}.dart", "void main() {\n  print(\"Hello, World!\");\n}\n"});
+        "pubspec.yaml",
+        "name: {name}\ndescription: A sample command-line application\n#dependencies:\n#  unittest: any\n"});
+
+    templates.add(new String[] {
+        "bin/{name.lower}.dart", "void main() {\n  print(\"Hello, World!\");\n}\n"});
 
     setTemplates(templates);
-    setMainFile("{name.lower}.dart");
+    setMainFile("bin/{name.lower}.dart");
   }
 
 }
