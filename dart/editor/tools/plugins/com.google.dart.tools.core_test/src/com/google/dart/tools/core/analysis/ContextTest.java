f0715843cdc4e1f0833ab36593362ff218c5a85a
lrn@google.com <lrn@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-02-04 13:27:19 +0000

    Disable Editor test that fails on correct code.
    BUG=http://dartbug.com/8292
    
    Review URL: https://codereview.chromium.org//12185017
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@18047 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/analysis/ContextTest.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/analysis/ContextTest.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2012, the Dart project authors.
- * 
+ *
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -172,7 +172,7 @@ public class ContextTest extends AbstractDartCoreTest {
     assertParsed(2, 1);
   }
 
-  public void test_resolve() throws Exception {
+  public void bug8292_test_resolve() throws Exception {
     LibraryUnit libraryUnit = context.resolve(libraryFile, FIVE_MINUTES_MS);
     assertEquals("Money", libraryUnit.getName());
     assertTrue(listener.getParsedCount() > 10);
