23664be7a72f0800c83b715d82fd2cbd19452937
brianwilkerson@google.com <brianwilkerson@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-02-19 17:53:22 +0000

    Unify utilities for error reporting Review URL: https://codereview.chromium.org//12298044
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@18709 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/internal/error/ErrorReporter.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2013, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.engine.internal.error;
+
+import com.google.dart.engine.ast.ASTNode;
+import com.google.dart.engine.error.AnalysisError;
+import com.google.dart.engine.error.AnalysisErrorListener;
+import com.google.dart.engine.error.ErrorCode;
+import com.google.dart.engine.scanner.Token;
+import com.google.dart.engine.source.Source;
+
+/**
+ * Instances of the class {@code ErrorReporter} wrap an error listener with utility methods used to
+ * create the errors being reported.
+ */
+public class ErrorReporter {
+  /**
+   * The error listener to which errors will be reported.
+   */
+  private AnalysisErrorListener errorListener;
+
+  /**
+   * The default source to be used when reporting errors.
+   */
+  private Source defaultSource;
+
+  /**
+   * The source to be used when reporting errors.
+   */
+  private Source source;
+
+  /**
+   * Initialize a newly created error reporter that will report errors to the given listener.
+   * 
+   * @param errorListener the error listener to which errors will be reported
+   * @param defaultSource the default source to be used when reporting errors
+   */
+  public ErrorReporter(AnalysisErrorListener errorListener, Source defaultSource) {
+    if (errorListener == null) {
+      throw new IllegalArgumentException("An error listener must be provided");
+    } else if (defaultSource == null) {
+      throw new IllegalArgumentException("A default source must be provided");
+    }
+    this.errorListener = errorListener;
+    this.defaultSource = defaultSource;
+    this.source = defaultSource;
+  }
+
+  /**
+   * Report an error with the given error code and arguments.
+   * 
+   * @param errorCode the error code of the error to be reported
+   * @param node the node specifying the location of the error
+   * @param arguments the arguments to the error, used to compose the error message
+   */
+  public void reportError(ErrorCode errorCode, ASTNode node, Object... arguments) {
+    errorListener.onError(new AnalysisError(
+        source,
+        node.getOffset(),
+        node.getLength(),
+        errorCode,
+        arguments));
+  }
+
+  /**
+   * Report an error with the given error code and arguments.
+   * 
+   * @param errorCode the error code of the error to be reported
+   * @param token the token specifying the location of the error
+   * @param arguments the arguments to the error, used to compose the error message
+   */
+  public void reportError(ErrorCode errorCode, Token token, Object... arguments) {
+    errorListener.onError(new AnalysisError(
+        source,
+        token.getOffset(),
+        token.getLength(),
+        errorCode,
+        arguments));
+  }
+
+  /**
+   * Set the source to be used when reporting errors. Setting the source to {@code null} will cause
+   * the default source to be used.
+   * 
+   * @param source the source to be used when reporting errors
+   */
+  public void setSource(Source source) {
+    this.source = source == null ? defaultSource : source;
+  }
+}
