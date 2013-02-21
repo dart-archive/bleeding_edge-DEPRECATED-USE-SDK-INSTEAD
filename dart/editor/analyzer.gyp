4fd8d6fa0fd788c27dccb7a466094b255db6fb7d
ricow@google.com <ricow@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-02-21 08:25:18 +0000

    Add support for building the new analyzer using gyp.
    Do we have a name for the new analyzer? I don't really like the new prefix I use for building and naming.
    
    Also, I don't particularly like the way I handle the jar files, but I
    did not find a way to easily make a new string out of a list of
    strings using a non space separator.
    
    Additionally, I currently copy in the dependent on jar files so that
    we can easily wrap this up, I could make the manifest just refer to
    the the files relative to tbe build dir, but that is just asking for
    trouble in the long term IMHO.
    Review URL: https://codereview.chromium.org//12262040
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@18804 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/analyzer.gyp
@@ -0,0 +1,58 @@
+# Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+{
+  'variables': {
+    'analyzer_name': 'new_analyzer',
+     # When changing the jar files that we depend on please change the list
+     # below.
+     'dependent_jar_files': [
+       '../third_party/commons-lang/3.1/commons-lang3-3.1.jar',
+       '../third_party/json/r2_20080312/json.jar',
+       '../third_party/guava/r13/guava-13.0.1.jar',
+       '../third_party/args4j/2.0.12/args4j-2.0.12.jar'
+     ],
+  },
+  'targets': [
+    {
+      'target_name': 'analyzer',
+      'type': 'none',
+      'variables': {
+        'java_source_files': [
+          '<!@(["python", "../tools/list_files.py", "\\.java$", "tools/plugins/com.google.dart.command.analyze"])',
+          '<!@(["python", "../tools/list_files.py", "\\.java$", "tools/plugins/com.google.dart.engine"])',
+        ],
+        # The file where we write the class path to be used in the manifest.
+        'class_path_file': '<(PRODUCT_DIR)/<(analyzer_name)/classpath_file',
+      },
+      'actions': [
+        {
+          'action_name': 'create_analyzer',
+          'inputs': [
+            '<@(java_source_files)',
+            '<@(dependent_jar_files)',
+            'tools/compile_analyzer.py',
+          ],
+          'outputs': [
+            '<(PRODUCT_DIR)/<(analyzer_name)/<(analyzer_name).jar',
+          ],
+          'action': [
+            'python',
+            'tools/compile_analyzer.py',
+            '--output_dir', '<(PRODUCT_DIR)/<(analyzer_name)/',
+            '--jar_file_name', '<(analyzer_name).jar',
+            '--jar_entry_directory', 'com',
+            '--dependent_jar_files', '"<@(dependent_jar_files)"',
+            '--entry_point', 'com.google.dart.command.analyze.AnalyzerMain',
+            '--class_path_file', 'classpath_file',
+            '<@(java_source_files)',
+          ],
+          'message': 'Creating <(_outputs).',
+        },
+      ],
+    },
+  ]
+}
+
+
---
+++ dart/editor/tools/compile_analyzer.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+#
+# Script to compile the analyzer.
+#
+# Usage: compile_analyzer.py OPTIONS files
+#
+
+import optparse
+import os
+import shutil
+import subprocess
+import sys
+
+def GetOptions():
+  options = optparse.OptionParser(usage='usage: %prog [options] <output>')
+  options.add_option("--class_path_file",
+      help='File describing the classpath in manifest style')
+  options.add_option("--output_dir",
+      help='Where to output files')
+  options.add_option("--jar_file_name",
+      help='Name of the resulting jar file')
+  options.add_option("--jar_entry_directory",
+      help='Which directory within output to pack into the jar files')
+  options.add_option("--entry_point",
+      help='The entry point for running the program.')
+  options.add_option("--dependent_jar_files",
+      help='The jar files that we link against, space separated.')
+  return options.parse_args()
+
+def CompileAnalyzer(options, args):
+  # We rely on all jar files being copied to the output dir.
+  class_path = options.output_dir + '*'
+  cmd = ['javac',
+         '-sourcepath', 'foobar',
+         '-source', '6',
+         '-target', '6',
+         '-implicit:none',
+         '-d', options.output_dir,
+         '-cp', class_path,
+         ]
+  cmd.extend(args)
+  subprocess.call(cmd)
+
+def CreateJarFile(options):
+  class_path_file_name = options.output_dir + options.class_path_file
+  jar_file_name = options.output_dir + options.jar_file_name
+  cmd = ['jar', 'cfem', jar_file_name, options.entry_point,
+         class_path_file_name,
+         '-C', options.output_dir, options.jar_entry_directory];
+  subprocess.call(cmd)
+
+def CopyFiles(options):
+  # Strip " from the string
+  files = options.dependent_jar_files.replace('"', '');
+  for f in files.split(" "):
+    shutil.copy(f, options.output_dir)
+
+def CreateClassPathFile(options):
+  class_path_file_name = options.output_dir + options.class_path_file
+  with open(class_path_file_name, 'w') as output:
+    print >> output, 'Class-Path:', '.',
+    for r,d,f in os.walk(options.output_dir):
+      for file in f:
+        if file.endswith('.jar'):
+          print >> output, file,
+    # Add new line
+    print >> output
+
+def main():
+  (options, args) = GetOptions()
+  # Clean out everything whenever we do a build, guarantees that we don't have
+  # any leftover jar files.
+  shutil.rmtree(options.output_dir, ignore_errors=True)
+  os.makedirs(options.output_dir)
+
+  CopyFiles(options)
+  CreateClassPathFile(options)
+  CompileAnalyzer(options, args)
+  CreateJarFile(options)
+
+
+if __name__=='__main__':
+  main()
