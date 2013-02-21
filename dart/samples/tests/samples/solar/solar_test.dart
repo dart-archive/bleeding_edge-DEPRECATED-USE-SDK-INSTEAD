433692335e1c094c259eb6754ae419b2ff7b3d55
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-02-21 21:51:35 +0000

    Assert that solar, clock, swipe, and time (4 of the editor samples) compile without errors. Review URL: https://codereview.chromium.org//12317034
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@18854 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/samples/tests/samples/solar/solar_test.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import '../../../solar/web/solar.dart' as solar;
+
+// @static-clean
+
+/**
+ * This test exists to ensure that the solar sample compiles without errors.
+ */
+void main() {
+  
+}
+
---
+++ dart/samples/tests/samples/time/time_test.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import '../../../time/time_server.dart' as time;
+
+// @static-clean
+
+/**
+ * This test exists to ensure that the time sample compiles without errors.
+ */
+void main() {
+
+}
