8e9804198e9ce62db900aaa795c69534ba7ffe3d
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-02-21 17:04:34 +0000

    fix new application code Review URL: https://codereview.chromium.org//12310043
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@18842 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/generator/web/webapp.dart
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/generator/web/webapp.dart
@@ -10,7 +10,7 @@ void reverseText(MouseEvent event) {
   var text = query("#sample_text_id").text;
   var buffer = new StringBuffer();
   for (int i = text.length - 1; i >= 0; i--) {
-    buffer.add(text[i]);
+    buffer.write(text[i]);
   }
   query("#sample_text_id").text = buffer.toString();
 }
