7e52696413ec0d7ac4b25b5977a7cdbcf60f16d6
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-02-25 22:02:01 +0000

    create package symlinks in new folders dartbug.com/6688 Review URL: https://codereview.chromium.org//12335069
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@19006 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/projects/CreateFolderWizard.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/projects/CreateFolderWizard.java
@@ -13,6 +13,10 @@
  */
 package com.google.dart.tools.ui.internal.projects;
 
+import com.google.dart.tools.core.DartCore;
+import com.google.dart.tools.core.pub.RunPubJob;
+
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -22,6 +26,9 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.dialogs.WizardNewFolderMainPage;
 import org.eclipse.ui.internal.wizards.newresource.ResourceMessages;
 import org.eclipse.ui.wizards.newresource.BasicNewFolderResourceWizard;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Create a new folder wizard
@@ -58,6 +65,13 @@ public class CreateFolderWizard extends BasicNewFolderResourceWizard {
    */
   public static final String WIZARD_ID = "com.google.dart.tools.ui.new.folder"; //$NON-NLS-1$
 
+  /**
+   * The directories in the pub white list, ones which have symlinks to the packages directory
+   */
+  // (TODO:keertip) Make sure this stays in sync with documentation 
+  // http://pub.dartlang.org/doc/pub-install.html
+  public static final List<String> pubDirectories = Arrays.asList("web", "example", "test", "bin");
+
   private WizardNewFolderMainPage mainPage;
 
   @Override
@@ -82,8 +96,40 @@ public class CreateFolderWizard extends BasicNewFolderResourceWizard {
     if (folder == null) {
       return false;
     }
+    // if new folder is pub white listed directory/sub-directory, run pub install
+    // to create the link to packages
+    if (pubDirectories.contains(folder.getName()) || isPubDirectoryParent(folder)) {
+      IContainer pubspecDir = getPubWorkingDir(folder);
+      if (pubspecDir != null) {
+        RunPubJob job = new RunPubJob(pubspecDir, RunPubJob.INSTALL_COMMAND);
+        job.schedule();
+      }
+    }
     selectAndReveal(folder);
+
     return true;
+  }
+
+  private IContainer getPubWorkingDir(IFolder folder) {
+    IContainer container = folder;
+    while (container != null) {
+      if (container.findMember(DartCore.PUBSPEC_FILE_NAME) != null) {
+        return container;
+      }
+      container = container.getParent();
+    }
+    return null;
+  }
+
+  private boolean isPubDirectoryParent(IFolder folder) {
+    IContainer parent = folder.getParent();
+    while (parent != null) {
+      if (pubDirectories.contains(parent.getName())) {
+        return true;
+      }
+      parent = parent.getParent();
+    }
+    return false;
   }
 
 }
