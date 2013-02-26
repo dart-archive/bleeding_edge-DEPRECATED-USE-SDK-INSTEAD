83477d4f385bba52e9c49e1e5ccb14a0092b881d
lukeEChurch@gmail.com <lukeEChurch@gmail.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-02-26 12:14:15 +0000

    Add missing file Review URL: https://codereview.chromium.org//12313124
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@19039 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.tools.debug.ui/src/com/google/dart/tools/debug/ui/internal/DebugInstrumentationUtilities.java
@@ -0,0 +1,56 @@
+package com.google.dart.tools.debug.ui.internal;
+
+import com.google.dart.engine.utilities.instrumentation.InstrumentationBuilder;
+import com.google.dart.tools.debug.core.DartLaunchConfigWrapper;
+
+import org.eclipse.debug.core.ILaunchConfiguration;
+
+/**
+ * Helper class for instrumenting launch and debug services
+ */
+public class DebugInstrumentationUtilities {
+
+  public static void recordLaunchConfiguration(DartLaunchConfigWrapper launch,
+      InstrumentationBuilder instrumentation) {
+
+    instrumentation.data("LaunchConfig-ApplicationName", launch.getApplicationName());
+    instrumentation.data("LaunchConfig-getConnectionHost", launch.getConnectionHost());
+    instrumentation.data("LaunchConfig-getConnectionPort", launch.getConnectionPort());
+    instrumentation.data("LaunchConfig-getProjectName", launch.getProjectName());
+    instrumentation.data("LaunchConfig-getUrl", launch.getUrl());
+    instrumentation.data("LaunchConfig-getWorkingDirectory", launch.getWorkingDirectory());
+
+    instrumentation.metric("LaunchConfig-getArguments", launch.getArguments());
+    instrumentation.metric("LaunchConfig-getBrowserName", launch.getBrowserName());
+    instrumentation.metric("LaunchConfig-getCheckedMode", String.valueOf(launch.getCheckedMode()));
+    instrumentation.metric("LaunchConfig-getLastLaunchTime", launch.getLastLaunchTime());
+    instrumentation.metric(
+        "LaunchConfig-getShouldLaunchFile",
+        String.valueOf(launch.getShouldLaunchFile()));
+    instrumentation.metric(
+        "LaunchConfig-getShowLaunchOutput",
+        String.valueOf(launch.getShowLaunchOutput()));
+    instrumentation.metric(
+        "LaunchConfig-getUseDefaultBrowser",
+        String.valueOf(launch.getUseDefaultBrowser()));
+    instrumentation.metric(
+        "LaunchConfig-getUseWebComponents",
+        String.valueOf(launch.getUseWebComponents()));
+    instrumentation.metric("LaunchConfig-getVmArgumentsAsArray", launch.getVmArgumentsAsArray());
+
+  }
+
+  public static void recordLaunchConfiguration(ILaunchConfiguration launch,
+      InstrumentationBuilder instrumentation) {
+
+    try {
+      instrumentation.metric("launchConfig-getCategory", launch.getCategory());
+      instrumentation.metric("launchConfig-getClass", launch.getClass().toString());
+
+      instrumentation.data("launchConfig-getName", launch.getName());
+
+    } catch (Exception e) {
+    }
+  }
+
+}
