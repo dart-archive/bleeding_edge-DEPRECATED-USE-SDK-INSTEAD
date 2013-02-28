1e0d557ece7bad35e2e38bbb98447c2274b29502
messick@google.com <messick@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-02-28 21:11:11 +0000

    Defining this class in the services plugin worked well for junit tests but failed for junit plugin tests. Needless to say, it did not work for the editor. Review URL: https://codereview.chromium.org//12377022
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@19257 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/ast/EphemeralIdentifier.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2013, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.engine.ast;
+
+import com.google.dart.engine.scanner.Token;
+import com.google.dart.engine.scanner.TokenType;
+
+/**
+ * Ephemeral identifiers are created as needed to mimic the presence of an empty identifier.
+ */
+public class EphemeralIdentifier extends SimpleIdentifier {
+
+  public EphemeralIdentifier(ASTNode parent, int location) {
+    super(new Token(TokenType.IDENTIFIER, location));
+    parent.becomeParentOf(this);
+  }
+}
