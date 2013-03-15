49efe557414866420caf0200ba38c23d8aaf444e
scheglov@google.com <scheglov@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-03-15 20:13:21 +0000

    Update generated analyzer_experimental for new List interface.
    R=pquitslund@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//12603007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@20103 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/util/plugins/com.google.dart.java2dart/resources/java_core.dart
+++ dart/editor/util/plugins/com.google.dart.java2dart/resources/java_core.dart
@@ -291,6 +291,10 @@ class ListWrapper<E> extends Collection<E> implements List<E> {
     return elements.indexOf(element, start);
   }
 
+  void insert(int index, E element) {
+    elements.insert(index, element);
+  }
+
   int lastIndexOf(E element, [int start]) {
     return elements.lastIndexOf(element, start);
   }
@@ -301,10 +305,6 @@ class ListWrapper<E> extends Collection<E> implements List<E> {
 
   void remove(Object element) {
     return elements.remove(element);
-  }
-
-  void insert(int index, E value) {
-    elements.insert(index, value);
   }
 
   E removeAt(int index) {
