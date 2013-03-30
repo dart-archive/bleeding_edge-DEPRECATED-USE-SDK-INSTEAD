e5c641c13b8d00bf3037260e106c7c4725ae76d0
danrubel@google.com <danrubel@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-03-30 23:05:52 +0000

    run pub before launch without waiting for analysis
    Review URL: https://codereview.chromium.org//13365004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@20717 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.debug.core/src/com/google/dart/tools/debug/core/DartLaunchConfigurationDelegate.java
+++ dart/editor/tools/plugins/com.google.dart.tools.debug.core/src/com/google/dart/tools/debug/core/DartLaunchConfigurationDelegate.java
@@ -15,6 +15,7 @@ package com.google.dart.tools.debug.core;
 
 import com.google.dart.engine.utilities.instrumentation.Instrumentation;
 import com.google.dart.engine.utilities.instrumentation.InstrumentationBuilder;
+import com.google.dart.tools.core.pub.PubBuildParticipant;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -55,15 +56,33 @@ public abstract class DartLaunchConfigurationDelegate extends LaunchConfiguratio
   }
 
   @Override
-  protected IProject[] getBuildOrder(ILaunchConfiguration configuration, String mode)
-      throws CoreException {
-    // indicate which project to save before launch
-    DartLaunchConfigWrapper launchConfig = new DartLaunchConfigWrapper(configuration);
-    IResource resource = launchConfig.getApplicationResource();
-    if (resource != null) {
-      return new IProject[] {resource.getProject()};
+  public boolean preLaunchCheck(ILaunchConfiguration configuration, String mode,
+      IProgressMonitor monitor) throws CoreException {
+
+    // Check to see if the user wants to save any dirty editors before launch
+    if (!super.preLaunchCheck(configuration, mode, monitor)) {
+      return false;
     }
-    return null;
+
+    // Run pub install for this application if necessary
+    DartLaunchConfigWrapper launchConfig = new DartLaunchConfigWrapper(configuration);
+    IResource res = launchConfig.getApplicationResource();
+    if (res == null) {
+      res = launchConfig.getProject();
+    }
+    if (res != null) {
+      new PubBuildParticipant().runPubFor(res, monitor);
+    }
+
+    // TODO (danrubel): run build.dart for this application if necessary
+
+    return true;
   }
 
+  @Override
+  protected IProject[] getBuildOrder(ILaunchConfiguration configuration, String mode)
+      throws CoreException {
+    // Rely on the preLaunchCheck method above to run pub and build.dart before launch
+    return new IProject[0];
+  }
 }
