61a3dfea27480b79e570fb20792ac151faaed45a
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-03-30 19:02:55 +0000

    Remove a call to the model in the plugins Project Explorer. Update a very old icon in the search results view. Add flex box support to the css editor. Review URL: https://codereview.chromium.org//13310003
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@20714 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/DartPluginImages.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/DartPluginImages.java
@@ -193,7 +193,9 @@ public class DartPluginImages {
   /*
    * Set of predefined Image Descriptors.
    */
-  public static final ImageDescriptor DESC_DART_LIB_FILE = createUnManaged(T_DART, "library.png"); //$NON-NLS-1$
+  public static final ImageDescriptor DESC_DART_LIB_FILE = createUnManaged(
+      T_DART,
+      "package_obj.gif"); //$NON-NLS-1$
   public static final ImageDescriptor DESC_DART_COMP_UNIT = createUnManaged(T_DART, "dart.png"); //$NON-NLS-1$
   public static final ImageDescriptor DESC_DART_HTML_FILE = createUnManaged(T_DART, "html.png"); //$NON-NLS-1$
   public static final ImageDescriptor DESC_DART_CLASS_TYPE_ALIAS = createUnManaged(
