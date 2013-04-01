7a7310851fc7089a74cfcf30199962bd3c182395
lukeEChurch@gmail.com <lukeEChurch@gmail.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-04-01 23:20:53 +0000

    Instrumenting the other operations in the context Review URL: https://codereview.chromium.org//13261023
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@20759 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/internal/context/InstrumentedAnalysisContextImpl.java
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/internal/context/InstrumentedAnalysisContextImpl.java
@@ -29,29 +29,40 @@ import java.util.UUID;
  */
 public class InstrumentedAnalysisContextImpl implements AnalysisContext {
 
-  protected String contextId = UUID.randomUUID().toString();
+  /**
+   * Record an exception that was thrown during analysis
+   */
+  private static void recordAnalysisException(InstrumentationBuilder instrumentation,
+      AnalysisException e) {
+    instrumentation.metric("Problem", e.getClass().toString());
+    instrumentation.data("AnalysisException", e.toString());
+  }
 
+  protected String contextId = UUID.randomUUID().toString();
   protected AnalysisContext basis = new AnalysisContextImpl();
 
   @Override
   public void applyChanges(ChangeSet changeSet) {
-    basis.applyChanges(changeSet);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-applyChanges");
+    try {
+      instrumentation.metric("contextId", contextId);
 
+      basis.applyChanges(changeSet);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public AnalysisError[] computeErrors(Source source) throws AnalysisException {
 
-    InstrumentationBuilder instrumentation = Instrumentation.builder("ComputeErrors");
-
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-computeErrors");
     try {
       instrumentation.metric("contextId", contextId);
 
       AnalysisError[] errors = basis.computeErrors(source);
       instrumentation.metric("Errors-count", errors.length);
-
       return errors;
-
     } finally {
       instrumentation.log();
     }
@@ -59,166 +70,411 @@ public class InstrumentedAnalysisContextImpl implements AnalysisContext {
 
   @Override
   public HtmlElement computeHtmlElement(Source source) throws AnalysisException {
-    return basis.computeHtmlElement(source);
+
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-computeHtmlElement");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      return basis.computeHtmlElement(source);
+    } catch (AnalysisException e) {
+      recordAnalysisException(instrumentation, e);
+      throw e;
+    } finally {
+      instrumentation.log();
+    }
+
   }
 
   @Override
   public SourceKind computeKindOf(Source source) {
-    return basis.computeKindOf(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-computeKindOf");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      return basis.computeKindOf(source);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public LibraryElement computeLibraryElement(Source source) throws AnalysisException {
-    return basis.computeLibraryElement(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-computeLibraryElement");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      return basis.computeLibraryElement(source);
+    } catch (AnalysisException e) {
+      recordAnalysisException(instrumentation, e);
+      throw e;
+    } finally {
+      instrumentation.log();
+    }
+
   }
 
   @Override
   public LineInfo computeLineInfo(Source source) throws AnalysisException {
-    return basis.computeLineInfo(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-computeLineInfo");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      return basis.computeLineInfo(source);
+    } catch (AnalysisException e) {
+      recordAnalysisException(instrumentation, e);
+      throw e;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public AnalysisContext extractContext(SourceContainer container) {
-    return basis.extractContext(container);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-extractContext");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      return basis.extractContext(container);
+    } finally {
+      instrumentation.log();
+    }
+
   }
 
   @Override
   public Element getElement(ElementLocation location) {
-    return basis.getElement(location);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getElement");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      return basis.getElement(location);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public AnalysisError[] getErrors(Source source) {
-    return basis.getErrors(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getErrors");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      AnalysisError[] ret = basis.getErrors(source);
+      instrumentation.metric("Errors-count", ret.length);
+      return ret;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public HtmlElement getHtmlElement(Source source) {
-    return basis.getHtmlElement(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getHtmlElement");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      return basis.getHtmlElement(source);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public Source[] getHtmlFilesReferencing(Source source) {
-    return basis.getHtmlFilesReferencing(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getHtmlFilesReferencing");
+    try {
+      instrumentation.metric("contextId", contextId);
+
+      Source[] ret = basis.getHtmlFilesReferencing(source);
+      instrumentation.metric("Source-count", ret.length);
+      return ret;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public Source[] getHtmlSources() {
-    return basis.getHtmlSources();
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getHtmlSources");
+    try {
+      instrumentation.metric("contextId", contextId);
+      Source[] ret = basis.getHtmlSources();
+      instrumentation.metric("Source-count", ret.length);
+      return ret;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public SourceKind getKindOf(Source source) {
-    return basis.getKindOf(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getKindOf");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.getKindOf(source);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public Source[] getLaunchableClientLibrarySources() {
-    return basis.getLaunchableClientLibrarySources();
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getLaunchableClientLibrarySources");
+    try {
+      instrumentation.metric("contextId", contextId);
+      Source[] ret = basis.getLaunchableClientLibrarySources();
+      instrumentation.metric("Source-count", ret.length);
+      return ret;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public Source[] getLaunchableServerLibrarySources() {
-    return basis.getLaunchableServerLibrarySources();
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getLaunchableServerLibrarySources");
+    try {
+      instrumentation.metric("contextId", contextId);
+      Source[] ret = basis.getLaunchableServerLibrarySources();
+      instrumentation.metric("Source-count", ret.length);
+      return ret;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public Source[] getLibrariesContaining(Source source) {
-    return basis.getLibrariesContaining(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getLibrariesContaining");
+    try {
+      instrumentation.metric("contextId", contextId);
+      Source[] ret = basis.getLibrariesContaining(source);
+      instrumentation.metric("Source-count", ret.length);
+      return ret;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public LibraryElement getLibraryElement(Source source) {
-    return basis.getLibraryElement(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getLibraryElement");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.getLibraryElement(source);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public Source[] getLibrarySources() {
-    return basis.getLibrarySources();
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getLibrarySources");
+    try {
+      instrumentation.metric("contextId", contextId);
+      Source[] ret = basis.getLibrarySources();
+      instrumentation.metric("Source-count", ret.length);
+      return ret;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public LineInfo getLineInfo(Source source) {
-    return basis.getLineInfo(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getLineInfo");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.getLineInfo(source);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public CompilationUnit getResolvedCompilationUnit(Source unitSource, LibraryElement library) {
-    return basis.getResolvedCompilationUnit(unitSource, library);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getResolvedCompilationUnit");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.getResolvedCompilationUnit(unitSource, library);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public CompilationUnit getResolvedCompilationUnit(Source unitSource, Source librarySource) {
-    return basis.getResolvedCompilationUnit(unitSource, librarySource);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getResolvedCompilationUnit");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.getResolvedCompilationUnit(unitSource, librarySource);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public SourceFactory getSourceFactory() {
-    return basis.getSourceFactory();
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-getSourceFactory");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.getSourceFactory();
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public boolean isClientLibrary(Source librarySource) {
-    return basis.isClientLibrary(librarySource);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-isClientLibrary");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.isClientLibrary(librarySource);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public boolean isServerLibrary(Source librarySource) {
-    return basis.isServerLibrary(librarySource);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-isServerLibrary");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.isServerLibrary(librarySource);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public void mergeContext(AnalysisContext context) {
-    basis.mergeContext(context);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-mergeContext");
+    try {
+      instrumentation.metric("contextId", contextId);
+      basis.mergeContext(context);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public CompilationUnit parseCompilationUnit(Source source) throws AnalysisException {
-    return basis.parseCompilationUnit(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-parseCompilationUnit");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.parseCompilationUnit(source);
+    } catch (AnalysisException e) {
+      recordAnalysisException(instrumentation, e);
+      throw e;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public HtmlUnit parseHtmlUnit(Source source) throws AnalysisException {
-    return basis.parseHtmlUnit(source);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-parseHtmlUnit");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.parseHtmlUnit(source);
+    } catch (AnalysisException e) {
+      recordAnalysisException(instrumentation, e);
+      throw e;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public ChangeNotice[] performAnalysisTask() {
-    return basis.performAnalysisTask();
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-performAnalysisTask");
+    try {
+      instrumentation.metric("contextId", contextId);
+      ChangeNotice[] ret = basis.performAnalysisTask();
+      instrumentation.metric("ChangeNotice-count", ret.length);
+      return ret;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public CompilationUnit resolveCompilationUnit(Source unitSource, LibraryElement library)
       throws AnalysisException {
-    return basis.resolveCompilationUnit(unitSource, library);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-resolveCompilationUnit");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.resolveCompilationUnit(unitSource, library);
+    } catch (AnalysisException e) {
+      recordAnalysisException(instrumentation, e);
+      throw e;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public CompilationUnit resolveCompilationUnit(Source unitSource, Source librarySource)
       throws AnalysisException {
-    return basis.resolveCompilationUnit(unitSource, librarySource);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-resolveCompilationUnit");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.resolveCompilationUnit(unitSource, librarySource);
+    } catch (AnalysisException e) {
+      recordAnalysisException(instrumentation, e);
+      throw e;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public HtmlUnit resolveHtmlUnit(Source htmlSource) throws AnalysisException {
-    return basis.resolveHtmlUnit(htmlSource);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-resolveHtmlUnit");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.resolveHtmlUnit(htmlSource);
+    } catch (AnalysisException e) {
+      recordAnalysisException(instrumentation, e);
+      throw e;
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public void setContents(Source source, String contents) {
-    basis.setContents(source, contents);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-setContents");
+    try {
+      instrumentation.metric("contextId", contextId);
+      basis.setContents(source, contents);
+    } finally {
+      instrumentation.log();
+    }
   }
 
   @Override
   public void setSourceFactory(SourceFactory factory) {
-    basis.setSourceFactory(factory);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-setSourceFactory");
+    try {
+      instrumentation.metric("contextId", contextId);
+      basis.setSourceFactory(factory);
+    } finally {
+      instrumentation.log();
+    }
 
   }
 
   @Override
   @Deprecated
   public Iterable<Source> sourcesToResolve(Source[] changedSources) {
-    return basis.sourcesToResolve(changedSources);
+    InstrumentationBuilder instrumentation = Instrumentation.builder("Analysis-sourcesToResolve");
+    try {
+      instrumentation.metric("contextId", contextId);
+      return basis.sourcesToResolve(changedSources);
+    } finally {
+      instrumentation.log();
+    }
   }
 
 }
