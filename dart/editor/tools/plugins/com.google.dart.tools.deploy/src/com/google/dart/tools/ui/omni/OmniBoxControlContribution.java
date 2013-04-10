2ff47b1f6d591bc4128c8d9751ad7a0814cfab25
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-04-10 20:15:44 +0000

    NPE fix for when popups go missing. Review URL: https://codereview.chromium.org//14085002
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@21230 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.deploy/src/com/google/dart/tools/ui/omni/OmniBoxControlContribution.java
+++ dart/editor/tools/plugins/com.google.dart.tools.deploy/src/com/google/dart/tools/ui/omni/OmniBoxControlContribution.java
@@ -435,14 +435,16 @@ public class OmniBoxControlContribution {
       textControl.addListener(SWT.Deactivate, new Listener() {
         @Override
         public void handleEvent(Event event) {
-          //selecting the scrollbar will deactivate but in that case we don't want to close
-          // TODO: bug; going from scrollbar back to text entry clears text!
-          Control focusControl = event.display.getFocusControl();
-          //in some cases the focus control goes null even though the text still receives
-          //key events (issue 1905) and we want to *not* close the popup
-          if (focusControl != null && focusControl != popup.table) {
-            popup.close();
-            popup = null;
+          if (popup != null) {
+            //selecting the scrollbar will deactivate but in that case we don't want to close
+            // TODO: bug; going from scrollbar back to text entry clears text!
+            Control focusControl = event.display.getFocusControl();
+            //in some cases the focus control goes null even though the text still receives
+            //key events (issue 1905) and we want to *not* close the popup
+            if (focusControl != null && focusControl != popup.table) {
+              popup.close();
+              popup = null;
+            }
           }
           textControl.removeListener(SWT.Deactivate, this);
         }
