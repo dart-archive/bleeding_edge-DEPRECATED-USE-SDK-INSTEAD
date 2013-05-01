04baa14e0f3916c00ec7854b9ae9c0d464c49c4f
devoncarew@google.com <devoncarew@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-05-01 16:21:04 +0000

    Turn on the re-connect dialog.
    R=danrubel@google.com
    
    Review URL: https://codereview.chromium.org//14574004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@22256 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.debug.ui/src/com/google/dart/tools/debug/ui/internal/DefaultDebugUIHelper.java
+++ dart/editor/tools/plugins/com.google.dart.tools.debug.ui/src/com/google/dart/tools/debug/ui/internal/DefaultDebugUIHelper.java
@@ -54,40 +54,34 @@ public class DefaultDebugUIHelper extends DebugUIHelper {
 
   @Override
   public void showDevtoolsDisconnectError(final String _title, final DartiumDebugTarget target) {
-    if (true) {
-      showError(
-          "Debugger Connection Closed",
-          "The debugger connection has been closed by DevTools.");
-    } else {
-      final Display display = Display.getDefault();
+    final Display display = Display.getDefault();
 
-      Display.getDefault().asyncExec(new Runnable() {
-        @Override
-        public void run() {
-          if (display.isDisposed()) {
+    Display.getDefault().asyncExec(new Runnable() {
+      @Override
+      public void run() {
+        if (display.isDisposed()) {
+          return;
+        }
+
+        String title = _title;
+        String message = "The debugger connection has been closed by DevTools.\n\n"
+            + "Do you want to re-connect? (DevTools must be closed first)";
+
+        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+
+        while (MessageDialog.openQuestion(shell, title, message)) {
+          try {
+            target.reconnect();
+
             return;
-          }
-
-          String title = _title;
-          String message = "The debugger connection has been closed by DevTools.\n\n"
-              + "Do you want to re-connect? (DevTools must be closed first)";
-
-          Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-
-          while (MessageDialog.openQuestion(shell, title, message)) {
-            try {
-              target.reconnect();
-
-              return;
-            } catch (IOException e) {
-              title = "Error Re-connecting";
-              message = "Got " + e.toString() + " while reconnecting.\n\n"
-                  + "Try to reconnect again?";
-            }
+          } catch (IOException e) {
+            title = "Error Re-connecting";
+            message = "Got " + e.toString() + " while reconnecting.\n\n"
+                + "Try to reconnect again?";
           }
         }
-      });
-    }
+      }
+    });
   }
 
   @Override
