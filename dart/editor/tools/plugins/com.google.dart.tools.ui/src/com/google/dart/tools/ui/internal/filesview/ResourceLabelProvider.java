50169f705b298e79c2400cecd61dee16a5148cf0
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-05-02 18:08:14 +0000

    Legacy resource label provider cleanup.
    R=scheglov@google.com
    
    Review URL: https://codereview.chromium.org//14718007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@22334 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/filesview/ResourceLabelProvider.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/filesview/ResourceLabelProvider.java
@@ -13,30 +13,248 @@
  */
 package com.google.dart.tools.ui.internal.filesview;
 
+import com.google.dart.tools.core.DartCore;
+import com.google.dart.tools.core.analysis.model.ProjectEvent;
 import com.google.dart.tools.core.analysis.model.ProjectListener;
-import com.google.dart.tools.core.model.ElementChangedEvent;
-import com.google.dart.tools.core.model.ElementChangedListener;
+import com.google.dart.tools.ui.DartToolsPlugin;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.LabelProviderChangedEvent;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.SWTException;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Label provider for resources in the {@link FilesView}.
  */
-public abstract class ResourceLabelProvider implements IStyledLabelProvider, ILabelProvider,
-    ElementChangedListener, ProjectListener {
+public class ResourceLabelProvider implements IStyledLabelProvider, ILabelProvider, ProjectListener {
+
+  private static final String IGNORE_FILE_ICON = "icons/full/dart16/dart_excl.png"; //$NON-NLS-1$
+
+  private static final String IGNORE_FOLDER_ICON = "icons/full/dart16/flder_obj_excl.png"; //$NON-NLS-1$
+
+  private static final String PACKAGES_FOLDER_ICON = "icons/full/dart16/fldr_obj_pkg.png"; //$NON-NLS-1$
+  @SuppressWarnings("unused")
+  private static final String LIBRARY_ICON = "icons/full/dart16/dart_library.png"; //$NON-NLS-1$
+  private static final String BUILD_FILE_ICON = "icons/full/dart16/build_dart.png"; //$NON-NLS-1$
+  private static final String PACKAGE_ICON = "icons/full/obj16/package_obj.gif"; //$NON-NLS-1$
 
   /**
    * Get a resource label provider instance.
    */
   public static ResourceLabelProvider createInstance() {
-    return new NewResourceLabelProvider();
+    return new ResourceLabelProvider();
+  }
+
+  private final WorkbenchLabelProvider workbenchLabelProvider = new WorkbenchLabelProvider();
+
+  private List<ILabelProviderListener> listeners = new ArrayList<ILabelProviderListener>();
+
+  private boolean disposed;
+
+  @Override
+  public void addListener(ILabelProviderListener listener) {
+    listeners.add(listener);
+
+    if (listeners.size() == 1) {
+      DartCore.getProjectManager().addProjectListener(this);
+    }
   }
 
   @Override
-  public void elementChanged(ElementChangedEvent event) {
-    //legacy method to be removed
-    //(implemented in legacy "old" label provider subclass)
+  public void dispose() {
+    disposed = true;
+
+    workbenchLabelProvider.dispose();
+
+    if (listeners.size() > 0) {
+      DartCore.getProjectManager().removeProjectListener(this);
+    }
+  }
+
+  @Override
+  public Image getImage(Object element) {
+    if (element instanceof IResource) {
+
+      IResource resource = (IResource) element;
+
+      if (!DartCore.isAnalyzed(resource)) {
+        if (resource instanceof IFile) {
+          return DartToolsPlugin.getImage(IGNORE_FILE_ICON);
+        }
+
+        if (resource instanceof IFolder) {
+          return DartToolsPlugin.getImage(IGNORE_FOLDER_ICON);
+        }
+      }
+
+      if (resource instanceof IFile) {
+        IFile file = (IFile) resource;
+
+        if (DartCore.isBuildDart(file)) {
+          return DartToolsPlugin.getImage(BUILD_FILE_ICON);
+        }
+
+        // TODO(scheglov) disable for now, even "get" operation gets blocked currently.
+        // TODO(scheglov) restore once AnalysisContext is fixed.
+//        SourceKind kind = DartCore.getProjectManager().getSourceKind(file);
+//
+//        if (kind == SourceKind.LIBRARY) {
+//          return DartToolsPlugin.getImage(LIBRARY_ICON);
+//        }
+      }
+
+      if (element instanceof IFolder) {
+        IFolder folder = (IFolder) element;
+
+        if (DartCore.isPackagesDirectory(folder)) {
+          return DartToolsPlugin.getImage(PACKAGES_FOLDER_ICON);
+        }
+
+        if (DartCore.isPackagesResource(folder)) {
+          return DartToolsPlugin.getImage(PACKAGE_ICON);
+        }
+      }
+    }
+
+    return workbenchLabelProvider.getImage(element);
+  }
+
+  @Override
+  public StyledString getStyledText(Object element) {
+    if (element instanceof IResource) {
+      IResource resource = (IResource) element;
+
+      // Un-analyzed resources are grey.
+      if (!DartCore.isAnalyzed(resource)) {
+        return new StyledString(resource.getName(), StyledString.QUALIFIER_STYLER);
+      }
+
+      StyledString string = new StyledString(resource.getName());
+
+      try {
+
+        if (resource instanceof IFolder) {
+          if (DartCore.isPackagesDirectory((IFolder) resource)) {
+            string.append(" [package:]", StyledString.QUALIFIER_STYLER); //$NON-NLS-1$
+          }
+          String version = resource.getPersistentProperty(DartCore.PUB_PACKAGE_VERSION);
+          if (version != null) {
+            string.append(" [" + version + "]", StyledString.QUALIFIER_STYLER); //$NON-NLS-1$ //$NON-NLS-2$
+            return string;
+          }
+        }
+
+        // TODO(scheglov) disable for now, even "get" operation gets blocked currently.
+        // TODO(scheglov) restore once AnalysisContext is fixed.
+//        if (resource instanceof IFile) {
+//          // Append the library name to library units.
+//
+//          ProjectManager projectManager = DartCore.getProjectManager();
+//          SourceKind kind = projectManager.getSourceKind((IFile) resource);
+//
+//          if (kind == SourceKind.LIBRARY) {
+//
+//            LibraryElement libraryElement = projectManager.getLibraryElementOrNull((IFile) resource);
+//
+//            if (libraryElement != null) {
+//
+//              String name = libraryElement.getName();
+//
+//              if (name == null || name.length() == 0) {
+//
+//                if (libraryElement.getEntryPoint() != null) {
+//                  name = FilenameUtils.removeExtension(resource.getName());
+//                }
+//
+//              }
+//
+//              string.append(" [" + name + "]", StyledString.QUALIFIER_STYLER); //$NON-NLS-1$ //$NON-NLS-2$
+//            }
+//
+//          }
+//
+//        }
+      } catch (Throwable th) {
+        DartToolsPlugin.log(th);
+      }
+
+      return string;
+    }
+
+    if (element instanceof DartLibraryNode && ((DartLibraryNode) element).getCategory() != null) {
+      StyledString string = new StyledString(((DartLibraryNode) element).getLabel());
+      string.append(" [" + ((DartLibraryNode) element).getCategory() + "]", //$NON-NLS-1$ //$NON-NLS-2$
+          StyledString.QUALIFIER_STYLER);
+
+      return string;
+    }
+
+    return workbenchLabelProvider.getStyledText(element);
+  }
+
+  @Override
+  public String getText(Object element) {
+    return workbenchLabelProvider.getText(element);
+  }
+
+  @Override
+  public boolean isLabelProperty(Object element, String property) {
+    return workbenchLabelProvider.isLabelProperty(element, property);
+  }
+
+  @Override
+  public void projectAnalyzed(ProjectEvent event) {
+    notifyListeners();
+  }
+
+  @Override
+  public void removeListener(ILabelProviderListener listener) {
+    listeners.remove(listener);
+
+    if (listeners.isEmpty()) {
+      DartCore.getProjectManager().removeProjectListener(this);
+    }
+  }
+
+  private void notifyListeners() {
+    if (disposed) {
+      return;
+    }
+
+    try {
+      for (final ILabelProviderListener listener : listeners) {
+        uiExec(new Runnable() {
+          @Override
+          public void run() {
+            if (!disposed) {
+              listener.labelProviderChanged(new LabelProviderChangedEvent(
+                  ResourceLabelProvider.this));
+            }
+          }
+        });
+      }
+    } catch (Throwable t) {
+      DartToolsPlugin.log(t);
+    }
+  }
+
+  private void uiExec(Runnable runnable) {
+    try {
+      Display.getDefault().asyncExec(runnable);
+    } catch (SWTException e) {
+      // Ignore -- might occur if async events get dispatched after the WS is closed
+    }
   }
 
 }
