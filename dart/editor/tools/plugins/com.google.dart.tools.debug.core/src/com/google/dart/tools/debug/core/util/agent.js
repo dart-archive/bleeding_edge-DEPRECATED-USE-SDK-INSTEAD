4b134903d4791a3119aed07faa38704b6ae29da4
kevmoo@j832.com <kevmoo@j832.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-05-03 17:16:23 +0000

    check if window is defined before setting up debug iframe
    BUG=https://code.google.com/p/dart/issues/detail?id=10417
    R=devoncarew@google.com
    
    Review URL: https://codereview.chromium.org//14697007
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@22377 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.debug.core/src/com/google/dart/tools/debug/core/util/agent.js
+++ dart/editor/tools/plugins/com.google.dart.tools.debug.core/src/com/google/dart/tools/debug/core/util/agent.js
@@ -1,43 +1,45 @@
-// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 // -- start Dart Editor debug agent --
 
-var _d_origin = window.location.protocol + '//' + window.location.host;
-var _d_remoteWindow = null;
+// When a script is loaded in a WebWorker, window will not exist
+if (typeof window == "object") {
+  var _d_origin = window.location.protocol + '//' + window.location.host;
+  var _d_remoteWindow = null;
 
-// create an iframe used to send log messages back to the editor
-var _d_remoteFrame = document.createElement('iframe');
-_d_remoteFrame.style.display = 'none';
-_d_remoteFrame.src = _d_origin + '/agent.html';
-document.documentElement.appendChild(_d_remoteFrame);
+  // create an iframe used to send log messages back to the editor
+  var _d_remoteFrame = document.createElement('iframe');
+  _d_remoteFrame.style.display = 'none';
+  _d_remoteFrame.src = _d_origin + '/agent.html';
+  document.documentElement.appendChild(_d_remoteFrame);
 
-_d_remoteFrame.onload = function () {
-  _d_remoteWindow = _d_remoteFrame.contentWindow;
-};
+  _d_remoteFrame.onload = function () {
+    _d_remoteWindow = _d_remoteFrame.contentWindow;
+  };
 
-// replace the dartPrint function with our own definition
-dartPrint = function(str) {
-  var MAX = 200000;
-  
-  if (typeof window == "object" && typeof console == "object") {
-    console.log(str);
-  } else if (typeof print == "function") {
-    print(str);
-  }
-  
-  if (_d_remoteWindow) {
-    if (str.length > MAX) {
-      str = str.substring(0, MAX - 20) + '\n\nOUTPUT TRUNCATED';
-      
-      if (window.console) {
-        console.log('Dart print() output has been truncated (' + MAX + ' chars max).');
-      }
+  // replace the dartPrint function with our own definition
+  dartPrint = function(str) {
+    var MAX = 200000;
+    
+    if (typeof window == "object" && typeof console == "object") {
+      console.log(str);
+    } else if (typeof print == "function") {
+      print(str);
     }
+    
+    if (_d_remoteWindow) {
+      if (str.length > MAX) {
+        str = str.substring(0, MAX - 20) + '\n\nOUTPUT TRUNCATED';
+        
+        if (window.console) {
+          console.log('Dart print() output has been truncated (' + MAX + ' chars max).');
+        }
+      }
 
-    _d_remoteWindow.postMessage(JSON.stringify({ message: str }), _d_origin);
-  }
-};
-
+      _d_remoteWindow.postMessage(JSON.stringify({ message: str }), _d_origin);
+    }
+  };
+}
 //-- end Dart Editor debug agent --
