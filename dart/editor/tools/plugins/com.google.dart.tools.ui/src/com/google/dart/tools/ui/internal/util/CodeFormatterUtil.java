f65757eafbd82dfc7209818dc56ee46229308f0e
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-07-08 21:55:37 +0000

    Dead code cleanup.
    R=scheglov@google.com
    
    Review URL: https://codereview.chromium.org//18870003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@24824 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/util/CodeFormatterUtil.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/util/CodeFormatterUtil.java
@@ -13,12 +13,10 @@
  */
 package com.google.dart.tools.ui.internal.util;
 
-import com.google.dart.compiler.ast.DartNode;
 import com.google.dart.tools.core.DartCore;
 import com.google.dart.tools.core.formatter.DefaultCodeFormatterConstants;
 import com.google.dart.tools.core.model.DartProject;
 import com.google.dart.tools.ui.DartToolsPlugin;
-import com.google.dart.tools.ui.DartX;
 import com.google.dart.tools.ui.text.editor.tmp.JavaScriptCore;
 
 import org.eclipse.core.runtime.Assert;
@@ -27,18 +25,9 @@ import org.eclipse.jface.text.BadPositionCategoryException;
 import org.eclipse.jface.text.DefaultPositionUpdater;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.Position;
-import org.eclipse.text.edits.DeleteEdit;
-import org.eclipse.text.edits.InsertEdit;
-import org.eclipse.text.edits.MultiTextEdit;
-import org.eclipse.text.edits.ReplaceEdit;
 import org.eclipse.text.edits.TextEdit;
 
-import java.util.Map;
-
 public class CodeFormatterUtil {
-
-  private static final int FORMAT_NEW = 0;
-  private static final int FORMAT_EXISTING = 1;
 
   /**
    * Creates a string that represents the given number of indentation units. The returned string can
@@ -50,8 +39,8 @@ public class CodeFormatterUtil {
    * @return the indent string
    */
   public static String createIndentString(int indentationUnits, DartProject project) {
-    Map<String, String> options = project != null ? project.getOptions(true)
-        : DartCore.getOptions();
+//    Map<String, String> options = project != null ? project.getOptions(true)
+//        : DartCore.getOptions();
     StringBuffer s = new StringBuffer();
     for (int i = 0; i < indentationUnits; i++) {
       s.append("  ");
@@ -80,182 +69,6 @@ public class CodeFormatterUtil {
       Assert.isTrue(false, "Formatter created edits with wrong positions: " + e.getMessage()); //$NON-NLS-1$
     }
     return null;
-  }
-
-  /**
-   * Old API. Consider to use format2 (TextEdit)
-   */
-//  public static String format(DartNode node, String string,
-//      int indentationLevel, int[] positions, String lineSeparator, Map options) {
-//
-//    TextEdit edit = format2(node, string, indentationLevel, lineSeparator,
-//        options);
-//    if (edit == null) {
-//      DartToolsPlugin.logErrorMessage("formatter failed to format (no edit returned). Will use unformatted text instead. node: " + node.getNodeType() + ", string: " + string); //$NON-NLS-1$ //$NON-NLS-2$
-//      return string;
-//    }
-//    return getOldAPICompatibleResult(string, edit, indentationLevel, positions,
-//        lineSeparator, options);
-//  }
-
-  /**
-   * Old API. Consider to use format2 (TextEdit)
-   */
-  public static String format(int kind, String string, int offset, int length,
-      int indentationLevel, int[] positions, String lineSeparator, Map<String, String> options) {
-    TextEdit edit = format2(kind, string, offset, length, indentationLevel, lineSeparator, options);
-    if (edit == null) {
-      //DartToolsPlugin.logErrorMessage("formatter failed to format (no edit returned). Will use unformatted text instead. kind: " + kind + ", string: " + string); //$NON-NLS-1$ //$NON-NLS-2$
-      return string.substring(offset, offset + length);
-    }
-    String formatted = getOldAPICompatibleResult(
-        string,
-        edit,
-        indentationLevel,
-        positions,
-        lineSeparator,
-        options);
-    return formatted.substring(offset, formatted.length() - (string.length() - (offset + length)));
-  }
-
-  public static String format(int kind, String string, int indentationLevel, int[] positions,
-      String lineSeparator, DartProject project) {
-    Map<String, String> options = project != null ? project.getOptions(true) : null;
-    return format(
-        kind,
-        string,
-        0,
-        string.length(),
-        indentationLevel,
-        positions,
-        lineSeparator,
-        options);
-  }
-
-  // transition code
-
-  /**
-   * Old API. Consider to use format2 (TextEdit)
-   */
-  public static String format(int kind, String string, int indentationLevel, int[] positions,
-      String lineSeparator, Map<String, String> options) {
-    return format(
-        kind,
-        string,
-        0,
-        string.length(),
-        indentationLevel,
-        positions,
-        lineSeparator,
-        options);
-  }
-
-  /**
-   * Creates edits that describe how to format the given string. Returns <code>null</code> if the
-   * code could not be formatted for the given kind.
-   * 
-   * @throws IllegalArgumentException If the offset and length are not inside the string, a
-   *           IllegalArgumentException is thrown.
-   */
-  public static TextEdit format2(DartNode node, String str, int indentationLevel,
-      String lineSeparator, Map<String, String> options) {
-    int code = 0;;
-    String prefix = ""; //$NON-NLS-1$
-    String suffix = ""; //$NON-NLS-1$
-    DartX.todo();
-//    if (node instanceof DartStatement) {
-//      code = CodeFormatter.K_STATEMENTS;
-//      if (node.getNodeType() == DartNode.SWITCH_CASE) {
-//        prefix = "switch(1) {"; //$NON-NLS-1$
-//        suffix = "}"; //$NON-NLS-1$
-//        code = CodeFormatter.K_STATEMENTS;
-//      }
-//    } else if (node instanceof DartExpression
-//        && node.getNodeType() != DartNode.VARIABLE_DECLARATION_EXPRESSION) {
-//      code = CodeFormatter.K_EXPRESSION;
-//    } else if (node instanceof DartBlock) {
-//      code = CodeFormatter.K_CLASS_BODY_DECLARATIONS;
-//    } else {
-//      switch (node.getNodeType()) {
-//        case DartNode.ARRAY_TYPE:
-//        case DartNode.PRIMITIVE_TYPE:
-//        case DartNode.QUALIFIED_TYPE:
-//        case DartNode.SIMPLE_TYPE:
-//          suffix = " x;"; //$NON-NLS-1$
-//          code = CodeFormatter.K_CLASS_BODY_DECLARATIONS;
-//          break;
-//        case DartNode.JAVASCRIPT_UNIT:
-//          code = CodeFormatter.K_JAVASCRIPT_UNIT;
-//          break;
-//        case DartNode.VARIABLE_DECLARATION_EXPRESSION:
-//        case DartNode.SINGLE_VARIABLE_DECLARATION:
-//          suffix = ";"; //$NON-NLS-1$
-//          code = CodeFormatter.K_STATEMENTS;
-//          break;
-//        case DartNode.VARIABLE_DECLARATION_FRAGMENT:
-//          prefix = "A "; //$NON-NLS-1$
-//          suffix = ";"; //$NON-NLS-1$
-//          code = CodeFormatter.K_STATEMENTS;
-//          break;
-//        case DartNode.PACKAGE_DECLARATION:
-//        case DartNode.IMPORT_DECLARATION:
-//          suffix = "\nclass A {}"; //$NON-NLS-1$
-//          code = CodeFormatter.K_JAVASCRIPT_UNIT;
-//          break;
-//        case DartNode.JSDOC:
-//          suffix = "void foo();"; //$NON-NLS-1$
-//          code = CodeFormatter.K_CLASS_BODY_DECLARATIONS;
-//          break;
-//        case DartNode.CATCH_CLAUSE:
-//          prefix = "try {}"; //$NON-NLS-1$
-//          code = CodeFormatter.K_STATEMENTS;
-//          break;
-//        case DartNode.ANONYMOUS_CLASS_DECLARATION:
-//          prefix = "new A()"; //$NON-NLS-1$
-//          suffix = ";"; //$NON-NLS-1$
-//          code = CodeFormatter.K_STATEMENTS;
-//          break;
-//        case DartNode.MODIFIER:
-//          suffix = " class x {}"; //$NON-NLS-1$
-//          code = CodeFormatter.K_JAVASCRIPT_UNIT;
-//          break;
-//        case DartNode.MEMBER_REF:
-//        case DartNode.FUNCTION_REF:
-//        case DartNode.FUNCTION_REF_PARAMETER:
-//        case DartNode.TAG_ELEMENT:
-//        case DartNode.TEXT_ELEMENT:
-//          // Javadoc formatting not yet supported:
-//          return null;
-//        default:
-//          //Assert.isTrue(false, "Node type not covered: " + node.getClass().getName()); //$NON-NLS-1$
-//          return null;
-//      }
-//    }
-//
-    //TODO (pquitslund): remove
-    return null;
-  }
-
-  /**
-   * Creates edits that describe how to format the given string. Returns <code>null</code> if the
-   * code could not be formatted for the given kind.
-   * 
-   * @throws IllegalArgumentException If the offset and length are not inside the string, a
-   *           IllegalArgumentException is thrown.
-   */
-  public static TextEdit format2(int kind, String string, int offset, int length,
-      int indentationLevel, String lineSeparator, Map<String, String> options) {
-    if (offset < 0 || length < 0 || offset + length > string.length()) {
-      throw new IllegalArgumentException(
-          "offset or length outside of string. offset: " + offset + ", length: " + length + ", string size: " + string.length()); //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
-    }
-    //TODO (pquitslund): remove
-    return null;
-  }
-
-  public static TextEdit format2(int kind, String string, int indentationLevel,
-      String lineSeparator, Map<String, String> options) {
-    return format2(kind, string, 0, string.length(), indentationLevel, lineSeparator, options);
   }
 
   /**
@@ -300,21 +113,6 @@ public class CodeFormatterUtil {
     }
 
     return getCoreOption(project, key, 2);
-  }
-
-  public static TextEdit reformat(int kind, String string, int offset, int length,
-      int indentationLevel, String lineSeparator, Map<String, String> options) {
-    if (offset < 0 || length < 0 || offset + length > string.length()) {
-      throw new IllegalArgumentException(
-          "offset or length outside of string. offset: " + offset + ", length: " + length + ", string size: " + string.length()); //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
-    }
-    //TODO (pquitslund): remove
-    return null;
-  }
-
-  public static TextEdit reformat(int kind, String string, int indentationLevel,
-      String lineSeparator, Map<String, String> options) {
-    return reformat(kind, string, 0, string.length(), indentationLevel, lineSeparator, options);
   }
 
   private static Document createDocument(String string, Position[] positions)
@@ -384,53 +182,6 @@ public class CodeFormatterUtil {
     } catch (NumberFormatException e) {
       return def;
     }
-  }
-
-  private static String getOldAPICompatibleResult(String string, TextEdit edit,
-      int indentationLevel, int[] positions, String lineSeparator, Map<String, String> options) {
-    Position[] p = null;
-
-    if (positions != null) {
-      p = new Position[positions.length];
-      for (int i = 0; i < positions.length; i++) {
-        p[i] = new Position(positions[i], 0);
-      }
-    }
-    String res = evaluateFormatterEdit(string, edit, p);
-
-    if (positions != null) {
-      for (int i = 0; i < positions.length; i++) {
-        Position curr = p[i];
-        positions[i] = curr.getOffset();
-      }
-    }
-    return res;
-  }
-
-  private static TextEdit shifEdit(TextEdit oldEdit, int diff) {
-    TextEdit newEdit;
-    if (oldEdit instanceof ReplaceEdit) {
-      ReplaceEdit edit = (ReplaceEdit) oldEdit;
-      newEdit = new ReplaceEdit(edit.getOffset() - diff, edit.getLength(), edit.getText());
-    } else if (oldEdit instanceof InsertEdit) {
-      InsertEdit edit = (InsertEdit) oldEdit;
-      newEdit = new InsertEdit(edit.getOffset() - diff, edit.getText());
-    } else if (oldEdit instanceof DeleteEdit) {
-      DeleteEdit edit = (DeleteEdit) oldEdit;
-      newEdit = new DeleteEdit(edit.getOffset() - diff, edit.getLength());
-    } else if (oldEdit instanceof MultiTextEdit) {
-      newEdit = new MultiTextEdit();
-    } else {
-      return null; // not supported
-    }
-    TextEdit[] children = oldEdit.getChildren();
-    for (int i = 0; i < children.length; i++) {
-      TextEdit shifted = shifEdit(children[i], diff);
-      if (shifted != null) {
-        newEdit.addChild(shifted);
-      }
-    }
-    return newEdit;
   }
 
 }
