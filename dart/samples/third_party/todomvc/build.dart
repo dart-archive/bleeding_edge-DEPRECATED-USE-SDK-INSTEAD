93b2e778ba69d8558a18fd0bfcfc0a600e8a4b69
sigmund@google.com <sigmund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-08-29 00:07:02 +0000

    Prune the old deploy code. This CL does a few changes:  - includes a few new steps in the deploy transforms that were missing  - deletes emitters.dart, paths.dart (which were only needed for generating    code), bin/dwc.dart (we never used this)  - changes test/run.sh to run from pkg/polymer/.  - updates tests expectations to match the new output  - for now, disable using shadowdom-polyfill in render tests because they    mismatch the results you'd get without the polyfill (css is changed with the    polyfill)
    R=jmesserly@google.com, terry@google.com
    
    Review URL: https://codereview.chromium.org//23445009
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@26828 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/samples/third_party/todomvc/build.dart
+++ dart/samples/third_party/todomvc/build.dart
@@ -5,8 +5,8 @@
 
 import 'dart:io';
 import 'package:polymer/component_build.dart';
+import 'package:polymer/deploy.dart' as deploy;
 
 void main() {
-  var args = new Options().arguments.toList()..addAll(['--', '--deploy']);
-  build(args, ['web/index.html']);
+  build(new Options().arguments, ['web/index.html']).then((_) => deploy.main());
 }
