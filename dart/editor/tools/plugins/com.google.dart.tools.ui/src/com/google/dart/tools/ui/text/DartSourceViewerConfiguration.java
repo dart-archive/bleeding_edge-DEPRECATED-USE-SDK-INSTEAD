6336c108ba5eaa25bc3e44cb1f0215d494a1a5d1
danrubel@google.com <danrubel@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-09-23 11:37:31 +0000

    Adjust reconciler delay to 500 ms
    R=scheglov@google.com
    
    Review URL: https://codereview.chromium.org//24351002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@27755 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/text/DartSourceViewerConfiguration.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/text/DartSourceViewerConfiguration.java
@@ -598,7 +598,7 @@ public class DartSourceViewerConfiguration extends TextSourceViewerConfiguration
       reconciler.setIsIncrementalReconciler(true);
       reconciler.setIsAllowedToModifyDocument(false);
       reconciler.setProgressMonitor(new NullProgressMonitor());
-      reconciler.setDelay(150);
+      reconciler.setDelay(500);
       return reconciler;
     }
     return null;
