07f50e839f274d0a930a2e9d2d2c6a66075b3606
shailentuli@google.com <shailentuli@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-09-30 17:51:49 +0000

    Created the survey sample app.
    R=john@johnmccutchan.com
    
    Review URL: https://codereview.chromium.org//25183002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@28062 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/samples/survey/web/elements/survey/survey_element.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library survey.web.sruvey;
+
+import 'dart:html';
+
+import 'package:polymer/polymer.dart';
+import 'package:survey/models.dart';
+
+/**
+ * The view for a survey element. Use this to add questions to the survey.
+ */
+@CustomTag('survey-element')
+class SurveyElement extends PolymerElement with ObservableMixin {
+  bool get applyAuthorStyles => true;
+  @observable SurveyApp app = new SurveyApp();
+
+  created() {
+    super.created();
+    app = appModel;
+    appModel.questions = toObservable([]);
+  }
+
+  addNewQuestion(Event e, var detail, Element sender) {
+    e.preventDefault();
+    app.questions.add(new Question());
+  }
+}
+
