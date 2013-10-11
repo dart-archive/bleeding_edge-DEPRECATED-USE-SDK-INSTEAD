54458e6818588cb1c9e83ad79ebc675c63ac8b60
scheglov@google.com <scheglov@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-10-11 16:29:11 +0000

    After waiting for workers, check additionally that all sources were analyzed.
    R=danrubel@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//26809003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@28525 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/refactoring/RefactoringUtils.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/refactoring/RefactoringUtils.java
@@ -14,8 +14,15 @@
 
 package com.google.dart.tools.ui.internal.refactoring;
 
+import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.Uninterruptibles;
+import com.google.dart.engine.context.AnalysisContext;
+import com.google.dart.engine.source.Source;
+import com.google.dart.tools.core.DartCore;
+import com.google.dart.tools.core.analysis.model.Project;
+import com.google.dart.tools.core.analysis.model.PubFolder;
 import com.google.dart.tools.core.internal.builder.AnalysisWorker;
+import com.google.dart.tools.ui.DartToolsPlugin;
 import com.google.dart.tools.ui.internal.text.editor.DartEditor;
 
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -24,6 +31,7 @@ import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.IJobManager;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -31,6 +39,8 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.progress.IProgressService;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -45,6 +55,7 @@ public class RefactoringUtils {
   public static boolean waitReadyForRefactoring() {
     Control focusControl = Display.getCurrent().getFocusControl();
     try {
+      // wait for analysis
       IProgressService progressService = PlatformUI.getWorkbench().getProgressService();
       progressService.busyCursorWhile(new IRunnableWithProgress() {
         @Override
@@ -52,6 +63,11 @@ public class RefactoringUtils {
           waitReadyForRefactoring(pm);
         }
       });
+      // at this point all contexts should have been analyzed, verify this
+      if (!verifyAnalysisDone()) {
+        return false;
+      }
+      // OK
       return true;
     } catch (Throwable ie) {
       return false;
@@ -107,5 +123,57 @@ public class RefactoringUtils {
       }
       Uninterruptibles.sleepUninterruptibly(5, TimeUnit.MILLISECONDS);
     }
+  }
+
+  /**
+   * @return {@code true} if all {@link AnalysisContext} are actually fully analyzed, so it is safe
+   *         to perform refactoring. Otherwise shows error dialog.
+   */
+  private static boolean verifyAnalysisDone() {
+    // prepare contexts
+    Map<AnalysisContext, Project> contextToProject = Maps.newHashMap();
+    for (Project project : DartCore.getProjectManager().getProjects()) {
+      // default context
+      AnalysisContext defaultContext = project.getDefaultContext();
+      contextToProject.put(defaultContext, project);
+      // separate Pub folders
+      for (PubFolder pubFolder : project.getPubFolders()) {
+        AnalysisContext context = pubFolder.getContext();
+        if (context != defaultContext) {
+          contextToProject.put(context, project);
+        }
+      }
+    }
+    // prepare description for unsafe sources
+    StringBuilder sb = new StringBuilder();
+    for (Entry<AnalysisContext, Project> entry : contextToProject.entrySet()) {
+      AnalysisContext context = entry.getKey();
+      Project project = entry.getValue();
+      Source[] sources = context.getRefactoringUnsafeSources();
+      // all sources are ready
+      if (sources.length == 0) {
+        continue;
+      }
+      // append project and its sources
+      sb.append(project.toString() + "=[");
+      for (Source source : sources) {
+        sb.append(source.getFullName());
+        sb.append(" ");
+      }
+      sb.setLength(sb.length() - 1);
+      sb.append("]\n\n");
+    }
+    // show unsafe sources
+    if (sb.length() != 0) {
+      String msg = sb.toString();
+      DartCore.logInformation(msg);
+      MessageDialog.openError(
+          DartToolsPlugin.getActiveWorkbenchShell(),
+          "Not all sources have been analyzed",
+          msg);
+      return false;
+    }
+    // OK
+    return true;
   }
 }
