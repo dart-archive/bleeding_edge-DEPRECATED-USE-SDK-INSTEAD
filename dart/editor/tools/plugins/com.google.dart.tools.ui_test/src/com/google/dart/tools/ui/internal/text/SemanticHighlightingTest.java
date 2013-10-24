2f2490533464479b0f46f4ca7c2d5e6ee35e3193
lrn@google.com <lrn@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-10-24 12:23:34 +0000

    Comment out 'const deprecated' declarations in editor tests.
    The introduction of 'deprecated' in the core library causes this declaration to give an unexpected warning.
    
    This is a quick fix to keep the tests happy. I didn't dare remove the library, but please see if this can be made prettier.
    
    Review URL: https://codereview.chromium.org//40073002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@29169 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui_test/src/com/google/dart/tools/ui/internal/text/SemanticHighlightingTest.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui_test/src/com/google/dart/tools/ui/internal/text/SemanticHighlightingTest.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2013, the Dart project authors.
- * 
+ *
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -414,7 +414,7 @@ public class SemanticHighlightingTest extends
         makeSource(
             "// filler filler filler filler filler filler filler filler filler filler",
             "library meta;",
-            "const deprecated = 42;",
+            "// const deprecated = 42;",
             ""));
     preparePositions(
         "// filler filler filler filler filler filler filler filler filler filler",
@@ -454,7 +454,7 @@ public class SemanticHighlightingTest extends
         makeSource(
             "// filler filler filler filler filler filler filler filler filler filler",
             "library meta;",
-            "const deprecated = 42;",
+            "// const deprecated = 42;",
             ""));
     setFileContent(
         "ModernLib.dart",
@@ -469,7 +469,7 @@ public class SemanticHighlightingTest extends
             "import '/meta.dart';",
             "@deprecated",
             "library deprecatedLib;",
-            "const deprecated = 0;",
+            "// const deprecated = 0;",
             ""));
     preparePositions(
         "// filler filler filler filler filler filler filler filler filler filler",
