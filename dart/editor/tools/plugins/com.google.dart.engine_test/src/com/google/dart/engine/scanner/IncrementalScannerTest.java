0cf0ee31ad8ef23b47d823698cb7670dde775ab8
brianwilkerson@google.com <brianwilkerson@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-11-11 18:51:52 +0000

    Improve tests for incremental analysis
    R=danrubel@google.com
    
    Review URL: https://codereview.chromium.org//68973003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@30166 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.engine_test/src/com/google/dart/engine/scanner/IncrementalScannerTest.java
+++ dart/editor/tools/plugins/com.google.dart.engine_test/src/com/google/dart/engine/scanner/IncrementalScannerTest.java
@@ -20,176 +20,224 @@ import com.google.dart.engine.source.TestSource;
 import com.google.dart.engine.utilities.collection.TokenMap;
 
 public class IncrementalScannerTest extends EngineTestCase {
+  /**
+   * The scanner used to perform incremental scanning, or {@code null} if {@link #scan} has not been
+   * invoked.
+   */
+  private IncrementalScanner incrementalScanner;
 
+  /**
+   * The first token from the token stream resulting from parsing the original source, or
+   * {@code null} if {@link #scan} has not been invoked.
+   */
   private Token originalTokens;
 
-  public void test_rescan_addedBeforeIdentifier1() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        "xa + b;");
-    assertEquals("xa", scanner.getFirstToken().getLexeme());
-    assertEquals("xa", scanner.getLastToken().getLexeme());
+  public void fail_delete_identifier_beginning() {
+    // "abs + b;"
+    // "s + b;")
+    scan("", "ab", "", "s + b;");
+    assertTokens("s");
   }
 
-  public void test_rescan_addedBeforeIdentifier2() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        "a + xb;");
-    assertEquals("xb", scanner.getFirstToken().getLexeme());
-    assertEquals("xb", scanner.getLastToken().getLexeme());
+  public void fail_delete_mergeTokens() {
+    // "a + b + c;"
+    // "ac;")
+    scan("a", " + b + ", "", "c;");
+    assertTokens("ac");
   }
 
-  public void test_rescan_addedNewIdentifier1() {
-    IncrementalScanner scanner = assertTokens(//
-        "a;  c;",
-        "a; b c;");
-    assertEquals("b", scanner.getFirstToken().getLexeme());
-    assertEquals("b", scanner.getLastToken().getLexeme());
+  public void fail_replace_multiple_partialFirstAndLast() {
+    // "aa + bb;"
+    // "ab * ab;")
+    scan("a", "a + b", "b * a", "b;");
+    assertTokens("ab", "*", "ab");
   }
 
-  public void test_rescan_addedNewIdentifier2() {
-    IncrementalScanner scanner = assertTokens(//
-        "a;  c;",
-        "a;b  c;");
-    assertEquals("b", scanner.getFirstToken().getLexeme());
-    assertEquals("b", scanner.getLastToken().getLexeme());
-    Token oldToken = originalTokens.getNext();
-    assertSame(TokenType.SEMICOLON, oldToken.getType());
-    Token newToken = scanner.getTokenMap().get(oldToken);
-    assertNotNull(newToken);
-    assertEquals(TokenType.SEMICOLON, newToken.getType());
-    assertNotSame(oldToken, newToken);
+  public void test_delete_identifier_end() {
+    // "abs + b;"
+    // "a + b;")
+    scan("a", "bs", "", " + b;");
+    assertTokens("a");
   }
 
-  public void test_rescan_addedToIdentifier1() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        "abs + b;");
-    assertEquals("abs", scanner.getFirstToken().getLexeme());
-    assertEquals("abs", scanner.getLastToken().getLexeme());
-    Token oldToken = originalTokens.getNext();
-    assertEquals(TokenType.PLUS, oldToken.getType());
-    Token newToken = scanner.getTokenMap().get(oldToken);
-    assertNotNull(newToken);
-    assertEquals(TokenType.PLUS, newToken.getType());
-    assertNotSame(oldToken, newToken);
+  public void test_delete_identifier_middle() {
+    // "abs + b;"
+    // "as + b;")
+    scan("a", "b", "", "s + b;");
+    assertTokens("as");
   }
 
-  public void test_rescan_addedToIdentifier2() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        "a + by;");
-    assertEquals("by", scanner.getFirstToken().getLexeme());
-    assertEquals("by", scanner.getLastToken().getLexeme());
+  public void test_insert_afterIdentifier1() {
+    // "a + b;"
+    // "abs + b;"
+    scan("a", "", "bs", " + b;");
+    assertTokens("abs");
+    assertReplaced(1, "+");
   }
 
-  public void test_rescan_appendWhitespace1() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        "a + b; ");
-    assertNull(scanner.getFirstToken());
-    assertNull(scanner.getLastToken());
+  public void test_insert_afterIdentifier2() {
+    // "a + b;"
+    // "a + by;"
+    scan("a + b", "", "y", ";");
+    assertTokens("by");
   }
 
-  public void test_rescan_appendWhitespace2() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b; ",
-        "a + b;  ");
-    assertNull(scanner.getFirstToken());
-    assertNull(scanner.getLastToken());
+  public void test_insert_beforeIdentifier() {
+    // "a + b;"
+    // "a + xb;")
+    scan("a + ", "", "x", "b;");
+    assertTokens("xb");
   }
 
-  public void test_rescan_insertedPeriod() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        "a + b.;");
-    assertEquals(".", scanner.getFirstToken().getLexeme());
-    assertEquals(".", scanner.getLastToken().getLexeme());
+  public void test_insert_beforeIdentifier_firstToken() {
+    // "a + b;"
+    // "xa + b;"
+    scan("", "", "x", "a + b;");
+    assertTokens("xa");
   }
 
-  public void test_rescan_insertedPeriodBetweenIdentifiers1() {
-    IncrementalScanner scanner = assertTokens(//
-        "a b;",
-        "a. b;");
-    assertEquals(".", scanner.getFirstToken().getLexeme());
-    assertEquals(".", scanner.getLastToken().getLexeme());
+  public void test_insert_convertOneFunctionToTwo() {
+    // "f() {}"
+    // "f() => 0; g() {}"
+    scan("f()", "", " => 0; g()", " {}");
+    assertTokens("=>", "0", ";", "g", "(", ")");
   }
 
-  public void test_rescan_insertedPeriodBetweenIdentifiers2() {
-    IncrementalScanner scanner = assertTokens(//
-        "a b;",
-        "a .b;");
-    assertEquals(".", scanner.getFirstToken().getLexeme());
-    assertEquals(".", scanner.getLastToken().getLexeme());
+  public void test_insert_newIdentifier1() {
+    // "a;  c;"
+    // "a; b c;"
+    scan("a; ", "", "b", " c;");
+    assertTokens("b");
   }
 
-  public void test_rescan_insertedPeriodBetweenIdentifiers3() {
-    IncrementalScanner scanner = assertTokens(//
-        "a  b;",
-        "a . b;");
-    assertEquals(".", scanner.getFirstToken().getLexeme());
-    assertEquals(".", scanner.getLastToken().getLexeme());
+  public void test_insert_newIdentifier2() {
+    // "a;  c;"
+    // "a;b  c;"
+    scan("a;", "", "b", "  c;");
+    assertTokens("b");
+    assertReplaced(1, ";");
   }
 
-  public void test_rescan_insertedPeriodIdentifier() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        "a + b.x;");
-    assertEquals(".", scanner.getFirstToken().getLexeme());
-    assertEquals("x", scanner.getLastToken().getLexeme());
+  public void test_insert_period() {
+    // "a + b;"
+    // "a + b.;"
+    scan("a + b", "", ".", ";");
+    assertTokens(".");
   }
 
-  public void test_rescan_insertedPeriodInsideExistingIdentifier() {
-    IncrementalScanner scanner = assertTokens(//
-        "ab;",
-        "a.b;");
-    assertEquals("a", scanner.getFirstToken().getLexeme());
-    assertEquals("b", scanner.getLastToken().getLexeme());
+  public void test_insert_period_betweenIdentifiers1() {
+    // "a b;"
+    // "a. b;"
+    scan("a", "", ".", " b");
+    assertTokens(".");
   }
 
-  public void test_rescan_insertLeadingWhitespace() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        " a + b;");
-    assertNull(scanner.getFirstToken());
-    assertNull(scanner.getLastToken());
+  public void test_insert_period_betweenIdentifiers2() {
+    // "a b;"
+    // "a .b;"
+    scan("a ", "", ".", "b;");
+    assertTokens(".");
   }
 
-  public void test_rescan_insertWhitespace() {
-    IncrementalScanner scanner = assertTokens(//
-        "a + b;",
-        "a  + b;");
-    assertNull(scanner.getFirstToken());
-    assertNull(scanner.getLastToken());
+  public void test_insert_period_betweenIdentifiers3() {
+    // "a  b;"
+    // "a . b;"
+    scan("a ", "", ".", " b;");
+    assertTokens(".");
   }
 
-  public void test_rescan_insertWhitespaceWithMultipleComments() {
-    IncrementalScanner scanner = assertTokens(//
-        createSource(//
-            "//comment",
-            "//comment2",
-            "a + b;"),
-        createSource(//
-            "//comment",
-            "//comment2",
-            "a  + b;"));
-    assertNull(scanner.getFirstToken());
-    assertNull(scanner.getLastToken());
+  public void test_insert_period_insideExistingIdentifier() {
+    // "ab;"
+    // "a.b;"
+    scan("a", "", ".", "b;");
+    assertTokens("a", ".", "b");
   }
 
-  public void test_rescan_oneFunctionToTwo() {
-    IncrementalScanner scanner = assertTokens(//
-        "f() {}",
-        "f() => 0; g() {}");
-    assertEquals("=>", scanner.getFirstToken().getLexeme());
-    assertEquals(")", scanner.getLastToken().getLexeme());
+  public void test_insert_periodAndIdentifier() {
+    // "a + b;"
+    // "a + b.x;"
+    scan("a + b", "", ".x", ";");
+    assertTokens(".", "x");
+  }
+
+  public void test_insert_whitespace_beginning_beforeToken() {
+    // "a + b;"
+    // " a + b;"
+    scan("", "", " ", "a + b;");
+    assertTokens();
+  }
+
+  public void test_insert_whitespace_betweenTokens() {
+    // "a + b;"
+    // "a  + b;"
+    scan("a ", "", " ", "+ b;");
+    assertTokens();
+  }
+
+  public void test_insert_whitespace_end_afterToken() {
+    // "a + b;"
+    // "a + b; "
+    scan("a + b;", "", " ", "");
+    assertTokens();
+  }
+
+  public void test_insert_whitespace_end_afterWhitespace() {
+    // "a + b; "
+    // "a + b;  "
+    scan("a + b; ", "", " ", "");
+    assertTokens();
+  }
+
+  public void test_insert_whitespace_withMultipleComments() {
+    // "//comment", "//comment2", "a + b;"
+    // "//comment", "//comment2", "a  + b;"
+    scan(createSource(//
+        "//comment",
+        "//comment2",
+        "a"), "", " ", " + b;");
+    assertTokens();
+  }
+
+  public void test_replace_identifier_beginning() {
+    // "bell + b;"
+    // "fell + b;")
+    scan("", "b", "f", "ell + b;");
+    assertTokens("fell");
+  }
+
+  public void test_replace_identifier_end() {
+    // "bell + b;"
+    // "belt + b;")
+    scan("bel", "l", "t", " + b;");
+    assertTokens("belt");
+  }
+
+  public void test_replace_identifier_middle() {
+    // "first + b;"
+    // "frost + b;")
+    scan("f", "ir", "ro", "st + b;");
+    assertTokens("frost");
+  }
+
+  public void test_replace_operator_oneForMany() {
+    // "a + b;"
+    // "a * c - b;")
+    scan("a ", "+", "* c -", " b;");
+    assertTokens("*", "c", "-");
+  }
+
+  public void test_replace_operator_oneForOne() {
+    // "a + b;"
+    // "a * b;")
+    scan("a ", "+", "*", " b;");
+    assertTokens("*");
   }
 
   public void test_tokenMap() throws Exception {
-    IncrementalScanner scanner = assertTokens(//
-        "main() {a + b;}",
-        "main() { a + b;}");
-    TokenMap tokenMap = scanner.getTokenMap();
+    // "main() {a + b;}"
+    // "main() { a + b;}"
+    scan("main() {", "", " ", "a + b;}");
+    TokenMap tokenMap = incrementalScanner.getTokenMap();
     Token oldToken = originalTokens;
     while (oldToken.getType() != TokenType.EOF) {
       Token newToken = tokenMap.get(oldToken);
@@ -200,24 +248,55 @@ public class IncrementalScannerTest extends EngineTestCase {
     }
   }
 
-  private IncrementalScanner assertTokens(String originalContents, String modifiedContents) {
-    //
-    // Compute the location of the deleted and inserted text.
-    //
-    int originalLength = originalContents.length();
-    int modifiedLength = modifiedContents.length();
-    int replaceStart = 0;
-    while (replaceStart < originalLength && replaceStart < modifiedLength
-        && originalContents.charAt(replaceStart) == modifiedContents.charAt(replaceStart)) {
-      replaceStart++;
+  private void assertReplaced(int tokenOffset, String lexeme) {
+    Token oldToken = originalTokens;
+    for (int i = 0; i < tokenOffset; i++) {
+      oldToken = oldToken.getNext();
     }
-    int originalEnd = originalLength - 1;
-    int modifiedEnd = modifiedLength - 1;
-    while (originalEnd >= replaceStart && modifiedEnd >= replaceStart
-        && originalContents.charAt(originalEnd) == modifiedContents.charAt(modifiedEnd)) {
-      originalEnd--;
-      modifiedEnd--;
+    assertEquals(lexeme, oldToken.getLexeme());
+    Token newToken = incrementalScanner.getTokenMap().get(oldToken);
+    assertNotNull(newToken);
+    assertEquals(lexeme, newToken.getLexeme());
+    assertNotSame(oldToken, newToken);
+  }
+
+  private void assertTokens(String... lexemes) {
+    Token first = incrementalScanner.getFirstToken();
+    Token last = incrementalScanner.getLastToken();
+    int count = lexemes.length;
+    if (count == 0) {
+      assertNull(first);
+      assertNull(last);
+    } else if (count == 1) {
+      assertNotNull(first);
+      assertEquals(lexemes[0], first.getLexeme());
+      assertSame(first, last);
+    } else {
+      for (String lexeme : lexemes) {
+        assertNotNull(first);
+        assertEquals(lexeme, first.getLexeme());
+        first = first.getNext();
+      }
+      assertSame(last.getNext(), first);
     }
+  }
+
+  /**
+   * Given a description of the original and modified contents, perform an incremental scan of the
+   * two pieces of text.
+   * 
+   * @param prefix the unchanged text before the edit region
+   * @param removed the text that was removed from the original contents
+   * @param added the text that was added to the modified contents
+   * @param suffix the unchanged text after the edit region
+   */
+  private void scan(String prefix, String removed, String added, String suffix) {
+    //
+    // Compute the information needed to perform the test.
+    //
+    String originalContents = prefix + removed + suffix;
+    String modifiedContents = prefix + added + suffix;
+    int replaceStart = prefix.length();
     Source source = new TestSource();
     //
     // Scan the original contents.
@@ -243,10 +322,15 @@ public class IncrementalScannerTest extends EngineTestCase {
     // Incrementally scan the modified contents.
     //
     GatheringErrorListener incrementalListener = new GatheringErrorListener();
-    IncrementalScanner incrementalScanner = new IncrementalScanner(source, new CharSequenceReader(
-        modifiedContents), incrementalListener);
-    Token incrementalTokens = incrementalScanner.rescan(originalTokens, replaceStart, originalEnd
-        - replaceStart + 1, modifiedEnd - replaceStart + 1);
+    incrementalScanner = new IncrementalScanner(
+        source,
+        new CharSequenceReader(modifiedContents),
+        incrementalListener);
+    Token incrementalTokens = incrementalScanner.rescan(
+        originalTokens,
+        replaceStart,
+        removed.length(),
+        added.length());
     //
     // Validate that the results of the incremental scan are the same as the full scan of the
     // modified source.
@@ -273,7 +357,5 @@ public class IncrementalScannerTest extends EngineTestCase {
     assertSame("Too many tokens", TokenType.EOF, incrementalToken.getType());
     assertSame("Not enough tokens", TokenType.EOF, modifiedTokens.getType());
     // TODO(brianwilkerson) Verify that the errors are correct?
-
-    return incrementalScanner;
   }
 }
