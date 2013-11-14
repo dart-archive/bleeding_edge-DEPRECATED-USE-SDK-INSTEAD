1f30970571ab2a04cbe426b7fd1cb286e1762985
pquitslund@google.com <pquitslund@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-11-14 23:56:04 +0000

    Dirty editor handling on project close (dartbug.com/14957).
    R=scheglov@google.com
    
    Review URL: https://codereview.chromium.org//61823010
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@30297 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/projects/HideProjectAction.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/projects/HideProjectAction.java
@@ -15,23 +15,14 @@ package com.google.dart.tools.ui.internal.projects;
 
 import com.google.dart.tools.ui.DartToolsPlugin;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.window.IShellProvider;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorReference;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.CloseResourceAction;
-import org.eclipse.ui.part.FileEditorInput;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.eclipse.ui.ide.IDE;
 
 /**
  * Standard action for hiding the currently selected project(s).
@@ -65,8 +56,8 @@ public class HideProjectAction extends CloseResourceAction {
   protected void invokeOperation(IResource resource, IProgressMonitor monitor) throws CoreException {
 
     try {
-      //preemptively close associated editors where we can, deferring only to super for dirty ones
-      closeCleanEditors(resource);
+      //preemptively close associated editors
+      closeEditors(resource);
 
     } catch (Throwable e) {
       //if we hit any exceptions, log them and trust super to do the right thing
@@ -77,15 +68,14 @@ public class HideProjectAction extends CloseResourceAction {
     }
   }
 
-  private void closeCleanEditors(final IResource project) throws Throwable {
+  private void closeEditors(final IResource project) throws Throwable {
     final Throwable[] ex = new Throwable[1];
     Display.getDefault().syncExec(new Runnable() {
       @Override
       public void run() {
         try {
-          IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-          IEditorReference[] editorRefs = collectEditorReferences(activePage, project);
-          activePage.closeEditors(editorRefs, false);
+          // Prompt occurs in super#run(), don't nag again
+          IDE.saveAllEditors(new IResource[] {project}, false);
         } catch (Throwable th) {
           ex[0] = th;
         }
@@ -94,30 +84,6 @@ public class HideProjectAction extends CloseResourceAction {
     if (ex[0] != null) {
       throw ex[0];
     }
-  }
-
-  private IEditorReference[] collectEditorReferences(IWorkbenchPage activePage, IResource project)
-      throws PartInitException {
-
-    List<IEditorReference> references = new ArrayList<IEditorReference>();
-
-    //grab editor references for non-dirty resources associated w/projects slated to close
-    for (IEditorReference editorRef : activePage.getEditorReferences()) {
-
-      IEditorInput input = editorRef.getEditorInput();
-      if (input instanceof FileEditorInput) {
-        FileEditorInput fileEditorInput = (FileEditorInput) input;
-        //defer dirty file handling to the super implementation
-        if (!editorRef.isDirty()) {
-          IFile file = fileEditorInput.getFile();
-          if (file.getProject().equals(project)) {
-            references.add(editorRef);
-          }
-        }
-      }
-    }
-
-    return references.toArray(new IEditorReference[] {});
   }
 
 }
