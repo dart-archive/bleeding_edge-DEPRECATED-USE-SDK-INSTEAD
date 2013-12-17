2a9e45c657265f8783f0063ee8753108b1327c0f
scheglov@google.com <scheglov@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2013-12-17 07:20:35 +0000

    Add PubspecException.java
    TBR
    
    R=keertip@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//110723008
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@31183 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/pub/PubspecException.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.dart.tools.core.pub;
+
+/**
+ * Description of {@code pubspec.yaml} parsing problem.
+ */
+public class PubspecException {
+  private final String message;
+  private final int line;
+  private final int charStart;
+  private final int charEnd;
+
+  public PubspecException(String message, int line, int charStart, int charEnd) {
+    this.message = message;
+    this.line = line;
+    this.charStart = charStart;
+    this.charEnd = charEnd;
+  }
+
+  public int getCharEnd() {
+    return charEnd;
+  }
+
+  public int getCharStart() {
+    return charStart;
+  }
+
+  public int getLine() {
+    return line;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
