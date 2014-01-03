2780b5c9dacbbc0ca42e952c7037c588569bc3fe
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-01-03 23:12:42 +0000

    fix windows tests
    R=danrubel@google.com
    
    Review URL: https://codereview.chromium.org//122683006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@31455 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.engine_test/src/com/google/dart/engine/source/ExplicitPackageUriResolverTest.java
+++ dart/editor/tools/plugins/com.google.dart.engine_test/src/com/google/dart/engine/source/ExplicitPackageUriResolverTest.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2012, the Dart project authors.
- *
+ * 
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -92,14 +92,17 @@ public class ExplicitPackageUriResolverTest extends TestCase {
   }
 
   public void test_resolve_resolvePathToPackage() throws Exception {
-    File directory = createFile("/does/not/exist/foo_project");
+    File directory = createFile("/src/foo/bar/baz/lib");
+    File dir2 = createFile("/gen/foo/bar/baz");
     String packages = "{\"packages\":{\"unittest\": [\"/dart/unittest/lib\"],"
-        + "\"foo.bar.baz\": [\"/src/foo/bar/baz/lib\", \"/gen/foo/bar/baz\"]}}";
+        + "\"foo.bar.baz\": [\"" + directory.getAbsolutePath() + "\",\"" + dir2.getAbsolutePath()
+        + "\"]}}";
     ExplicitPackageUriResolver resolver = new MockExplicitPackageUriResolver(directory, packages);
-    String resolvedPath = resolver.resolvePathToPackage("baz/lib");
+    File file = createFile("/baz/lib");
+    String resolvedPath = resolver.resolvePathToPackage(file.getAbsolutePath());
     assertNotNull(resolvedPath);
     assertEquals("foo.bar.baz", resolvedPath);
-    resolvedPath = resolver.resolvePathToPackage("dart/mypackage");
+    resolvedPath = resolver.resolvePathToPackage(createFile("dart/mypackage").getAbsolutePath());
     assertNull(resolvedPath);
   }
 
--- dart/editor/tools/plugins/com.google.dart.tools.debug.core_test/src/com/google/dart/tools/debug/core/dartium/BreakpointManagerTest.java
+++ dart/editor/tools/plugins/com.google.dart.tools.debug.core_test/src/com/google/dart/tools/debug/core/dartium/BreakpointManagerTest.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2013, the Dart project authors.
- *
+ * 
  * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -16,6 +16,8 @@ package com.google.dart.tools.debug.core.dartium;
 
 import com.google.dart.engine.source.ExplicitPackageUriResolver;
 import com.google.dart.engine.source.ExplicitPackageUriResolverTest;
+
+import static com.google.dart.engine.utilities.io.FileUtilities2.createFile;
 
 import junit.framework.TestCase;
 
@@ -33,8 +35,11 @@ public class BreakpointManagerTest extends TestCase {
 
     @Override
     protected String resolvePathToPackage(IFile resource, String filePath) {
+      File directory = createFile("/src/foo/bar/baz/lib");
+      File dir2 = createFile("/gen/foo/bar/baz");
       String packages = "{\"packages\":{\"unittest\": [\"/dart/unittest/lib\"],"
-          + "\"foo.bar.baz\": [\"/src/foo/bar/baz/lib\", \"/gen/foo/bar/baz\"]}}";
+          + "\"foo.bar.baz\": [\"" + directory.getAbsolutePath() + "\",\"" + dir2.getAbsolutePath()
+          + "\"]}}";
       ExplicitPackageUriResolver resolver = new ExplicitPackageUriResolverTest.MockExplicitPackageUriResolver(
           new File("root"),
           packages);
@@ -46,12 +51,12 @@ public class BreakpointManagerTest extends TestCase {
   public void test_getPackagePath() {
     MockBreakpointManager manager = new MockBreakpointManager();
 
-    String path = manager.getPackagePath("baz/lib/myLib.dart", null);
+    String path = manager.getPackagePath(createFile("/baz/lib/myLib.dart").getAbsolutePath(), null);
     assertNotNull(path);
     assertEquals("foo.bar.baz/myLib.dart", path);
-    path = manager.getPackagePath("baz/src/myLib.dart", null);
+    path = manager.getPackagePath(createFile("/baz/src/myLib.dart").getAbsolutePath(), null);
     assertNull(path);
-    path = manager.getPackagePath("bar/baz/lib/util/util.dart", null);
+    path = manager.getPackagePath(createFile("/bar/baz/lib/util/util.dart").getAbsolutePath(), null);
     assertNotNull(path);
     assertEquals("foo.bar.baz/util/util.dart", path);
   }
