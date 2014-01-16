dc9648711d787fcb50aca07c94d73e80a2675f05
sra@google.com <sra@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-01-16 01:07:48 +0000

    Add swarm back to samples/
    This is a temporary re-addition of samples/swarm in an attempt solve some build problems.
    
    Review URL: https://codereview.chromium.org//139703006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@31864 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/samples/swarm/swarm_ui_lib/base/base.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library base;
+import 'dart:html';
+import 'dart:async';
+import 'dart:math';
+import '../observable/observable.dart';
+import '../util/utilslib.dart';
+
+part 'AnimationScheduler.dart';
+part 'Device.dart';
+part 'DomWrapper.dart';
+part 'Env.dart';
+part 'Size.dart';
