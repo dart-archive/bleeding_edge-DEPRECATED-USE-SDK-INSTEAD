58663781808687d62efd36c0eb88988ab1e12417
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-03-04 20:46:42 +0000

    mark build as derived when importing project dartbug.com/16683
    R=devoncarew@google.com
    
    Review URL: https://codereview.chromium.org//186653005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@33300 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.eclipse.ui/src/com/google/dart/eclipse/wizards/ImportFolderWizard.java
+++ dart/editor/tools/plugins/com.google.dart.eclipse.ui/src/com/google/dart/eclipse/wizards/ImportFolderWizard.java
@@ -13,7 +13,14 @@
  */
 package com.google.dart.eclipse.wizards;
 
+import com.google.dart.tools.core.DartCore;
+
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceProxy;
+import org.eclipse.core.resources.IResourceProxyVisitor;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IImportWizard;
@@ -25,6 +32,27 @@ import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
  * Import Dart Wizard. Imports existing Dart source as a project.
  */
 public class ImportFolderWizard extends Wizard implements IImportWizard, INewWizard {
+
+  /**
+   * A visitor that checks for the build directory and marks it as derived.
+   */
+  class BuildDirectoryFinder implements IResourceProxyVisitor {
+
+    @Override
+    public boolean visit(IResourceProxy proxy) throws CoreException {
+      if (proxy.getType() == IResource.FOLDER) {
+        if (proxy.getName().equals(DartCore.BUILD_DIRECTORY_NAME)) {
+          IFolder folder = (IFolder) proxy.requestResource();
+          if (DartCore.isBuildDirectory(folder)) {
+            folder.setDerived(true, null);
+          }
+          return false;
+        }
+        return true;
+      }
+      return true;
+    }
+  }
 
   private IWorkbench workbench;
   private ImportFolderWizardPage page;
@@ -57,9 +85,15 @@ public class ImportFolderWizard extends Wizard implements IImportWizard, INewWiz
     }
 
     ProjectUtils.updatePerspective();
+
+    try {
+      newProject.accept(new BuildDirectoryFinder(), IResource.DEPTH_INFINITE);
+    } catch (CoreException e) {
+
+    }
+
     BasicNewResourceWizard.selectAndReveal(newProject, workbench.getActiveWorkbenchWindow());
 
     return true;
   }
-
 }
