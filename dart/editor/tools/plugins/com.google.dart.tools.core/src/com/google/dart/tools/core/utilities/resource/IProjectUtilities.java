0f6be8b54cfdebe25e431caf537877ff5c2482b7
danrubel@google.com <danrubel@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-04-06 06:18:21 +0000

    remove unused IProjectUtilities methods
    BUG=dartbug.com/18052
    R=brianwilkerson@google.com
    
    Review URL: https://codereview.chromium.org//224843008
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@34759 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/utilities/resource/IProjectUtilities.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core/src/com/google/dart/tools/core/utilities/resource/IProjectUtilities.java
@@ -13,31 +13,13 @@
  */
 package com.google.dart.tools.core.utilities.resource;
 
-import com.google.dart.tools.core.DartCore;
-import com.google.dart.tools.core.internal.model.DartProjectNature;
 import com.google.dart.tools.core.refresh.DartPackagesFolderMatcher;
 
 import org.eclipse.core.resources.FileInfoMatcherDescription;
-import org.eclipse.core.resources.ICommand;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceFilterDescription;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubProgressMonitor;
-
-import java.io.File;
-import java.net.URI;
 
 /**
  * The class <code>IProjectUtilities</code> defines utility methods used to work with projects.
@@ -45,30 +27,6 @@ import java.net.URI;
  * @coverage dart.tools.core.utilities
  */
 public final class IProjectUtilities {
-  /**
-   * Add to the given project a file that is linked to the given file.
-   * 
-   * @param project the project to which the linked file should be added
-   * @param file the file that the linked file should be linked with
-   * @param monitor the progress monitor used to provide feedback to the user, or <code>null</code>
-   *          if no feedback is desired
-   * @return the file that is linked to the given file
-   * @throws CoreException if the link could not be created for some reason
-   */
-  public static IFile addLinkToProject(IProject project, File file, IProgressMonitor monitor)
-      throws CoreException {
-    IFile newFile = computeLinkPoint(project, file.getName());
-    newFile.createLink(new Path(file.getAbsolutePath()), 0, monitor);
-    if (!newFile.exists()) {
-      throw new CoreException(new Status(
-          IStatus.ERROR,
-          DartCore.PLUGIN_ID,
-          IStatus.ERROR,
-          "Failed to create a link to " + file.getAbsolutePath() + " in " + project.getLocation(),
-          null));
-    }
-    return newFile;
-  }
 
   public static void configurePackagesFilter(IProject project) throws CoreException {
     FileInfoMatcherDescription matcher = new FileInfoMatcherDescription(
@@ -78,160 +36,6 @@ public final class IProjectUtilities {
     project.createFilter(IResourceFilterDescription.EXCLUDE_ALL
         | IResourceFilterDescription.FOLDERS | IResourceFilterDescription.FILES
         | IResourceFilterDescription.INHERITABLE, matcher, 0, new NullProgressMonitor());
-  }
-
-  /**
-   * Create or open the project in the given directory (or the directory containing the given file
-   * if the file is not a directory).
-   * 
-   * @param file the file or directory indicating which directory should be the root of the project
-   * @param monitor the progress monitor used to provide feedback to the user, or <code>null</code>
-   *          if no feedback is desired
-   * @return the resource associated with the file that was passed in
-   * @throws CoreException if the project cannot be opened or created
-   */
-  public static IResource createOrOpenProject(File file, IProgressMonitor monitor)
-      throws CoreException {
-
-    IResource existingResource = getIFileForAbsolutePath(file);
-    if (existingResource != null) {
-      return existingResource;
-    }
-
-    final File projectDirectory;
-    if (file.isDirectory()) {
-      projectDirectory = file;
-    } else {
-      projectDirectory = file.getParentFile();
-    }
-    final IWorkspace workspace = ResourcesPlugin.getWorkspace();
-    workspace.run(new IWorkspaceRunnable() {
-      @Override
-      public void run(IProgressMonitor monitor) throws CoreException {
-        String projectName = projectDirectory.getName();
-        IProject project = getProject(workspace, projectName);
-        IProjectDescription description = createProjectDescription(
-            project,
-            projectDirectory.toURI());
-
-        monitor.beginTask("Create project " + projectName, 300); //$NON-NLS-1$
-        project.create(description, new SubProgressMonitor(monitor, 100));
-        if (monitor.isCanceled()) {
-          throw new OperationCanceledException();
-        }
-        project.open(IResource.NONE, new SubProgressMonitor(monitor, 100));
-        if (monitor.isCanceled()) {
-          throw new OperationCanceledException();
-        }
-
-        DartProjectNature nature = new DartProjectNature();
-        nature.setProject(project);
-        nature.configure();
-
-        IProjectUtilities.configurePackagesFilter(project);
-
-        monitor.done();
-      }
-    },
-        monitor);
-
-    String projectName = projectDirectory.getName();
-    return getProject(workspace, projectName);
-  }
-
-  /**
-   * Return a file in the given project whose name is derived from the given base name that does not
-   * already exist.
-   * 
-   * @param project the project containing the file that is returned
-   * @param baseName the base of the name of the returned file
-   * @return a file that can be used to link to the given file
-   */
-  private static IFile computeLinkPoint(IProject project, String fileName) {
-    IFile newFile = project.getFile(fileName);
-    if (!newFile.exists() && !hasSimilarChild(project, fileName)) {
-      return newFile;
-    }
-    int dotIndex = fileName.lastIndexOf('.');
-    String extension;
-    String baseName;
-    if (dotIndex < 0) {
-      extension = "";
-      baseName = fileName;
-    } else {
-      extension = fileName.substring(dotIndex);
-      baseName = fileName.substring(0, dotIndex);
-    }
-    int index = 2;
-    while (newFile.exists() || hasSimilarChild(project, newFile.getName())) {
-      newFile = project.getFile(baseName + index++ + extension);
-    }
-    return newFile;
-  }
-
-  /**
-   * Return a project description for the
-   * 
-   * @param project
-   * @param location
-   * @return
-   */
-  private static IProjectDescription createProjectDescription(IProject project, URI location) {
-    IWorkspace workspace = ResourcesPlugin.getWorkspace();
-    IProjectDescription description = workspace.newProjectDescription(project.getName());
-    description.setLocationURI(location);
-    description.setNatureIds(new String[] {DartCore.DART_PROJECT_NATURE});
-    ICommand command = description.newCommand();
-    command.setBuilderName(DartCore.DART_BUILDER_ID);
-    description.setBuildSpec(new ICommand[] {command});
-    return description;
-  }
-
-  private static IFile getIFileForAbsolutePath(File file) {
-    return ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(
-        new Path(file.getAbsolutePath()));
-  }
-
-  /**
-   * Return the project being created, or <code>null</code> if the name is not a valid project name.
-   * 
-   * @return the project being created
-   */
-  private static IProject getProject(IWorkspace workspace, String name) {
-    if (!workspace.validateName(name, IResource.PROJECT).isOK()) {
-      return null;
-    }
-    return workspace.getRoot().getProject(name);
-  }
-
-  /**
-   * Projects do not allow a link to be created if there is another resource (linked or not) whose
-   * name is the same as the new name with only case differences. Return <code>true</code> if the
-   * project has a member whose name is the same when case is ignored.
-   * 
-   * @param project the project containing the members
-   * @param fileName the name being checked for
-   * @return <code>true</code> if the project has a member whose name is the same when case is
-   *         ignored
-   * @throws
-   */
-  private static boolean hasSimilarChild(IProject project, String fileName) {
-    try {
-      IResource[] members = project.members();
-      if (members == null) {
-        return false;
-      }
-      for (IResource member : members) {
-        if (member.getName().equalsIgnoreCase(fileName)) {
-          return true;
-        }
-      }
-    } catch (CoreException exception) {
-      DartCore.logInformation(
-          "Could not get members of project " + project.getLocation(),
-          exception);
-    }
-    return false;
   }
 
   /**
--- dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/utilities/resource/IProjectUtilitiesTest.java
+++ dart/editor/tools/plugins/com.google.dart.tools.core_test/src/com/google/dart/tools/core/utilities/resource/IProjectUtilitiesTest.java
@@ -13,151 +13,26 @@
  */
 package com.google.dart.tools.core.utilities.resource;
 
-import com.google.dart.tools.core.mock.MockFile;
-import com.google.dart.tools.core.mock.MockProject;
-import com.google.dart.tools.core.test.util.FileOperation;
-import com.google.dart.tools.core.test.util.TestUtilities;
-
 import junit.framework.TestCase;
 
-import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.FileInfoMatcherDescription;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class IProjectUtilitiesTest extends TestCase {
-  public void test_IProjectUtilities_addLinkToProject_duplicate() throws CoreException {
-    MockProject project = new MockProject("project") {
-      @Override
-      public IFile getFile(String fileName) {
-        return new MockFile(this, fileName) {
-          private boolean linkCreated = false;
 
-          @Override
-          public void createLink(IPath localLocation, int updateFlags, IProgressMonitor monitor)
-              throws CoreException {
-            linkCreated = true;
-          }
-
-          @Override
-          public boolean exists() {
-            String name = getName();
-            if (!containsDigit(name)) {
-              return true;
-            }
-            return linkCreated;
-          }
-
-          private boolean containsDigit(String string) {
-            int length = string.length();
-            for (int i = 0; i < length; i++) {
-              if (Character.isDigit(string.charAt(i))) {
-                return true;
-              }
-            }
-            return false;
-          }
-        };
-      }
-    };
-    IFile newFile = IProjectUtilities.addLinkToProject(project, new File("foo.dart"), null);
-    assertNotNull(newFile);
-    assertEquals("foo2.dart", newFile.getName());
-    assertTrue(newFile.exists());
-  }
-
-  public void test_IProjectUtilities_addLinkToProject_noDuplicate() throws CoreException {
-    MockProject project = new MockProject("project") {
-      @Override
-      public IFile getFile(String fileName) {
-        return new MockFile(this, fileName) {
-          private boolean linkCreated = false;
-
-          @Override
-          public void createLink(IPath localLocation, int updateFlags, IProgressMonitor monitor)
-              throws CoreException {
-            linkCreated = true;
-          }
-
-          @Override
-          public boolean exists() {
-            return linkCreated;
-          }
-        };
-      }
-    };
-    String fileName = "foo.dart";
-    IFile newFile = IProjectUtilities.addLinkToProject(project, new File(fileName), null);
-    assertNotNull(newFile);
-    assertEquals(fileName, newFile.getName());
-    assertTrue(newFile.exists());
-  }
-
-  public void test_IProjectUtilities_createOrOpenProject_directory() throws Exception {
-    TestUtilities.runWithTempDirectory(new FileOperation() {
-      @Override
-      public void run(File tempDirectory) throws Exception {
-        File projectDir = createProjectDir(tempDirectory);
-        createDartFile(projectDir);
-        IResource result = IProjectUtilities.createOrOpenProject(
-            projectDir,
-            new NullProgressMonitor());
-        assertTrue(result instanceof IProject);
-        IProject project = (IProject) result;
-        TestUtilities.deleteProject(project);
-      }
-    });
-  }
-
-  public void test_IProjectUtilities_createOrOpenProject_file() throws Exception {
-    TestUtilities.runWithTempDirectory(new FileOperation() {
-      @Override
-      public void run(File tempDirectory) throws Exception {
-        File projectDir = createProjectDir(tempDirectory);
-        File dartFile = createDartFile(projectDir);
-        IResource result = IProjectUtilities.createOrOpenProject(
-            dartFile,
-            new NullProgressMonitor());
-        IProject project = null;
-        if (result instanceof IProject) {
-          project = (IProject) result;
-        }
-        if (result instanceof IFile) {
-          project = ((IFile) result).getProject();
-        }
-        assert (project != null);
-        TestUtilities.deleteProject(project);
-      }
-    });
-  }
-
-  private File createDartFile(File projectDir) throws IOException {
-    File dartFile = new File(projectDir, "test.dart");
-    PrintWriter writer = null;
-    try {
-      writer = new PrintWriter(new FileWriter(dartFile));
-      writer.println("class Test {");
-      writer.println("}");
-      writer.flush();
-    } finally {
-      if (writer != null) {
-        writer.close();
-      }
-    }
-    return dartFile;
-  }
-
-  private File createProjectDir(File tempDirectory) {
-    File projectDir = new File(tempDirectory, "projectDir");
-    projectDir.mkdirs();
-    return projectDir;
+  public void test_configurePackagesFilter() throws Exception {
+    IProject project = mock(IProject.class);
+    IProjectUtilities.configurePackagesFilter(project);
+    verify(project).createFilter(
+        anyInt(),
+        any(FileInfoMatcherDescription.class),
+        anyInt(),
+        any(IProgressMonitor.class));
   }
 }
