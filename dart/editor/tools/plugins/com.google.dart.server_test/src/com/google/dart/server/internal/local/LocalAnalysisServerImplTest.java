76077d1510fde6e2e5e3109cb96aa08c4de2fa3f
scheglov@google.com <scheglov@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-04-11 21:36:48 +0000

    Stub for Analysis Server tests.
    R=brianwilkerson@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//235783005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@34998 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.server_test/src/com/google/dart/server/internal/local/LocalAnalysisServerImplTest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2014, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.dart.server.internal.local;
+
+import junit.framework.TestCase;
+
+public class LocalAnalysisServerImplTest extends TestCase {
+  private LocalAnalysisServerImpl server = new LocalAnalysisServerImpl();
+
+  public void test_getVersion() throws Exception {
+    assertEquals("0.0.1", server.version());
+  }
+}
