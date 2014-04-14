850222d76875858d470e7d6e9e8fef0164d34725
keertip@google.com <keertip@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-04-14 17:26:57 +0000

    add a RelativeFileResolver to analysis engine
    R=brianwilkerson@google.com
    
    Review URL: https://codereview.chromium.org//227333008
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@35030 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/source/RelativeFileUriResolver.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2014, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.engine.source;
+
+import java.io.File;
+import java.net.URI;
+
+/**
+ * Instances of the class {@code RelativeFileUriResolver} resolve {@code file} URI's.
+ * 
+ * @coverage dart.engine.source
+ */
+public class RelativeFileUriResolver extends UriResolver {
+
+  /**
+   * The name of the {@code file} scheme.
+   */
+  public static final String FILE_SCHEME = "file";
+
+  /**
+   * Return {@code true} if the given URI is a {@code file} URI.
+   * 
+   * @param uri the URI being tested
+   * @return {@code true} if the given URI is a {@code file} URI
+   */
+  public static boolean isFileUri(URI uri) {
+    return uri.getScheme().equals(FILE_SCHEME);
+  }
+
+  /**
+   * The directories for the relatvie URI's
+   */
+  private File[] relativeDirectories;
+
+  /**
+   * The root directory for all the source trees
+   */
+  private File rootDirectory;
+
+  /**
+   * Initialize a newly created resolver to resolve {@code file} URI's relative to the given root
+   * directory.
+   */
+  public RelativeFileUriResolver(File rootDirectory, File... relativeDirectories) {
+    super();
+    this.rootDirectory = rootDirectory;
+    this.relativeDirectories = relativeDirectories;
+  }
+
+  @Override
+  public Source fromEncoding(UriKind kind, URI uri) {
+    if (kind == UriKind.FILE_URI) {
+      return new FileBasedSource(new File(uri), kind);
+    }
+    return null;
+  }
+
+  @Override
+  public Source resolveAbsolute(URI uri) {
+    String rootPath = rootDirectory.toURI().getPath();
+    if (uri.getPath().startsWith(rootPath)) {
+      String filePath = uri.getPath().substring(rootPath.length());
+      for (File dir : relativeDirectories) {
+        File file = new File(dir, filePath);
+        if (file.exists()) {
+          return new FileBasedSource(file, UriKind.FILE_URI);
+        }
+      }
+    }
+    return null;
+  }
+
+}
---
+++ dart/editor/tools/plugins/com.google.dart.engine_test/src/com/google/dart/engine/source/RelativeFileResolverTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2014, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.engine.source;
+
+import com.google.dart.engine.utilities.io.FileUtilities2;
+
+import static com.google.dart.engine.utilities.io.FileUtilities2.createFile;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.net.URI;
+
+public class RelativeFileResolverTest extends TestCase {
+
+  public void test_creation() {
+    File root = createFile("/does/not/exist");
+    File directory = createFile("/does/not/exist/relative");
+    assertNotNull(new RelativeFileUriResolver(root, directory));
+  }
+
+  public void test_fromEncoding_file() throws Exception {
+    File root = createFile("/does/not/exist");
+    File directory = createFile("/does/not/exist/relative");
+    UriResolver resolver = new RelativeFileUriResolver(root, directory);
+    Source result = resolver.fromEncoding(UriKind.FILE_URI, new URI("file:/does/not/exist.dart"));
+    assertNotNull(result);
+    assertEquals(createFile("/does/not/exist.dart").getAbsolutePath(), result.getFullName());
+  }
+
+  public void test_fromEncoding_nonFile() throws Exception {
+    File root = createFile("/does/not/exist");
+    File directory = createFile("/does/not/exist/relative");
+    UriResolver resolver = new RelativeFileUriResolver(root, directory);
+    Source result = resolver.fromEncoding(UriKind.PACKAGE_URI, new URI("file:/does/not/exist.dart"));
+    assertNull(result);
+  }
+
+  public void test_resolve_file() throws Exception {
+    File root = FileUtilities2.createTempDir("/does/not/exist");
+    File directory = FileUtilities2.createTempDir("/does/not/exist/relative");
+
+    File testFile = new File(directory, "exist.dart");
+    testFile.createNewFile();
+    URI uri = new URI("file", null, root.getPath() + "/exist.dart", null, null);
+    UriResolver resolver = new RelativeFileUriResolver(root, directory);
+    Source result = resolver.resolveAbsolute(uri);
+    assertNotNull(result);
+    assertEquals(testFile.getAbsolutePath(), result.getFullName());
+  }
+
+  public void test_resolve_nonFile() throws Exception {
+    File root = createFile("/does/not/exist");
+    File directory = createFile("/does/not/exist/relative");
+    UriResolver resolver = new RelativeFileUriResolver(root, directory);
+    Source result = resolver.resolveAbsolute(new URI("dart:core"));
+    assertNull(result);
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    FileUtilities2.deleteTempDir();
+  }
+}
