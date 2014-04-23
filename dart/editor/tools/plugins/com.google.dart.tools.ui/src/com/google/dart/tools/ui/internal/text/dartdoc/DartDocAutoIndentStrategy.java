56ba760ef00461b0f12f8303bcd02fa895d9a41a
scheglov@google.com <scheglov@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-04-23 21:43:46 +0000

    Issue 10740. Fix for new-line command in DartDoc.
    R=messick@google.com
    BUG= https://code.google.com/p/dart/issues/detail?id=10740
    
    Review URL: https://codereview.chromium.org//250173002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@35334 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/dartdoc/DartDocAutoIndentStrategy.java
+++ dart/editor/tools/plugins/com.google.dart.tools.ui/src/com/google/dart/tools/ui/internal/text/dartdoc/DartDocAutoIndentStrategy.java
@@ -14,23 +14,21 @@
 
 package com.google.dart.tools.ui.internal.text.dartdoc;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DefaultIndentLineAutoEditStrategy;
 import org.eclipse.jface.text.DocumentCommand;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.ITypedRegion;
-import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.TextUtilities;
 
 /**
  * A simple auto indent strategy for Dartdoc comments (as well as regular multi-line comments).
+ * 
+ * @coverage dart.editor.ui.text.dart
  */
 public class DartDocAutoIndentStrategy extends DefaultIndentLineAutoEditStrategy {
-  private String partitioning;
-
   public DartDocAutoIndentStrategy(String partitioning) {
-    this.partitioning = partitioning;
   }
 
   @Override
@@ -60,53 +58,32 @@ public class DartDocAutoIndentStrategy extends DefaultIndentLineAutoEditStrategy
       int p = (offset == d.getLength() ? offset - 1 : offset);
       IRegion info = d.getLineInformationOfOffset(p);
       int start = info.getOffset();
+      int end = start + info.getLength();
 
-      // find white spaces
-      int end = findEndOfWhiteSpace(d, start, offset);
+      // split line
+      String strLine = d.get(start, end - start);
+      int firstNotWS = StringUtils.indexOfAnyBut(strLine, " \t");
+      String strWS = strLine.substring(0, firstNotWS);
+      String strAfterWS = strLine.substring(firstNotWS);
 
-      String startStr = getLineStart(d, start, offset, true);
+      String lineDelimiter = TextUtilities.getDefaultLineDelimiter(d);
+      StringBuffer buf = new StringBuffer();
 
-      StringBuffer buf = new StringBuffer(c.text);
-
-      if (end > start) {
-        // append to input
-        buf.append(d.get(start, end - start));
+      buf.append(lineDelimiter);
+      buf.append(strWS);
+      if (strAfterWS.startsWith("/")) {
+        buf.append(" ");
       }
-
-      if (startStr.endsWith("*")) {
-        // look for post '*' whitespace and preserve it (for 4-space code indents)
-        String postWs = getLineStart(d, end + 1, offset, false);
-
-        if (postWs.length() > 0) {
-          buf.append("*");
-          buf.append(postWs);
-        } else {
-          // append the Dartdoc continuation chars
-          buf.append("* ");
-        }
-      } else if (startStr.endsWith("/")) {
-        // indent one more then the /** indent
-        buf.append(" * ");
-        if (isNewComment(d, offset)) {
-          c.shiftsCaret = false;
-          c.caretOffset = offset + buf.length();
-          String lineDelimiter = TextUtilities.getDefaultLineDelimiter(d);
-
-          int eolOffset = start + info.getLength();
-          int replacementLength = eolOffset - p;
-          String restOfLine = d.get(p, replacementLength);
-          IRegion prefix = findPrefixRange(d, info);
-          String indentation = d.get(prefix.getOffset(), prefix.getLength());
-          String endTag = lineDelimiter + indentation + " */"; //$NON-NLS-1$
-
-          c.length = replacementLength;
-          buf.append(restOfLine);
-          buf.append(endTag);
-        }
+      buf.append("* ");
+      int newCaretOffset = offset + buf.length();
+      if (strLine.endsWith("*/")) {
+        buf.append(lineDelimiter);
+        buf.append(strWS);
+        buf.append(" ");
       }
-
+      c.shiftsCaret = false;
+      c.caretOffset = newCaretOffset;
       c.text = buf.toString();
-
     } catch (BadLocationException excp) {
       // stop work
     }
@@ -131,72 +108,5 @@ public class DartDocAutoIndentStrategy extends DefaultIndentLineAutoEditStrategy
     }
 
     return document.get(start, end - start);
-  }
-
-  /**
-   * Returns the range of the Javadoc prefix on the given line in <code>document</code>. The prefix
-   * greedily matches the following regex pattern: <code>\w*\*\w*</code>, that is, any number of
-   * whitespace characters, followed by an asterisk ('*'), followed by any number of whitespace
-   * characters.
-   * 
-   * @param document the document to which <code>line</code> refers
-   * @param line the line from which to extract the prefix range
-   * @return an <code>IRegion</code> describing the range of the prefix on the given line
-   * @throws BadLocationException if accessing the document fails
-   */
-  private IRegion findPrefixRange(IDocument document, IRegion line) throws BadLocationException {
-    int lineOffset = line.getOffset();
-    int lineEnd = lineOffset + line.getLength();
-    int indentEnd = findEndOfWhiteSpace(document, lineOffset, lineEnd);
-    if (indentEnd < lineEnd && document.getChar(indentEnd) == '*') {
-      indentEnd++;
-      while (indentEnd < lineEnd && document.getChar(indentEnd) == ' ') {
-        indentEnd++;
-      }
-    }
-    return new Region(lineOffset, indentEnd - lineOffset);
-  }
-
-  /**
-   * Guesses if the command operates within a newly created Javadoc comment or not. If in doubt, it
-   * will assume that the Javadoc is new.
-   * 
-   * @param document the document
-   * @param commandOffset the command offset
-   * @return <code>true</code> if the comment should be closed, <code>false</code> if not
-   */
-  private boolean isNewComment(IDocument document, int commandOffset) {
-
-    try {
-      int lineIndex = document.getLineOfOffset(commandOffset) + 1;
-      if (lineIndex >= document.getNumberOfLines()) {
-        return true;
-      }
-
-      IRegion line = document.getLineInformation(lineIndex);
-      ITypedRegion partition = TextUtilities.getPartition(
-          document,
-          partitioning,
-          commandOffset,
-          false);
-      int partitionEnd = partition.getOffset() + partition.getLength();
-      if (line.getOffset() >= partitionEnd) {
-        return false;
-      }
-
-      if (document.getLength() == partitionEnd) {
-        return true; // partition goes to end of document - probably a new comment
-      }
-
-      String comment = document.get(partition.getOffset(), partition.getLength());
-      if (comment.indexOf("/*", 2) != -1) {
-        return true; // enclosed another comment -> probably a new comment
-      }
-
-      return false;
-
-    } catch (BadLocationException e) {
-      return false;
-    }
   }
 }
---
+++ dart/editor/tools/plugins/com.google.dart.tools.ui_test/src/com/google/dart/tools/ui/internal/text/dart/DartDocAutoIndentStrategyTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2014, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.tools.ui.internal.text.dart;
+
+import com.google.dart.engine.EngineTestCase;
+import com.google.dart.tools.internal.corext.refactoring.util.ReflectionUtils;
+import com.google.dart.tools.ui.internal.text.dartdoc.DartDocAutoIndentStrategy;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.DocumentCommand;
+import org.eclipse.jface.text.IDocument;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class DartDocAutoIndentStrategyTest extends EngineTestCase {
+  private static final String EOL = System.getProperty("line.separator", "\n");
+
+  private static void assert_customizeDocumentCommand(String initial, String newText,
+      String expected) {
+    int initialOffset = initial.indexOf('!');
+    int expectedOffset = expected.indexOf('!');
+    assertTrue("No cursor position in initial: " + initial, initialOffset != -1);
+    assertTrue("No cursor position in expected: " + expected, expectedOffset != -1);
+    initial = StringUtils.remove(initial, '!');
+    expected = StringUtils.remove(expected, '!');
+    // force "smart mode"
+    DartDocAutoIndentStrategy strategy = new DartDocAutoIndentStrategy(null);
+    // prepare document
+    IDocument document = new Document(initial);
+    // handle command
+    DocumentCommand command = new DocumentCommand() {
+    };
+    command.caretOffset = -1;
+    command.doit = true;
+    command.offset = initialOffset;
+    command.text = newText;
+    strategy.customizeDocumentCommand(document, command);
+    // update document
+    ReflectionUtils.invokeMethod(command, "execute(org.eclipse.jface.text.IDocument)", document);
+    // check new content
+    String actual = document.get();
+    assertEquals(expected, actual);
+    // check caret offset
+    int actualOffset = command.caretOffset;
+    if (actualOffset == -1) {
+      actualOffset = initialOffset + command.text.length();
+    }
+    assertThat(actualOffset).isEqualTo(expectedOffset);
+  }
+
+  private static void assertSmartInsertAfterNewLine(String initial, String expected) {
+    assert_customizeDocumentCommand(initial, EOL, expected);
+  }
+
+  public void test_afterStart_beforeEnd_hasEOL_beforeEOF() throws Exception {
+    assertSmartInsertAfterNewLine(createSource(//
+        "/**!*/",
+        ""), createSource(//
+        "/**",
+        " * !",
+        " */",
+        ""));
+  }
+
+  public void test_afterStart_beforeEnd_noEOL_beforeEOF() throws Exception {
+    assertSmartInsertAfterNewLine(createSource(//
+        "/**!*/"),
+        createSource(//
+            "/**",
+            " * !",
+            " */"));
+  }
+
+  public void test_inMiddleLine() throws Exception {
+    assertSmartInsertAfterNewLine(createSource(//
+        "/**",
+        " * ab!cd",
+        " */",
+        ""), createSource(//
+        "/**",
+        " * ab",
+        " * !cd",
+        " */",
+        ""));
+  }
+}
