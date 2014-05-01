8eedd25b765d55be0b3c70feb6c3365c8e38db95
scheglov@google.com <scheglov@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-05-01 04:20:38 +0000

    Fix Editor build.
    TBR
    
    R=brianwilkerson@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//264713006
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@35611 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/internal/index/operation/OperationProcessor.java
+++ dart/editor/tools/plugins/com.google.dart.engine/src/com/google/dart/engine/internal/index/operation/OperationProcessor.java
@@ -90,9 +90,6 @@ public class OperationProcessor {
   public void run() {
     synchronized (this) {
       // This processor is, or was, already running on a different thread.
-      if (state == ProcessorState.STOPPED) {
-        return;
-      }
       if (state != ProcessorState.READY) {
         throw new IllegalStateException("Operation processors can only be run one time: " + state); //$NON-NLS-1$
       }
