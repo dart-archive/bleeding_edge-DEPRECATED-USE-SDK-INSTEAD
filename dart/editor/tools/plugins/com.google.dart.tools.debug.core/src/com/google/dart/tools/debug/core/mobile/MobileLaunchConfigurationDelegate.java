4c0815326766df775087dac34a31703ec281bac4
danrubel@google.com <danrubel@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-05-29 22:13:30 +0000

    detect missing local ip address during mobile launch
    BUG=
    R=keertip@google.com
    
    Review URL: https://codereview.chromium.org//301193003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@36805 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.tools.debug.core/src/com/google/dart/tools/debug/core/mobile/MobileLaunchConfigurationDelegate.java
+++ dart/editor/tools/plugins/com.google.dart.tools.debug.core/src/com/google/dart/tools/debug/core/mobile/MobileLaunchConfigurationDelegate.java
@@ -64,15 +64,17 @@ public class MobileLaunchConfigurationDelegate extends DartLaunchConfigurationDe
       // TODO(keertip): change to localhost and pubserve once port forwarding is setup right
       try {
         server = ResourceServerManager.getServer();
+        String resPath = resource.getFullPath().toPortableString();
+        String localAddress = server.getLocalAddress();
+        if (localAddress == null) {
+          // TODO (danrubel) Improve UX to help user work through this issue
+          throw new CoreException(new Status(
+              IStatus.ERROR,
+              DartDebugCorePlugin.PLUGIN_ID,
+              "Unable to get local IP address"));
+        }
 
-        URI uri = new URI(
-            "http",
-            null,
-            server.getLocalAddress(),
-            server.getPort(),
-            resource.getFullPath().toPortableString(),
-            null,
-            null);
+        URI uri = new URI("http", null, localAddress, server.getPort(), resPath, null, null);
 
         launchUrl = uri.toString();
         // launchUrl = server.getUrlForFile((File) resource);
@@ -82,7 +84,7 @@ public class MobileLaunchConfigurationDelegate extends DartLaunchConfigurationDe
         throw new CoreException(new Status(
             IStatus.ERROR,
             DartDebugCorePlugin.PLUGIN_ID,
-            "Unable to launch on device",
+            "Unable to launch on device: " + e.getLocalizedMessage(),
             e));
       }
     } else {
