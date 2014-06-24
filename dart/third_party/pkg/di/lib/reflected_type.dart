8f123ac326014752a13d827c8aa7a084d06cd8d6
herhut@google.com <herhut@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-06-24 09:09:04 +0000

    Remove reflectedType hack while waiting for update of third party DI package.
    BUG=
    R=floitsch@google.com
    
    Review URL: https://codereview.chromium.org//357433002
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@37645 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/third_party/pkg/di/lib/reflected_type.dart
+++ dart/third_party/pkg/di/lib/reflected_type.dart
@@ -1,33 +1,12 @@
 library di.src.reflected_type;
 
-// These are used by _getReflectedTypeWorkaround, see http://dartbug.com/12607
-@MirrorsUsed(targets:
-    const ['_js_helper.createRuntimeType', 'dart._js_mirrors.JsClassMirror'],
-    override: 'di.src.reflected_type')
-import 'dart:mirrors';
-
 Map<ClassMirror, Type> _cache = <ClassMirror, Type>{};
 
-// Horrible hack to work around: http://dartbug.com/12607
+// Removed the horrible hack to work around bug 
+// http://dartbug.com/12607 as it is no longer needed, nor does
+// it work with the new MirrorsUsed tracking that disallows 
+// reflection on internal libraries. This will go away with an
+// update of DI.
 Type getReflectedTypeWorkaround(ClassMirror cls) {
-  // On Dart VM, just return reflectedType.
-  if (1.0 is! int) return cls.reflectedType;
-  if (!cls.isOriginalDeclaration) {
-    cls = cls.originalDeclaration;
-  }
-  if (_cache[cls] == null) {
-    var mangledName = reflect(cls).getField(_mangledNameField).reflectee;
-    _cache[cls] = _jsHelper.invoke(#createRuntimeType, [mangledName]).reflectee;
-  }
-  return _cache[cls];
+  return cls.reflectedType;
 }
-
-final LibraryMirror _jsHelper =
-    currentMirrorSystem().libraries[Uri.parse('dart:_js_helper')];
-
-final Symbol _mangledNameField = () {
-  var jsClassMirrorMirror = reflect(reflectClass(ClassMirror)).type;
-  for (var name in jsClassMirrorMirror.declarations.keys) {
-    if (MirrorSystem.getName(name) == '_mangledName') return name;
-  }
-}();
