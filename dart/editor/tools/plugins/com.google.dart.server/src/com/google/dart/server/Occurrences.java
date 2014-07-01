7256ea45c4ae25a6c10949b67bd188bf653344ca
jwren@google.com <jwren@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-07-01 22:04:26 +0000

    CL containing missing files for my last CL, this will fix the build
    Review URL: https://codereview.chromium.org//354133005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@37914 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.server/src/com/google/dart/server/Occurrences.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.server;
+
+/**
+ * The interface {@code Occurrences} defines the behavior of objects that represent a set of
+ * occurrences in a file.
+ * 
+ * @coverage dart.server
+ */
+public interface Occurrences {
+  /**
+   * An empty array of occurrences.
+   */
+  Occurrences[] EMPTY_ARRAY = new Occurrences[0];
+
+  /**
+   * A description of the element represented by this node.
+   * 
+   * @return the {@link Element}
+   */
+  public Element getElement();
+
+  /**
+   * Return the length of the referenced element.
+   * 
+   * @return the length of the referenced element
+   */
+  public int getLength();
+
+  /**
+   * Return the offsets of the name of the referenced element within the file.
+   * 
+   * @return the offsets of the name of the referenced element within the file
+   */
+  public int[] getOffsets();
+
+}
---
+++ dart/editor/tools/plugins/com.google.dart.server/src/com/google/dart/server/internal/shared/OccurrencesImpl.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2014, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.server.internal.shared;
+
+import com.google.dart.engine.utilities.general.ObjectUtilities;
+import com.google.dart.server.Element;
+import com.google.dart.server.Occurrences;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+
+/**
+ * A concrete implementation of {@link Occurrences}.
+ * 
+ * @coverage dart.server.shared
+ */
+public class OccurrencesImpl implements Occurrences {
+
+  private final Element element;
+  private final int length;
+  private final int[] offsets;
+
+  public OccurrencesImpl(Element element, int length, int[] offsets) {
+    this.element = element;
+    this.length = length;
+    this.offsets = offsets;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof OccurrencesImpl) {
+      OccurrencesImpl other = (OccurrencesImpl) obj;
+      return ObjectUtilities.equals(other.element, element) && other.length == length
+          && Arrays.equals(other.offsets, offsets);
+    }
+    return false;
+  }
+
+  @Override
+  public Element getElement() {
+    return element;
+  }
+
+  @Override
+  public int getLength() {
+    return length;
+  }
+
+  @Override
+  public int[] getOffsets() {
+    return offsets;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("[element=");
+    builder.append(element.toString());
+    builder.append(", length=");
+    builder.append(length);
+    builder.append(", offsets=");
+    builder.append(StringUtils.join(offsets, ", "));
+    builder.append("]");
+    return builder.toString();
+  }
+
+}
