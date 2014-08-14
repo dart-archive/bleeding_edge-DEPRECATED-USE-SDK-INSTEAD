faac03e29c4d863894992e5b224e2560ccda5a42
jwren@google.com <jwren@google.com@260f80e4-7a28-3924-810f-c04153c831b5>
2014-08-14 05:22:55 +0000

    Initial cut at having the enum objects from the spec generated as classes with public static strings. Type/enum aren't being generated yet.
    R=paulberry@google.com, scheglov@google.com
    
    Review URL: https://codereview.chromium.org//462253005
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@39240 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.server/src/com/google/dart/server/generated/types/CompletionSuggestionKind.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2014, the Dart project authors.
+ *
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * This file has been automatically generated.  Please do not edit it manually.
+ * To regenerate the file, use the script "pkg/analysis_server/spec/generate_files".
+ */
+package com.google.dart.server.generated.types;
+
+/**
+ * An enumeration of the kinds of elements that can be included in a completion suggestion.
+ *
+ * @coverage dart.server.generated.types
+ */
+public class CompletionSuggestionKind {
+
+  public static final String ARGUMENT_LIST = "ARGUMENT_LIST";
+
+  public static final String CLASS = "CLASS";
+
+  public static final String CLASS_ALIAS = "CLASS_ALIAS";
+
+  public static final String CONSTRUCTOR = "CONSTRUCTOR";
+
+  public static final String FIELD = "FIELD";
+
+  public static final String FUNCTION = "FUNCTION";
+
+  public static final String FUNCTION_TYPE_ALIAS = "FUNCTION_TYPE_ALIAS";
+
+  public static final String GETTER = "GETTER";
+
+  public static final String IMPORT = "IMPORT";
+
+  public static final String LIBRARY_PREFIX = "LIBRARY_PREFIX";
+
+  public static final String LOCAL_VARIABLE = "LOCAL_VARIABLE";
+
+  public static final String METHOD = "METHOD";
+
+  public static final String METHOD_NAME = "METHOD_NAME";
+
+  public static final String NAMED_ARGUMENT = "NAMED_ARGUMENT";
+
+  public static final String OPTIONAL_ARGUMENT = "OPTIONAL_ARGUMENT";
+
+  public static final String PARAMETER = "PARAMETER";
+
+  public static final String SETTER = "SETTER";
+
+  public static final String TOP_LEVEL_VARIABLE = "TOP_LEVEL_VARIABLE";
+
+  public static final String TYPE_PARAMETER = "TYPE_PARAMETER";
+}
---
+++ dart/editor/tools/plugins/com.google.dart.server/src/com/google/dart/server/generated/types/SearchResultKind.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2014, the Dart project authors.
+ *
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * This file has been automatically generated.  Please do not edit it manually.
+ * To regenerate the file, use the script "pkg/analysis_server/spec/generate_files".
+ */
+package com.google.dart.server.generated.types;
+
+/**
+ * An enumeration of the kinds of search results returned by the search domain.
+ *
+ * @coverage dart.server.generated.types
+ */
+public class SearchResultKind {
+
+  public static final String DECLARATION = "DECLARATION";
+
+  public static final String INVOCATION = "INVOCATION";
+
+  public static final String READ = "READ";
+
+  public static final String READ_WRITE = "READ_WRITE";
+
+  public static final String REFERENCE = "REFERENCE";
+
+  public static final String WRITE = "WRITE";
+}
