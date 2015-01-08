28c400cb46002517ba5d3f6c60c13e8d5b1a8729
scheglov@google.com <scheglov@google.com>
2015-01-08 19:37:19 +0000

    Remove remaining references to Angular and Polymer from the analysis server.
    R=brianwilkerson@google.com
    BUG=
    
    Review URL: https://codereview.chromium.org//842753004
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@42713 260f80e4-7a28-3924-810f-c04153c831b5

--- dart/editor/tools/plugins/com.google.dart.server/src/com/google/dart/server/generated/types/AnalysisErrorType.java
+++ dart/editor/tools/plugins/com.google.dart.server/src/com/google/dart/server/generated/types/AnalysisErrorType.java
@@ -23,15 +23,13 @@ package com.google.dart.server.generated.types;
  */
 public class AnalysisErrorType {
 
-  public static final String ANGULAR = "ANGULAR";
-
   public static final String CHECKED_MODE_COMPILE_TIME_ERROR = "CHECKED_MODE_COMPILE_TIME_ERROR";
 
   public static final String COMPILE_TIME_ERROR = "COMPILE_TIME_ERROR";
 
   public static final String HINT = "HINT";
 
-  public static final String POLYMER = "POLYMER";
+  public static final String LINT = "LINT";
 
   public static final String STATIC_TYPE_WARNING = "STATIC_TYPE_WARNING";
 
