dart-archive/bleeding_edge-DEPRECATED-USE-SDK-INSTEAD

commit cdfb883ffc4aaec73f4cc69e27c5f84d7bb66a03

Wrap standalone invariant() calls in assert().
To avoid unnecessary allocation: some of them have closure as an
argument.

R=floitsch@google.com, johnniwinther@google.com
BUG=

Review URL: https://codereview.chromium.org//819183005

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@42878 260f80e4-7a28-3924-810f-c04153c831b5

dart/pkg/compiler/lib/src/dart_types.dart
@@ -1479,7 +1479,8 @@ class Types implements DartTypes {
         return intersection.first;
       }
     }
-    invariant(CURRENT_ELEMENT_SPANNABLE, false,
+
+    compiler.internalError(CURRENT_ELEMENT_SPANNABLE,
         message: 'No least upper bound computed for $a and $b.');
     return null;
   }

dart/pkg/compiler/lib/src/resolution/members.dart
@@ -2054,7 +2054,9 @@ abstract class MappingVisitor<T> extends CommonResolverVisitor<T> {
 
   /// Register [node] as the definition of [element].
   void defineLocalVariable(Node node, LocalVariableElement element) {
-    invariant(node, element != null);
+    if (element == null) {
+      throw compiler.internalError(node, 'element is null');
+    }
     checkLocalDefinitionName(node, element);
     registry.defineElement(node, element);
   }
@@ -4177,9 +4179,13 @@ class ClassResolverVisitor extends TypeDefinitionVisitor {
     : super(compiler, classElement, registry);
 
   DartType visitClassNode(ClassNode node) {
-    invariant(node, element != null);
-    invariant(element, element.resolutionState == STATE_STARTED,
-        message: () => 'cyclic resolution of class $element');
+    if (element == null) {
+      throw compiler.internalError(node, 'element is null');
+    }
+    if (element.resolutionState != STATE_STARTED) {
+      throw compiler.internalError(element,
+          'cyclic resolution of class $element');
+    }
 
     InterfaceType type = element.computeType(compiler);
     scope = new TypeDeclarationScope(scope, element);
@@ -4264,9 +4270,13 @@ class ClassResolverVisitor extends TypeDefinitionVisitor {
       compiler.reportError(node, MessageKind.EXPERIMENTAL_ENUMS);
     }
 
-    invariant(node, element != null);
-    invariant(element, element.resolutionState == STATE_STARTED,
-        message: () => 'cyclic resolution of class $element');
+    if (element == null) {
+      throw compiler.internalError(node, 'element is null');
+    }
+    if (element.resolutionState != STATE_STARTED) {
+      throw compiler.internalError(element,
+          'cyclic resolution of class $element');
+    }
 
     InterfaceType enumType = element.computeType(compiler);
     element.supertype = compiler.objectClass.computeType(compiler);
@@ -4304,9 +4314,13 @@ class ClassResolverVisitor extends TypeDefinitionVisitor {
   }
 
   DartType visitNamedMixinApplication(NamedMixinApplication node) {
-    invariant(node, element != null);
-    invariant(element, element.resolutionState == STATE_STARTED,
-        message: () => 'cyclic resolution of class $element');
+    if (element == null) {
+      throw compiler.internalError(node, 'element is null');
+    }
+    if (element.resolutionState != STATE_STARTED) {
+      throw compiler.internalError(element,
+          'cyclic resolution of class $element');
+    }
 
     if (identical(node.classKeyword.stringValue, 'typedef')) {
       // TODO(aprelev@gmail.com): Remove this deprecation diagnostic

commit 35ad9cd66ebe7f8ea4104a829631e8e38ca7d05c

Fix build broken by r42878.
Remove parameter name left at internalError invocation.

TBR=johnniwinther@google.com
BUG=

Review URL: https://codereview.chromium.org//852833002

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@42881 260f80e4-7a28-3924-810f-c04153c831b5

dart/pkg/compiler/lib/src/dart_types.dart
@@ -1481,7 +1481,7 @@ class Types implements DartTypes {
     }
 
     compiler.internalError(CURRENT_ELEMENT_SPANNABLE,
-        message: 'No least upper bound computed for $a and $b.');
+        'No least upper bound computed for $a and $b.');
     return null;
   }
 