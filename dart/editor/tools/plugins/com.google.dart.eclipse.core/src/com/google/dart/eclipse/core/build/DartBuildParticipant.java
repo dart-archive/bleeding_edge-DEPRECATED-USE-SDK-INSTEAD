89baa1659603d053632e9c0a4cce1a65ea523b0a
keertip@google.com <keertip@google.com>
2015-02-03 17:13:01 +0000

    add a builder to renalyze sources in the plugins
    R=scheglov@google.com
    
    Review URL: https://codereview.chromium.org//898763003
    
    git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@43409 260f80e4-7a28-3924-810f-c04153c831b5

---
+++ dart/editor/tools/plugins/com.google.dart.eclipse.core/src/com/google/dart/eclipse/core/build/DartBuildParticipant.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.eclipse.core.build;
+
+import com.google.dart.tools.core.DartCore;
+import com.google.dart.tools.core.DartCoreDebug;
+import com.google.dart.tools.core.builder.BuildEvent;
+import com.google.dart.tools.core.builder.BuildParticipant;
+import com.google.dart.tools.core.builder.CleanEvent;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+/**
+ * Performs a reanalyze sources for clean build
+ */
+public class DartBuildParticipant implements BuildParticipant {
+
+  @Override
+  public void build(BuildEvent event, IProgressMonitor monitor) throws CoreException {
+    // no op
+  }
+
+  @Override
+  public void clean(CleanEvent event, IProgressMonitor monitor) throws CoreException {
+    if (DartCoreDebug.ENABLE_ANALYSIS_SERVER) {
+      DartCore.getAnalysisServer().analysis_reanalyze();
+    }
+  }
+}
