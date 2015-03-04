dart-archive/bleeding_edge-DEPRECATED-USE-SDK-INSTEAD

commit a5366174d2a1119794a16a75902fe3f8ab43d974

Track untagged values pushed to the expression stack in unoptimized code.
Verify that they are all consumed before we hit any call.

Usage of untagged values should be limited to irregexp backend which utilizes LoadUntagged instruction.

R=fschneider@google.com

Review URL: https://codereview.chromium.org//970233002

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@44225 260f80e4-7a28-3924-810f-c04153c831b5

dart/runtime/vm/flow_graph_compiler.cc
@@ -389,6 +389,12 @@ void FlowGraphCompiler::VisitBlocks() {
       continue;
     }
 
+#if defined(DEBUG)
+    if (!is_optimizing()) {
+      FrameStateClear();
+    }
+#endif
+
     LoopInfoComment(assembler(), *entry, *loop_headers);
 
     entry->set_offset(assembler()->CodeSize());
@@ -426,7 +432,17 @@ void FlowGraphCompiler::VisitBlocks() {
         pending_deoptimization_env_ = NULL;
         EmitInstructionEpilogue(instr);
       }
+
+#if defined(DEBUG)
+      if (!is_optimizing()) {
+        FrameStateUpdateWith(instr);
+      }
+#endif
     }
+
+#if defined(DEBUG)
+    ASSERT(is_optimizing() || FrameStateIsSafeToCall());
+#endif
   }
 
   if (inline_id_to_function_.length() > max_inlining_id + 1) {
@@ -1596,6 +1612,79 @@ RawArray* FlowGraphCompiler::InliningIdToFunction() const {
   }
   return res.raw();
 }
+
+
+#if defined(DEBUG)
+void FlowGraphCompiler::FrameStateUpdateWith(Instruction* instr) {
+  ASSERT(!is_optimizing());
+
+  switch (instr->tag()) {
+    case Instruction::kPushArgument:
+    case Instruction::kPushTemp:
+      // Do nothing.
+      break;
+
+    case Instruction::kDropTemps:
+      FrameStatePop(instr->locs()->input_count() +
+          instr->AsDropTemps()->num_temps());
+      break;
+
+    default:
+      FrameStatePop(instr->locs()->input_count());
+      break;
+  }
+
+  ASSERT(!instr->locs()->can_call() || FrameStateIsSafeToCall());
+
+  FrameStatePop(instr->ArgumentCount());
+  Definition* defn = instr->AsDefinition();
+  if ((defn != NULL) && defn->HasTemp()) {
+    FrameStatePush(defn);
+  }
+}
+
+
+void FlowGraphCompiler::FrameStatePush(Definition* defn) {
+  Representation rep = defn->representation();
+  if ((rep == kUnboxedDouble) ||
+      (rep == kUnboxedFloat64x2) ||
+      (rep == kUnboxedFloat32x4)) {
+    // LoadField instruction lies about its representation in the unoptimized
+    // code because Definition::representation() can't depend on the type of
+    // compilation but MakeLocationSummary and EmitNativeCode can.
+    ASSERT(defn->IsLoadField() && defn->AsLoadField()->IsUnboxedLoad());
+    ASSERT(defn->locs()->out(0).IsRegister());
+    rep = kTagged;
+  }
+  ASSERT(!is_optimizing());
+  ASSERT((rep == kTagged) || (rep == kUntagged));
+  ASSERT(rep != kUntagged || flow_graph_.IsIrregexpFunction());
+  frame_state_.Add(rep);
+}
+
+
+void FlowGraphCompiler::FrameStatePop(intptr_t count) {
+  ASSERT(!is_optimizing());
+  frame_state_.TruncateTo(Utils::Maximum(0, frame_state_.length() - count));
+}
+
+
+bool FlowGraphCompiler::FrameStateIsSafeToCall() {
+  ASSERT(!is_optimizing());
+  for (intptr_t i = 0; i < frame_state_.length(); i++) {
+    if (frame_state_[i] != kTagged) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+void FlowGraphCompiler::FrameStateClear() {
+  ASSERT(!is_optimizing());
+  frame_state_.TruncateTo(0);
+}
+#endif
 
 
 }  // namespace dart

dart/runtime/vm/flow_graph_compiler.h
@@ -644,11 +644,23 @@ class FlowGraphCompiler : public ValueObject {
     return stackmap_table_builder_;
   }
 
+#if defined(DEBUG)
+  void FrameStateUpdateWith(Instruction* instr);
+  void FrameStatePush(Definition* defn);
+  void FrameStatePop(intptr_t count);
+  bool FrameStateIsSafeToCall();
+  void FrameStateClear();
+#endif
+
   Isolate* isolate_;
   Assembler* assembler_;
   const ParsedFunction& parsed_function_;
   const FlowGraph& flow_graph_;
   const GrowableArray<BlockEntryInstr*>& block_order_;
+
+#if defined(DEBUG)
+  GrowableArray<Representation> frame_state_;
+#endif
 
   // Compiler specific per-block state.  Indexed by postorder block number
   // for convenience.  This is not the block's index in the block order,

commit 11ba674f37ce1e07e561a52d51d89367fa42ba20

Fix x64 build broken by r44225.
TBR=fschneider@google.com

Review URL: https://codereview.chromium.org//981533002

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@44226 260f80e4-7a28-3924-810f-c04153c831b5

dart/runtime/vm/flow_graph_compiler.cc
@@ -1665,7 +1665,8 @@ void FlowGraphCompiler::FrameStatePush(Definition* defn) {
 
 void FlowGraphCompiler::FrameStatePop(intptr_t count) {
   ASSERT(!is_optimizing());
-  frame_state_.TruncateTo(Utils::Maximum(0, frame_state_.length() - count));
+  frame_state_.TruncateTo(Utils::Maximum(static_cast<intptr_t>(0),
+      frame_state_.length() - count));
 }
 
 