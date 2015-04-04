dart-archive/bleeding_edge-DEPRECATED-USE-SDK-INSTEAD

commit 519829256e30d7230d782a28d04588b2fef353f3

Fix interpretation of string contents after interpolation expressions.
Previously, the logic for skipping the opening quote(s) (and optional
'r') of a string were erroneously being invoked after string
interpolations.  As a result, a string like '${x}"y' was being
interpreted as '${x}y'.  Usually, this had no visible user effect,
because analyzer's interpretation of strings is currently only used
for constant evaluation, and string interpolation is rare in constant
expressions.  However, it was triggering an internal error in the case
where the interpolation is at the end of a multiline string, since it
caused analyzer to compute that the number of characters following the
interpolation was negative.

In addition to fixing the bug, I've modified the logic for logging the
internal error so that it also asserts; this allows the error to be
detected by unit tests.  I've also fixed an off-by-one error in the
internal error logic which had prevented the bug from being noticed on
single-line strings.

BUG=dartbug.com/23046
R=brianwilkerson@google.com

Review URL: https://codereview.chromium.org//1056103002

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@44880 260f80e4-7a28-3924-810f-c04153c831b5

dart/pkg/analyzer/lib/src/generated/ast.dart
@@ -16703,28 +16703,30 @@ class StringLexemeHelper {
   int end;
 
   StringLexemeHelper(this.lexeme, this.isFirst, this.isLast) {
-    isRaw = StringUtilities.startsWithChar(lexeme, 0x72);
-    if (isRaw) {
-      start++;
-    }
-    if (StringUtilities.startsWith3(lexeme, start, 0x27, 0x27, 0x27)) {
-      isSingleQuoted = true;
-      isMultiline = true;
-      start += 3;
-      start = _trimInitialWhitespace(start);
-    } else if (StringUtilities.startsWith3(lexeme, start, 0x22, 0x22, 0x22)) {
-      isSingleQuoted = false;
-      isMultiline = true;
-      start += 3;
-      start = _trimInitialWhitespace(start);
-    } else if (start < lexeme.length && lexeme.codeUnitAt(start) == 0x27) {
-      isSingleQuoted = true;
-      isMultiline = false;
-      start++;
-    } else if (start < lexeme.length && lexeme.codeUnitAt(start) == 0x22) {
-      isSingleQuoted = false;
-      isMultiline = false;
-      start++;
+    if (isFirst) {
+      isRaw = StringUtilities.startsWithChar(lexeme, 0x72);
+      if (isRaw) {
+        start++;
+      }
+      if (StringUtilities.startsWith3(lexeme, start, 0x27, 0x27, 0x27)) {
+        isSingleQuoted = true;
+        isMultiline = true;
+        start += 3;
+        start = _trimInitialWhitespace(start);
+      } else if (StringUtilities.startsWith3(lexeme, start, 0x22, 0x22, 0x22)) {
+        isSingleQuoted = false;
+        isMultiline = true;
+        start += 3;
+        start = _trimInitialWhitespace(start);
+      } else if (start < lexeme.length && lexeme.codeUnitAt(start) == 0x27) {
+        isSingleQuoted = true;
+        isMultiline = false;
+        start++;
+      } else if (start < lexeme.length && lexeme.codeUnitAt(start) == 0x22) {
+        isSingleQuoted = false;
+        isMultiline = false;
+        start++;
+      }
     }
     end = lexeme.length;
     if (isLast) {

dart/pkg/analyzer/lib/src/generated/parser.dart
@@ -3454,7 +3454,9 @@ class Parser {
     StringLexemeHelper helper = new StringLexemeHelper(lexeme, isFirst, isLast);
     int start = helper.start;
     int end = helper.end;
-    if (end - start + 1 < 0) {
+    bool stringEndsAfterStart = end >= start;
+    assert(stringEndsAfterStart);
+    if (!stringEndsAfterStart) {
       AnalysisEngine.instance.logger.logError(
           "Internal error: computeStringValue($lexeme, $isFirst, $isLast)");
       return "";
@@ -4228,7 +4230,8 @@ class Parser {
       } finally {
         _inInitializer = wasInInitializer;
       }
-    } else if (_matches(TokenType.PERIOD) || _matches(TokenType.QUESTION_PERIOD)) {
+    } else if (_matches(TokenType.PERIOD) ||
+        _matches(TokenType.QUESTION_PERIOD)) {
       Token operator = getAndAdvance();
       return new PropertyAccess(prefix, operator, parseSimpleIdentifier());
     } else {
@@ -7822,8 +7825,8 @@ class Parser {
     Token nextToken = _skipSimpleIdentifier(token);
     if (nextToken != null) {
       return nextToken;
-    } else if (_tokenMatches(token, TokenType.CLOSE_PAREN)
-            || _tokenMatches(token, TokenType.COMMA)) {
+    } else if (_tokenMatches(token, TokenType.CLOSE_PAREN) ||
+        _tokenMatches(token, TokenType.COMMA)) {
       // If the `id.` is followed by something that cannot produce a valid
       // structure then assume this is a prefixed identifier but missing the
       // trailing identifier

dart/pkg/analyzer/test/generated/parser_test.dart
@@ -1559,9 +1559,8 @@ class Foo {
   }
 
   void test_missingMethodParameters_void_block() {
-    MethodDeclaration method = parse3("parseClassMember",
-        <Object>["C"], "void m {} }",
-        [ParserErrorCode.MISSING_METHOD_PARAMETERS]);
+    MethodDeclaration method = parse3("parseClassMember", <Object>["C"],
+        "void m {} }", [ParserErrorCode.MISSING_METHOD_PARAMETERS]);
     expect(method.parameters, hasLength(0));
   }
 
@@ -1866,8 +1865,7 @@ class Foo {
 
   void test_topLevel_getter() {
     FunctionDeclaration funct = parse3("parseCompilationUnitMember",
-        <Object>[emptyCommentAndMetadata()],
-        "get x => 7;");
+        <Object>[emptyCommentAndMetadata()], "get x => 7;");
     expect(funct.functionExpression.parameters, isNull);
   }
 
@@ -8956,6 +8954,23 @@ void''');
     expect((secondString as SimpleStringLiteral).value, "b");
   }
 
+  void test_parseStringLiteral_endsWithInterpolation() {
+    StringLiteral literal = parse4('parseStringLiteral', r"'x$y'");
+    expect(literal, new isInstanceOf<StringInterpolation>());
+    StringInterpolation interpolation = literal;
+    expect(interpolation.elements, hasLength(3));
+    expect(interpolation.elements[0], new isInstanceOf<InterpolationString>());
+    InterpolationString element0 = interpolation.elements[0];
+    expect(element0.value, 'x');
+    expect(
+        interpolation.elements[1], new isInstanceOf<InterpolationExpression>());
+    InterpolationExpression element1 = interpolation.elements[1];
+    expect(element1.expression, new isInstanceOf<SimpleIdentifier>());
+    expect(interpolation.elements[2], new isInstanceOf<InterpolationString>());
+    InterpolationString element2 = interpolation.elements[2];
+    expect(element2.value, '');
+  }
+
   void test_parseStringLiteral_interpolated() {
     StringInterpolation literal =
         parse4("parseStringLiteral", "'a \${b} c \$this d'");
@@ -8973,6 +8988,23 @@ void''');
         parse4("parseStringLiteral", "'''\\x20\na'''");
     expect(literal.literal, isNotNull);
     expect(literal.value, " \na");
+  }
+
+  void test_parseStringLiteral_multiline_endsWithInterpolation() {
+    StringLiteral literal = parse4('parseStringLiteral', r"'''x$y'''");
+    expect(literal, new isInstanceOf<StringInterpolation>());
+    StringInterpolation interpolation = literal;
+    expect(interpolation.elements, hasLength(3));
+    expect(interpolation.elements[0], new isInstanceOf<InterpolationString>());
+    InterpolationString element0 = interpolation.elements[0];
+    expect(element0.value, 'x');
+    expect(
+        interpolation.elements[1], new isInstanceOf<InterpolationExpression>());
+    InterpolationExpression element1 = interpolation.elements[1];
+    expect(element1.expression, new isInstanceOf<SimpleIdentifier>());
+    expect(interpolation.elements[2], new isInstanceOf<InterpolationString>());
+    InterpolationString element2 = interpolation.elements[2];
+    expect(element2.value, '');
   }
 
   void test_parseStringLiteral_multiline_escapedBackslash() {
@@ -9026,6 +9058,40 @@ void''');
     expect(literal.value, "\\t\na");
   }
 
+  void test_parseStringLiteral_multiline_quoteAfterInterpolation() {
+    StringLiteral literal = parse4('parseStringLiteral', r"""'''$x'y'''""");
+    expect(literal, new isInstanceOf<StringInterpolation>());
+    StringInterpolation interpolation = literal;
+    expect(interpolation.elements, hasLength(3));
+    expect(interpolation.elements[0], new isInstanceOf<InterpolationString>());
+    InterpolationString element0 = interpolation.elements[0];
+    expect(element0.value, '');
+    expect(
+        interpolation.elements[1], new isInstanceOf<InterpolationExpression>());
+    InterpolationExpression element1 = interpolation.elements[1];
+    expect(element1.expression, new isInstanceOf<SimpleIdentifier>());
+    expect(interpolation.elements[2], new isInstanceOf<InterpolationString>());
+    InterpolationString element2 = interpolation.elements[2];
+    expect(element2.value, "'y");
+  }
+
+  void test_parseStringLiteral_multiline_startsWithInterpolation() {
+    StringLiteral literal = parse4('parseStringLiteral', r"'''${x}y'''");
+    expect(literal, new isInstanceOf<StringInterpolation>());
+    StringInterpolation interpolation = literal;
+    expect(interpolation.elements, hasLength(3));
+    expect(interpolation.elements[0], new isInstanceOf<InterpolationString>());
+    InterpolationString element0 = interpolation.elements[0];
+    expect(element0.value, '');
+    expect(
+        interpolation.elements[1], new isInstanceOf<InterpolationExpression>());
+    InterpolationExpression element1 = interpolation.elements[1];
+    expect(element1.expression, new isInstanceOf<SimpleIdentifier>());
+    expect(interpolation.elements[2], new isInstanceOf<InterpolationString>());
+    InterpolationString element2 = interpolation.elements[2];
+    expect(element2.value, 'y');
+  }
+
   void test_parseStringLiteral_multiline_twoSpaces() {
     SimpleStringLiteral literal = parse4("parseStringLiteral", "'''  \na'''");
     expect(literal.literal, isNotNull);
@@ -9044,10 +9110,44 @@ void''');
     expect(literal.value, " a\nb");
   }
 
+  void test_parseStringLiteral_quoteAfterInterpolation() {
+    StringLiteral literal = parse4('parseStringLiteral', r"""'$x"'""");
+    expect(literal, new isInstanceOf<StringInterpolation>());
+    StringInterpolation interpolation = literal;
+    expect(interpolation.elements, hasLength(3));
+    expect(interpolation.elements[0], new isInstanceOf<InterpolationString>());
+    InterpolationString element0 = interpolation.elements[0];
+    expect(element0.value, '');
+    expect(
+        interpolation.elements[1], new isInstanceOf<InterpolationExpression>());
+    InterpolationExpression element1 = interpolation.elements[1];
+    expect(element1.expression, new isInstanceOf<SimpleIdentifier>());
+    expect(interpolation.elements[2], new isInstanceOf<InterpolationString>());
+    InterpolationString element2 = interpolation.elements[2];
+    expect(element2.value, '"');
+  }
+
   void test_parseStringLiteral_single() {
     SimpleStringLiteral literal = parse4("parseStringLiteral", "'a'");
     expect(literal.literal, isNotNull);
     expect(literal.value, "a");
+  }
+
+  void test_parseStringLiteral_startsWithInterpolation() {
+    StringLiteral literal = parse4('parseStringLiteral', r"'${x}y'");
+    expect(literal, new isInstanceOf<StringInterpolation>());
+    StringInterpolation interpolation = literal;
+    expect(interpolation.elements, hasLength(3));
+    expect(interpolation.elements[0], new isInstanceOf<InterpolationString>());
+    InterpolationString element0 = interpolation.elements[0];
+    expect(element0.value, '');
+    expect(
+        interpolation.elements[1], new isInstanceOf<InterpolationExpression>());
+    InterpolationExpression element1 = interpolation.elements[1];
+    expect(element1.expression, new isInstanceOf<SimpleIdentifier>());
+    expect(interpolation.elements[2], new isInstanceOf<InterpolationString>());
+    InterpolationString element2 = interpolation.elements[2];
+    expect(element2.value, 'y');
   }
 
   void test_parseSuperConstructorInvocation_named() {

dart/tests/language/regress_23046_test.dart
@@ -1,0 +1,17 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "package:expect/expect.dart";
+
+// Make sure the logic for skipping the initial quotes in a string isn't
+// re-triggered after an interpolation expression.
+
+const String x = '$y"';
+const String y = 'foo';
+const Map m = const { x: 0, y: 1 };
+
+main() {
+  Expect.equals(x, 'foo"');
+  Expect.equals(m.length, 2);
+}

commit 13149869d7ecb0820ad5be61984673bf5dba0622

Update status files to reflect bug 23100.
These status file updates reflect breakages I introduced in r44880.

BUG=dartbug.com/23100
TBR=brianwilkerson@google.com

Review URL: https://codereview.chromium.org//1056253002

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@44904 260f80e4-7a28-3924-810f-c04153c831b5

dart/tests/co19/co19-analyzer2.status
@@ -287,3 +287,8 @@ WebPlatformTest/dom/Node-replaceChild_t01: CompileTimeError # co19-roll r761: Pl
 WebPlatformTest/html/semantics/forms/the-input-element/email_t02: StaticWarning # co19 issue 701
 Language/12_Expressions/12_Instance_Creation/2_Const_A11_t01: MissingCompileTimeError # Issue 22010
 Language/12_Expressions/12_Instance_Creation/2_Const_A11_t03: MissingCompileTimeError # Issue 22010
+
+[ $compiler == dart2analyzer && $host_checked ]
+Language/12_Expressions/05_Strings/1_String_Interpolation_A01_t10: MissingCompileTimeError # Issue 23100
+Language/12_Expressions/05_Strings/1_String_Interpolation_A01_t16: MissingCompileTimeError # Issue 23100
+WebPlatformTest/dom/nodes/Element-childElementCount-nochild_t01: Pass # Should fail - Issue 23100

dart/tests/language/language_analyzer2.status
@@ -459,3 +459,6 @@ regress_12561_test: StaticWarning
 main_not_a_function_test/01: Fail # Issue 20030
 main_test/03: Fail # Issue 20030
 no_main_test/01: Fail # Issue 20030
+
+[ $compiler == dart2analyzer && $host_checked ]
+class_keyword_test/02: MissingCompileTimeError # Issue 23100